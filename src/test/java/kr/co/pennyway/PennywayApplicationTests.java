868c5d1d96cb35da6adc67178b2e9dfba970923e
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-06 16:55:44 +0900

    chore: init project

---
+++ src/test/java/kr/co/pennyway/PennywayApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.pennyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PennywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
