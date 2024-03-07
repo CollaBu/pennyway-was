6861ea9884341dfcdc971a6c92ff59f9ffd5ed48
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-07 10:40:03 +0900

    feat: common-공통 어노테이션 정의

---
+++ pennyway-common/src/main/java/kr/co/pennyway/common/annotation/Helper.java
@@ -0,0 +1,12 @@
+package kr.co.pennyway.common.annotation;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface Helper {
+}
