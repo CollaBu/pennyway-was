d1c5cbf7fc33aa154c9a059861b9b016fe202564
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-13 15:15:48 +0900

    feat: Jwt 예외 클래스

---
+++ pennyway-infra/src/main/java/kr/co/infra/common/exception/JwtErrorException.java
@@ -0,0 +1,22 @@
+package kr.co.infra.common.exception;
+
+import kr.co.pennyway.common.exception.BaseErrorCode;
+import kr.co.pennyway.common.exception.CausedBy;
+import kr.co.pennyway.common.exception.GlobalErrorException;
+
+public class JwtErrorException extends GlobalErrorException {
+    private final JwtErrorCode errorCode;
+
+    public JwtErrorException(JwtErrorCode jwtErrorCode) {
+        super(jwtErrorCode);
+        this.errorCode = jwtErrorCode;
+    }
+
+    public CausedBy causedBy() {
+        return errorCode.causedBy();
+    }
+
+    public JwtErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
