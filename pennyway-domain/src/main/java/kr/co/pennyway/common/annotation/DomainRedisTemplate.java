032e8941ee0525f59ebe33ba670cb70e920beac7
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-19 00:25:47 +0900

    ✨ Redis Configuration 설정 (#9)
    * chore: infra 모듈 내 redis 의존성 주입 (api)
    
    * chore: domain 모듈 내 redis 의존성 주입 (implementation)
    
    * chore: redis 환경변수 설정
    
    * feat: Domain Redis Connection Bean Qualify Annotation 생성
    
    * feat: Domain Redis CacheManager Qualify Annotation 생성
    
    * feat: Domain Redis Template Qualify Annotation 생성
    
    * chore: domain 모듈 redis config 작성
    
    * feat: Infra Redis CacheManager Qualify Annotation 생성
    
    * feat: SecurityUser Redis CacheManager Qualify Annotation 생성
    
    * feat: Oidc Redis CacheManager Qualify Annotation 생성
    
    * chore: infra 모듈 cache config 설정

---
+++ pennyway-domain/src/main/java/kr/co/pennyway/common/annotation/DomainRedisTemplate.java
@@ -0,0 +1,13 @@
+package kr.co.pennyway.common.annotation;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD,
+        ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Qualifier("domainRedisTemplate")
+public @interface DomainRedisTemplate {
+}
