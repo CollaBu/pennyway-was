640f072a25395a8701c1f6479d34757cd15bf162
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-20 15:00:17 +0900

    feat: db, application 타입 형변환 인터페이스 생성

---
+++ pennyway-domain/src/main/java/kr/co/pennyway/common/converter/LegacyCommonType.java
@@ -0,0 +1,10 @@
+package kr.co.pennyway.common.converter;
+
+public interface LegacyCommonType {
+    /**
+     * Legacy Super System 공통 코드를 반환한다.
+     *
+     * @return String 공통 코드
+     */
+    String getCode();
+}
