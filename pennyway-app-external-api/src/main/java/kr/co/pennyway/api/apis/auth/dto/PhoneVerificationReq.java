0b7be9624aef5851273b7f5dcf337eafd7007732
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-21 12:55:38 +0900

    rename: cerification -> verification 단어 수정

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/dto/PhoneVerificationReq.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/dto/PhoneVerificationReq.java
@@ -5,13 +5,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
-public class PhoneCertificationReq {
+public class PhoneVerificationReq {
     @Schema(title = "인증번호 요청 DTO", description = "전화번호로 인증번호 송신 요청을 위한 DTO")
     public record PushCodeReq(
             @Schema(description = "전화번호", example = "01012345678")
             @NotNull(message = "전화번호는 필수입니다.")
             @Pattern(regexp = "^01[01]-\\d{4}-\\d{4}$\n", message = "전화번호 형식이 올바르지 않습니다.")
-            String phoneNumber
+            String phone
     ) {
     }
 
@@ -20,7 +20,7 @@ public class PhoneCertificationReq {
             @Schema(description = "전화번호", example = "01012345678")
             @NotNull(message = "전화번호는 필수입니다.")
             @Pattern(regexp = "^01[01]-\\d{4}-\\d{4}$\n", message = "전화번호 형식이 올바르지 않습니다.")
-            String phoneNumber,
+            String phone,
             @Schema(description = "6자리 정수 인증번호", example = "123456")
             @NotBlank(message = "인증번호는 필수입니다.")
             @Pattern(regexp = "^\\d{6}$", message = "인증번호는 6자리 숫자여야 합니다.")
