926086b65fdc54b0d75d6deb103c7ad6c7382074
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-21 21:34:38 +0900

    fix: auth controller cookie util 의존성 주입

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/controller/AuthController.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/controller/AuthController.java
@@ -6,10 +6,12 @@ import kr.co.pennyway.api.apis.auth.dto.SignUpReq;
 import kr.co.pennyway.api.apis.auth.usecase.AuthUseCase;
 import kr.co.pennyway.api.common.response.SuccessResponse;
 import kr.co.pennyway.api.common.security.jwt.Jwts;
+import kr.co.pennyway.api.common.util.CookieUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
 import java.util.Map;
 
 @Slf4j
@@ -26,14 +29,17 @@ import java.util.Map;
 @RequestMapping("/api/v1/auth")
 public class AuthController {
     private final AuthUseCase authUseCase;
+    private final CookieUtil cookieUtil;
 
     @Operation(summary = "일반 회원가입")
     @PostMapping("/sign-up")
     // TODO: Spring Security 설정 후 @PreAuthorize("isAnonymous()") 추가
     public ResponseEntity<?> signUp(@RequestBody @Validated SignUpReq.General request) {
         Pair<Long, Jwts> jwts = authUseCase.signUp(request);
+        ResponseCookie cookie = cookieUtil.createCookie("refreshToken", jwts.getValue().refreshToken(), Duration.ofDays(7).toSeconds());
+
         return ResponseEntity.ok()
-                .header(HttpHeaders.SET_COOKIE, jwts.getValue().refreshToken())
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
                 .header(HttpHeaders.AUTHORIZATION, jwts.getValue().accessToken())
                 .body(SuccessResponse.from("user", Map.of("id", jwts.getKey())))
                 ;
