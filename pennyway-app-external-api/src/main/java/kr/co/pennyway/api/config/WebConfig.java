96a3d3574ff6ee1edb351dd35ae96fda9c793733
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-25 17:41:50 +0900

    fix: 사용자에게 code type 입력받는 필드 & converter 제거

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/config/WebConfig.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/config/WebConfig.java
@@ -1,6 +1,5 @@
 package kr.co.pennyway.api.config;
 
-import kr.co.pennyway.api.common.converter.ValidationCodeConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registrar) {
-        registrar.addConverter(new ValidationCodeConverter());
+        
     }
 }
