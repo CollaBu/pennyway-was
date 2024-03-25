c6168a3ad7866b2759b56d2526afbf3bf9593c78
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-25 23:36:01 +0900

    fix: user-sync-helper에서 oauth 계정 있으면 username 반환

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/helper/UserSyncHelper.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/helper/UserSyncHelper.java
@@ -8,6 +8,7 @@ import kr.co.pennyway.domain.domains.user.exception.UserErrorException;
 import kr.co.pennyway.domain.domains.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Slf4j
 @Helper
@@ -19,16 +20,16 @@ public class UserSyncHelper {
      * 일반 회원가입 시 이미 가입된 회원인지 확인
      *
      * @param phone String : 전화번호
-     * @return Boolean : oauth 회원 여부 (TRUE: oauth 회원, FALSE: 일반 회원)
+     * @return Pair<Boolean, String> : 이미 가입된 회원인지 여부 (TRUE: 가입되지 않은 회원, FALSE: 가입된 회원), 가입된 회원인 경우 회원 ID 반환
      * @throws UserErrorException : 이미 일반 회원가입을 한 유저인 경우
      */
-    public Boolean isSignedUserWhenGeneral(String phone) {
+    public Pair<Boolean, String> isSignedUserWhenGeneral(String phone) {
         User user;
         try {
             user = userService.readUserByPhone(phone);
         } catch (GlobalErrorException e) {
             log.info("User not found. phone: {}", phone);
-            return Boolean.FALSE;
+            return Pair.of(Boolean.TRUE, null);
         }
 
         if (user.getPassword() != null) {
@@ -36,6 +37,6 @@ public class UserSyncHelper {
             throw new UserErrorException(UserErrorCode.ALREADY_SIGNUP);
         }
 
-        return Boolean.TRUE;
+        return Pair.of(Boolean.FALSE, user.getUsername());
     }
 }
