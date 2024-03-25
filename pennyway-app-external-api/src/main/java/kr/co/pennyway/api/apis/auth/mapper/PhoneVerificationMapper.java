9f22d7cb16d86bceba7e3db42565b954ff13083e
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-25 17:56:53 +0900

    feat: phone verification mapper 클래스 정의

---
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/mapper/PhoneVerificationMapper.java
@@ -0,0 +1,39 @@
+package kr.co.pennyway.api.apis.auth.mapper;
+
+import kr.co.pennyway.api.apis.auth.dto.PhoneVerificationDto;
+import kr.co.pennyway.api.common.exception.PhoneVerificationErrorCode;
+import kr.co.pennyway.api.common.exception.PhoneVerificationException;
+import kr.co.pennyway.common.annotation.Mapper;
+import kr.co.pennyway.domain.common.redis.phone.PhoneVerificationCode;
+import kr.co.pennyway.domain.common.redis.phone.PhoneVerificationService;
+import kr.co.pennyway.infra.client.aws.sms.SmsDto;
+import kr.co.pennyway.infra.client.aws.sms.SmsProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Mapper
+@RequiredArgsConstructor
+public class PhoneVerificationMapper {
+    private final PhoneVerificationService phoneVerificationService;
+    private final SmsProvider smsProvider;
+
+    public PhoneVerificationDto.PushCodeRes saveCode(PhoneVerificationDto.PushCodeReq request, PhoneVerificationCode codeType) {
+        SmsDto.Info info = smsProvider.sendCode(SmsDto.To.of(request.phone()));
+        LocalDateTime expiresAt = phoneVerificationService.create(request.phone(), info.code(), codeType);
+        return PhoneVerificationDto.PushCodeRes.of(request.phone(), info.requestAt(), expiresAt);
+    }
+
+    public Boolean isValidCode(PhoneVerificationDto.VerifyCodeReq request, PhoneVerificationCode codeType) {
+        String expectedCode;
+        try {
+            expectedCode = phoneVerificationService.readByPhone(request.phone(), codeType);
+        } catch (IllegalArgumentException e) {
+            throw new PhoneVerificationException(PhoneVerificationErrorCode.EXPIRED_OR_INVALID_PHONE);
+        }
+
+        return expectedCode.equals(request.code());
+    }
+}
