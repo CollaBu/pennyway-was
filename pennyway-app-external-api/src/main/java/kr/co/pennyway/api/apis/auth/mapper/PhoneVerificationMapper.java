32b443aecd85271264c41e7ed2aca8fa33a6bcc9
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-25 20:39:46 +0900

    rename: phone verification mapper 메서드 주석 추가

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/mapper/PhoneVerificationMapper.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/mapper/PhoneVerificationMapper.java
@@ -20,12 +20,27 @@ public class PhoneVerificationMapper {
     private final PhoneVerificationService phoneVerificationService;
     private final SmsProvider smsProvider;
 
+    /**
+     * 휴대폰 번호로 인증 코드를 발송하고 캐싱한다. (5분간 유효)
+     *
+     * @param request  {@link PhoneVerificationDto.PushCodeReq}
+     * @param codeType {@link PhoneVerificationCode}
+     * @return {@link PhoneVerificationDto.PushCodeRes}
+     */
     public PhoneVerificationDto.PushCodeRes sendCode(PhoneVerificationDto.PushCodeReq request, PhoneVerificationCode codeType) {
         SmsDto.Info info = smsProvider.sendCode(SmsDto.To.of(request.phone()));
         LocalDateTime expiresAt = phoneVerificationService.create(request.phone(), info.code(), codeType);
         return PhoneVerificationDto.PushCodeRes.of(request.phone(), info.requestAt(), expiresAt);
     }
 
+    /**
+     * 휴대폰 번호로 인증 코드를 확인한다.
+     *
+     * @param request  {@link PhoneVerificationDto.VerifyCodeReq}
+     * @param codeType {@link PhoneVerificationCode}
+     * @return Boolean : 인증 코드가 유효한지 여부 (TRUE: 유효, 실패하는 경우 예외가 발생하므로 FALSE가 반환되지 않음)
+     * @throws PhoneVerificationException : 전화번호가 만료되었거나 유효하지 않은 경우(EXPIRED_OR_INVALID_PHONE), 인증 코드가 유효하지 않은 경우(IS_NOT_VALID_CODE)
+     */
     public Boolean isValidCode(PhoneVerificationDto.VerifyCodeReq request, PhoneVerificationCode codeType) {
         String expectedCode;
         try {
