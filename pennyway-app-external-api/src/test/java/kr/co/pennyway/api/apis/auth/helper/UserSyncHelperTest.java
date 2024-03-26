0554b6d7213dd4b4016307e604e3c54dc8f35e8e
Jinwoo Lee <binaryWooo@gmail.com>
2024-03-26 23:14:41 +0900

    fix: 테스트 실패 오류 해결

--- pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/auth/helper/UserSyncHelperTest.java
+++ pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/auth/helper/UserSyncHelperTest.java
@@ -1,5 +1,8 @@
 package kr.co.pennyway.api.apis.auth.helper;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+
 import kr.co.pennyway.domain.domains.user.domain.User;
 import kr.co.pennyway.domain.domains.user.exception.UserErrorCode;
 import kr.co.pennyway.domain.domains.user.exception.UserErrorException;
@@ -11,55 +14,56 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.BDDMockito.given;
-
 @ExtendWith(MockitoExtension.class)
 public class UserSyncHelperTest {
-    private final String phone = "010-1234-5678";
-    private UserSyncHelper userSyncHelper;
-    @Mock
-    private UserService userService;
 
-    @BeforeEach
-    void setUp() {
-        userSyncHelper = new UserSyncHelper(userService);
-    }
+  private final String phone = "010-1234-5678";
+  private UserSyncHelper userSyncHelper;
+  @Mock
+  private UserService userService;
 
-    @DisplayName("일반 회원가입 시, 회원 정보가 없으면 FALSE를 반환한다.")
-    @Test
-    void isSignedUserWhenGeneralReturnFalse() {
-        // given
-        given(userService.readUserByPhone(phone)).willThrow(new UserErrorException(UserErrorCode.NOT_FOUND));
+  @BeforeEach
+  void setUp() {
+    userSyncHelper = new UserSyncHelper(userService);
+  }
 
-        // when
-        Boolean result = userSyncHelper.isSignedUserWhenGeneral(phone);
+  @DisplayName("일반 회원가입 시, 회원 정보가 없으면 FALSE를 반환한다.")
+  @Test
+  void isSignedUserWhenGeneralReturnFalse() {
+    // given
+    given(userService.readUserByPhone(phone)).willThrow(
+        new UserErrorException(UserErrorCode.NOT_FOUND));
 
-        // then
-        assertEquals(result, Boolean.FALSE);
-    }
+    // when
+    Boolean result = userSyncHelper.isSignedUserWhenGeneral(phone).getKey();
 
-    @DisplayName("일반 회원가입 시, oauth 회원 정보가 있으면 TRUE를 반환한다.")
-    @Test
-    void isSignedUserWhenGeneralReturnTrue() {
-        // given
-        given(userService.readUserByPhone(phone)).willReturn(User.builder().password(null).build());
+    // then
+    assertEquals(result, Boolean.FALSE);
+  }
 
-        // when
-        Boolean result = userSyncHelper.isSignedUserWhenGeneral(phone);
+  @DisplayName("일반 회원가입 시, oauth 회원 정보가 있으면 TRUE를 반환한다.")
+  @Test
+  void isSignedUserWhenGeneralReturnTrue() {
+    // given
+    given(userService.readUserByPhone(phone)).willReturn(User.builder().password(null).build());
 
-        // then
-        assertEquals(result, Boolean.TRUE);
-    }
+    // when
+    Boolean result = userSyncHelper.isSignedUserWhenGeneral(phone).getKey();
 
-    @DisplayName("일반 회원가입 시, 이미 일반회원 가입된 회원인 경우 UserErrorException을 발생시킨다.")
-    @Test
-    void isSignedUserWhenGeneralThrowUserErrorException() {
-        // given
-        given(userService.readUserByPhone(phone)).willReturn(User.builder().password("password").build());
+    // then
+    assertEquals(result, Boolean.TRUE);
+  }
 
-        // when - then
-        UserErrorException exception = org.junit.jupiter.api.Assertions.assertThrows(UserErrorException.class, () -> userSyncHelper.isSignedUserWhenGeneral(phone));
-        System.out.println(exception.getExplainError());
-    }
+  @DisplayName("일반 회원가입 시, 이미 일반회원 가입된 회원인 경우 UserErrorException을 발생시킨다.")
+  @Test
+  void isSignedUserWhenGeneralThrowUserErrorException() {
+    // given
+    given(userService.readUserByPhone(phone)).willReturn(
+        User.builder().password("password").build());
+
+    // when - then
+    UserErrorException exception = org.junit.jupiter.api.Assertions.assertThrows(
+        UserErrorException.class, () -> userSyncHelper.isSignedUserWhenGeneral(phone));
+    System.out.println(exception.getExplainError());
+  }
 }
