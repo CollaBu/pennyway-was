213d5490e096bee7858f1714ea3ce8dc923a20ff
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-27 18:44:16 +0900

    fix: 일반 회원가입, 로그인 시나리오 helper 클래스 분리

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/helper/UserGeneralSignHelper.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/helper/UserGeneralSignHelper.java
@@ -1,5 +1,6 @@
 package kr.co.pennyway.api.apis.auth.helper;
 
+import kr.co.pennyway.api.apis.auth.dto.SignUpReq;
 import kr.co.pennyway.common.annotation.Helper;
 import kr.co.pennyway.domain.domains.user.domain.User;
 import kr.co.pennyway.domain.domains.user.exception.UserErrorCode;
@@ -10,13 +11,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * 일반 회원가입, 로그인 시나리오 도우미 클래스
+ *
+ * @author YANG JAESEO
+ */
 @Slf4j
 @Helper
 @RequiredArgsConstructor
-public class UserSignInHelper {
+public class UserGeneralSignHelper {
     private final UserService userService;
 
     private final PasswordEncoder bCryptPasswordEncoder;
+
+    @Transactional
+    public User createUserWithEncryptedPassword(SignUpReq.General request) {
+        User user = request.toEntity(bCryptPasswordEncoder);
+        return userService.createUser(user);
+    }
 
     /**
      * 로그인 시 유저가 존재하고 비밀번호가 일치하는지 확인
