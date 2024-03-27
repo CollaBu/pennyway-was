850fdc068cbba16b76fa6a3b00d4db8069ea3efe
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-27 19:45:32 +0900

    feat: 회원가입 시나리오 개선

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/usecase/AuthUseCase.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/usecase/AuthUseCase.java
@@ -3,16 +3,16 @@ package kr.co.pennyway.api.apis.auth.usecase;
 import kr.co.pennyway.api.apis.auth.dto.PhoneVerificationDto;
 import kr.co.pennyway.api.apis.auth.dto.SignInReq;
 import kr.co.pennyway.api.apis.auth.dto.SignUpReq;
-import kr.co.pennyway.api.apis.auth.helper.UserSyncHelper;
-import kr.co.pennyway.api.apis.auth.mapper.JwtAuthMapper;
+import kr.co.pennyway.api.apis.auth.helper.JwtAuthHelper;
 import kr.co.pennyway.api.apis.auth.mapper.PhoneVerificationMapper;
+import kr.co.pennyway.api.apis.auth.mapper.UserGeneralSignMapper;
+import kr.co.pennyway.api.apis.auth.mapper.UserSyncMapper;
 import kr.co.pennyway.api.common.security.jwt.Jwts;
 import kr.co.pennyway.common.annotation.UseCase;
 import kr.co.pennyway.domain.common.redis.phone.PhoneVerificationService;
 import kr.co.pennyway.domain.common.redis.phone.PhoneVerificationType;
 import kr.co.pennyway.domain.domains.user.domain.User;
 import kr.co.pennyway.domain.domains.user.exception.UserErrorException;
-import kr.co.pennyway.domain.domains.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.Pair;
@@ -22,13 +22,12 @@ import org.springframework.transaction.annotation.Transactional;
 @UseCase
 @RequiredArgsConstructor
 public class AuthUseCase {
-    private final UserService userService;
-    private final UserSyncHelper userSyncHelper;
+    private final UserSyncMapper userSyncMapper;
+    private final UserGeneralSignMapper userGeneralSignMapper;
 
-    private final JwtAuthMapper jwtAuthMapper;
+    private final JwtAuthHelper jwtAuthHelper;
     private final PhoneVerificationMapper phoneVerificationMapper;
     private final PhoneVerificationService phoneVerificationService;
-
 
     public PhoneVerificationDto.PushCodeRes sendCode(PhoneVerificationDto.PushCodeReq request) {
         return phoneVerificationMapper.sendCode(request, PhoneVerificationType.SIGN_UP);
@@ -36,33 +35,34 @@ public class AuthUseCase {
 
     public PhoneVerificationDto.VerifyCodeRes verifyCode(PhoneVerificationDto.VerifyCodeReq request) {
         Boolean isValidCode = phoneVerificationMapper.isValidCode(request, PhoneVerificationType.SIGN_UP);
-        Pair<Boolean, String> isOauthUser = checkOauthUser(request.phone());
+        Pair<Boolean, String> isOauthUser = checkOauthUserNotGeneralSignUp(request.phone());
 
         phoneVerificationService.extendTimeToLeave(request.phone(), PhoneVerificationType.SIGN_UP);
 
-        return PhoneVerificationDto.VerifyCodeRes.valueOf(isValidCode, isOauthUser.getKey(), isOauthUser.getValue());
+        return PhoneVerificationDto.VerifyCodeRes.valueOf(isValidCode, isOauthUser.getLeft(), isOauthUser.getRight());
     }
 
     @Transactional
-    public Pair<Long, Jwts> signUp(SignUpReq.General request) {
-        // TODO: 인증 번호 확인 로직 추가
-        // phoneVerificationHelper.verify(request.phone(), request.code());
+    public Pair<Long, Jwts> signUp(SignUpReq.Info request) {
+        phoneVerificationMapper.isValidCode(PhoneVerificationDto.VerifyCodeReq.from(request), PhoneVerificationType.SIGN_UP);
+        Pair<Boolean, String> isOauthUser = checkOauthUserNotGeneralSignUp(request.phone());
 
-        User user = userService.createUser(request.toEntity());
+        User user = userGeneralSignMapper.saveUserWithEncryptedPassword(request, isOauthUser);
+        phoneVerificationService.delete(request.phone(), PhoneVerificationType.SIGN_UP);
 
-        return Pair.of(user.getId(), jwtAuthMapper.createToken(user));
+        return Pair.of(user.getId(), jwtAuthHelper.createToken(user));
     }
 
     @Transactional(readOnly = true)
     public Pair<Long, Jwts> signIn(SignInReq.General request) {
-        User user = userSyncHelper.readUserIfValid(request.username(), request.password());
+        User user = userGeneralSignMapper.readUserIfValid(request.username(), request.password());
 
-        return Pair.of(user.getId(), jwtAuthMapper.createToken(user));
+        return Pair.of(user.getId(), jwtAuthHelper.createToken(user));
     }
 
-    private Pair<Boolean, String> checkOauthUser(String phone) {
+    private Pair<Boolean, String> checkOauthUserNotGeneralSignUp(String phone) {
         try {
-            return userSyncHelper.isSignedUserWhenGeneral(phone);
+            return userSyncMapper.isGeneralSignUpAllowed(phone);
         } catch (UserErrorException e) {
             phoneVerificationService.delete(phone, PhoneVerificationType.SIGN_UP);
             throw e;
