f39cdb79b7e858b069c0b7b50aeb2de118be6b11
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-27 17:31:59 +0900

    ✨ 로그인 API (#23)
    * feat: 일반 로그인 요청 dto 작성
    
    * feat: user repository find-by-username 메서드 추가
    
    * feat: user service read-user-by-username 메서드 구현
    
    * feat: 유저 비밀번호 예외 추가
    
    * feat: user sync helper read-user-if-valid 메서드
    
    * feat: auth user case내 sign in 메서드 추가
    
    * feat: sign in api 추가
    
    * test: sign in test case 추가
    
    * fix: sign in dto 정규표현식 검사 제거

---
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/dto/SignInReq.java
@@ -0,0 +1,19 @@
+package kr.co.pennyway.api.apis.auth.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SignInReq {
+    public record General(
+            @Schema(description = "아이디", example = "pennyway")
+            @NotBlank(message = "아이디를 입력해주세요")
+            String username,
+            @Schema(description = "비밀번호", example = "pennyway1234")
+            @NotBlank(message = "비밀번호를 입력해주세요")
+            String password
+    ) {
+    }
+}
