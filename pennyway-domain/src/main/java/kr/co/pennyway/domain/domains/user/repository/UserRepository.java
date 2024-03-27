583ca3242f51fa7f8183f1cf4cdd176d3a3ed9a7
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-27 12:53:23 +0900

    feat: user repository find-by-username 메서드 추가

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/user/repository/UserRepository.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/user/repository/UserRepository.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByPhone(String phone);
+
+    Optional<User> findByUsername(String username);
 }
