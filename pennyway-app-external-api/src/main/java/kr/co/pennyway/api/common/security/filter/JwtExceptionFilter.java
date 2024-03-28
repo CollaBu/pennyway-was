8e7cda6b839d72e2b993636047830ac3d7f74e71
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-28 11:48:24 +0900

    feat: jwt 예외 필터 작성

---
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/common/security/filter/JwtExceptionFilter.java
@@ -0,0 +1,42 @@
+package kr.co.pennyway.api.common.security.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kr.co.pennyway.api.common.response.ErrorResponse;
+import kr.co.pennyway.infra.common.exception.JwtErrorException;
+import kr.co.pennyway.infra.common.util.JwtErrorCodeUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.NonNull;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtExceptionFilter extends OncePerRequestFilter {
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            JwtErrorException exception = JwtErrorCodeUtil.determineAuthErrorException(e);
+            log.warn("Exception caught in JwtExceptionFilter: {}", exception.getMessage());
+
+            sendAuthError(response, exception);
+        }
+    }
+
+    private void sendAuthError(HttpServletResponse response, JwtErrorException e) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(e.getErrorCode().getStatusCode().getCode());
+
+        ErrorResponse errorResponse = ErrorResponse.of(e.causedBy().getCode(), e.causedBy().getReason());
+        objectMapper.writeValue(response.getWriter(), errorResponse);
+    }
+}
