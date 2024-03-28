ad568369389c2837db40758e5e55eefbe2a8cf90
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-28 12:45:32 +0900

    chore: security filter config 설정

---
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/config/security/SecurityFilterConfig.java
@@ -0,0 +1,33 @@
+package kr.co.pennyway.api.config.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.co.pennyway.api.common.security.filter.JwtAuthenticationFilter;
+import kr.co.pennyway.api.common.security.filter.JwtExceptionFilter;
+import kr.co.pennyway.domain.common.redis.forbidden.ForbiddenTokenService;
+import kr.co.pennyway.infra.common.jwt.JwtProvider;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@Configuration
+public class SecurityFilterConfig {
+    private final UserDetailsService userDetailServiceImpl;
+    private final ForbiddenTokenService forbiddenTokenService;
+
+    private final JwtProvider accessTokenProvider;
+
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    public JwtExceptionFilter jwtExceptionFilter() {
+        return new JwtExceptionFilter(objectMapper);
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthorizationFilter() {
+        return new JwtAuthenticationFilter(userDetailServiceImpl, forbiddenTokenService, accessTokenProvider);
+    }
+}
