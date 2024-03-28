6b697c3b45bbc03f10e507194b34e53a53b70daf
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-28 12:15:29 +0900

    feat: forbedden token entity 정의

---
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/common/redis/forbidden/ForbiddenToken.java
@@ -0,0 +1,42 @@
+package kr.co.pennyway.domain.common.redis.forbidden;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+@Getter
+@RedisHash("forbiddenToken")
+public class ForbiddenToken {
+    @Id
+    private final String accessToken;
+    private final Long userId;
+    @TimeToLive
+    private final long ttl;
+
+    @Builder
+    private ForbiddenToken(String accessToken, Long userId, long ttl) {
+        this.accessToken = accessToken;
+        this.userId = userId;
+        this.ttl = ttl;
+    }
+
+    public static ForbiddenToken of(String accessToken, Long userId, long ttl) {
+        return new ForbiddenToken(accessToken, userId, ttl);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ForbiddenToken that)) return false;
+        return accessToken.equals(that.accessToken) && userId.equals(that.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = accessToken.hashCode();
+        result = ((1 << 5) - 1) * result + userId.hashCode();
+        return result;
+    }
+}
