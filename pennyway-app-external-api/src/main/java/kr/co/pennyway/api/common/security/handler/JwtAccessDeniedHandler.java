928deec8c162e41b40d06de609e772f143aa49bf
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-28 11:39:52 +0900

    feat: 403 에러 핸들러 작성

---
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/common/security/handler/JwtAccessDeniedHandler.java
@@ -0,0 +1,33 @@
+package kr.co.pennyway.api.common.security.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kr.co.pennyway.api.common.response.ErrorResponse;
+import kr.co.pennyway.common.exception.CausedBy;
+import kr.co.pennyway.common.exception.ReasonCode;
+import kr.co.pennyway.common.exception.StatusCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.error("handle error: {}", accessDeniedException.getMessage());
+        CausedBy causedBy = CausedBy.of(StatusCode.FORBIDDEN, ReasonCode.ACCESS_TO_THE_REQUESTED_RESOURCE_IS_FORBIDDEN);
+
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(causedBy.statusCode().getCode());
+        ErrorResponse errorResponse = ErrorResponse.of(causedBy.getCode(), causedBy.getReason());
+        objectMapper.writeValue(response.getWriter(), errorResponse);
+    }
+}
