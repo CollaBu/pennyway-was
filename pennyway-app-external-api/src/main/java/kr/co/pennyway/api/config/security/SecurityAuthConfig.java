4ed8f00f221dc7d8282e485648e117d2b5df9f4e
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-30 02:31:08 +0900

    chore: external api 모듈 내 jackson nullable module 종속성 추가

---
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/config/security/SecurityAuthConfig.java
@@ -0,0 +1,36 @@
+package kr.co.pennyway.api.config.security;
+
+import kr.co.pennyway.api.common.security.authentication.UserDetailServiceImpl;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@Configuration
+public class SecurityAuthenticationConfig {
+    private final UserDetailServiceImpl userDetailsService;
+
+    @Bean
+    public PasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(bCryptPasswordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
