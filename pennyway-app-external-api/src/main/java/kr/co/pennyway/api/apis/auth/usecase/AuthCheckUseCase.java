164ac5f7520a2b1223c88c92728f3b41ee54bef8
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-01 17:22:41 +0900

    feat: username 중복 검사 api 개방

---
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/usecase/AuthCheckUseCase.java
@@ -0,0 +1,19 @@
+package kr.co.pennyway.api.apis.auth.usecase;
+
+import kr.co.pennyway.common.annotation.UseCase;
+import kr.co.pennyway.domain.domains.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@UseCase
+@RequiredArgsConstructor
+public class AuthCheckUseCase {
+    private final UserService userService;
+
+    @Transactional(readOnly = true)
+    public boolean checkUsernameDuplicate(String nickname) {
+        return userService.isExistNickname(nickname);
+    }
+}
