b6943e49a416f8d5f4c976e05e98c5486f1852a8
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-01 20:27:59 +0900

    ✨ 닉네임 중복검사 API (#31)
    * rename: auth controller '일반 회원 가입' 전화번호 인증 swagger 문서 상 명시
    
    * feat: 닉네임 중복 검사 domain service 메서드 추가
    
    * feat: username 중복 검사 api 개방
    
    * fix: 중복 검사 체크 url을 anonymous endpoints에 추가
    
    * fix: swagger endpoints와 read only public endpoints 분리
    
    * fix: 닉네임 중복 검사 인가 기준 permit-all로 변경
    
    * rename: is-exist-nickname -> is-exist-username
    
    * rename: auth check controller 매개변수명 username으로 수정

---
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/controller/AuthCheckController.java
@@ -0,0 +1,31 @@
+package kr.co.pennyway.api.apis.auth.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import kr.co.pennyway.api.apis.auth.usecase.AuthCheckUseCase;
+import kr.co.pennyway.api.common.response.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@Tag(name = "[계정 검사 API]")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/duplicate")
+public class AuthCheckController {
+    private final AuthCheckUseCase authCheckUseCase;
+
+    @Operation(summary = "닉네임 중복 검사")
+    @GetMapping("/username")
+    @PreAuthorize("permitAll()")
+    public ResponseEntity<?> checkUsername(@RequestParam @Validated String username) {
+        return ResponseEntity.ok(SuccessResponse.from("isDuplicate", authCheckUseCase.checkUsernameDuplicate(username)));
+    }
+}
