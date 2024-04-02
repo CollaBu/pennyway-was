ba5243deec49478ff24907a0bd03d68c415f75a8
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-02 15:11:31 +0900

    chore: cache config @configuration 어노테이션 재삽입

--- pennyway-infra/src/main/java/kr/co/pennyway/infra/config/CacheConfig.java
+++ pennyway-infra/src/main/java/kr/co/pennyway/infra/config/CacheConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.CacheKeyPrefix;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -23,6 +24,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import java.time.Duration;
 import java.util.Map;
 
+@Configuration
 @EnableCaching
 public class CacheConfig implements PennywayInfraConfig {
     private final String host;
