1c9c88e41aee23c3dde3783f63ab4a78b6e8aafe
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-02 11:45:48 +0900

    feat: oidc configuration properties config 세팅

--- pennyway-infra/src/main/java/kr/co/pennyway/infra/config/ConfigurationPropertiesConfig.java
+++ pennyway-infra/src/main/java/kr/co/pennyway/infra/config/ConfigurationPropertiesConfig.java
@@ -1,11 +1,17 @@
 package kr.co.pennyway.infra.config;
 
+import kr.co.pennyway.infra.common.properties.AppleOidcProperties;
+import kr.co.pennyway.infra.common.properties.GoogleOidcProperties;
+import kr.co.pennyway.infra.common.properties.KakaoOidcProperties;
 import kr.co.pennyway.infra.common.properties.ServerProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @EnableConfigurationProperties({
-        ServerProperties.class
+        ServerProperties.class,
+        AppleOidcProperties.class,
+        GoogleOidcProperties.class,
+        KakaoOidcProperties.class
 })
 @Configuration
 public class ConfigurationPropertiesConfig {
