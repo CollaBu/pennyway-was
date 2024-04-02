a18e5b979c57e9461e67061deded0750d23ee671
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-02 21:28:22 +0900

    fix: cache config 클래스 마커 인터페이스 구현 제거

--- pennyway-infra/src/main/java/kr/co/pennyway/infra/config/CacheConfig.java
+++ pennyway-infra/src/main/java/kr/co/pennyway/infra/config/CacheConfig.java
@@ -3,12 +3,10 @@ package kr.co.pennyway.infra.config;
 import kr.co.pennyway.infra.common.annotation.InfraRedisConnectionFactory;
 import kr.co.pennyway.infra.common.annotation.OidcCacheManager;
 import kr.co.pennyway.infra.common.annotation.SecurityUserCacheManager;
-import kr.co.pennyway.infra.common.importer.PennywayInfraConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.CacheKeyPrefix;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -24,9 +22,9 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import java.time.Duration;
 import java.util.Map;
 
-@Configuration
+
 @EnableCaching
-public class CacheConfig implements PennywayInfraConfig {
+public class CacheConfig {
     private final String host;
     private final int port;
     private final String password;
