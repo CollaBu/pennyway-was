0a5c60d00bb2c89bf0f694c4deaebed0255ae54e
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-03 00:55:30 +0900

    fix: cache config @configuration 어노테이션 복구

--- pennyway-infra/src/main/java/kr/co/pennyway/infra/config/CacheConfig.java
+++ pennyway-infra/src/main/java/kr/co/pennyway/infra/config/CacheConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.CacheKeyPrefix;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -22,7 +23,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import java.time.Duration;
 import java.util.Map;
 
-
+@Configuration
 @EnableCaching
 public class CacheConfig {
     private final String host;
