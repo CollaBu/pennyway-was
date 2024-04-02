3e2559693a2cd6602aa56d5716cecddfd8bbf223
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-02 15:12:17 +0900

    chore: infra config -> api 모듈에서 사용할 infra 모듈의 properties 명시

---
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/config/InfraConfig.java
@@ -0,0 +1,18 @@
+package kr.co.pennyway.api.config;
+
+import kr.co.pennyway.infra.common.properties.AppleOidcProperties;
+import kr.co.pennyway.infra.common.properties.GoogleOidcProperties;
+import kr.co.pennyway.infra.common.properties.KakaoOidcProperties;
+import kr.co.pennyway.infra.common.properties.ServerProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties({
+        ServerProperties.class,
+        AppleOidcProperties.class,
+        GoogleOidcProperties.class,
+        KakaoOidcProperties.class
+})
+public class InfraConfig {
+}
