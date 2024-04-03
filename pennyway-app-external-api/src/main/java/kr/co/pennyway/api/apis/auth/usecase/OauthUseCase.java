b1608266bf5245ddd4f1f61e301ee9e2e0368ab2
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-03 17:11:19 +0900

    feat: oauth 로그인 use case 구현

---
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/usecase/OauthUseCase.java
@@ -0,0 +1,38 @@
+package kr.co.pennyway.api.apis.auth.usecase;
+
+import kr.co.pennyway.api.apis.auth.dto.SignInReq;
+import kr.co.pennyway.api.apis.auth.helper.JwtAuthHelper;
+import kr.co.pennyway.api.apis.auth.helper.OauthOidcHelper;
+import kr.co.pennyway.api.apis.auth.mapper.UserOauthSignMapper;
+import kr.co.pennyway.api.common.security.jwt.Jwts;
+import kr.co.pennyway.common.annotation.UseCase;
+import kr.co.pennyway.domain.domains.oauth.exception.OauthErrorCode;
+import kr.co.pennyway.domain.domains.oauth.exception.OauthException;
+import kr.co.pennyway.domain.domains.oauth.type.Provider;
+import kr.co.pennyway.domain.domains.user.domain.User;
+import kr.co.pennyway.infra.common.oidc.OidcDecodePayload;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@UseCase
+@RequiredArgsConstructor
+public class OauthUseCase {
+    private final OauthOidcHelper oauthOidcHelper;
+    private final JwtAuthHelper jwtAuthHelper;
+    private UserOauthSignMapper userOauthSignMapper;
+
+    @Transactional(readOnly = true)
+    public Pair<Long, Jwts> signIn(Provider provider, SignInReq.Oauth request) {
+        OidcDecodePayload payload = oauthOidcHelper.getPayload(provider, request.idToken());
+        log.info("payload : {}", payload);
+
+        if (!request.oauthId().equals(payload.sub()))
+            throw new OauthException(OauthErrorCode.NOT_MATCHED_OAUTH_ID);
+        User user = userOauthSignMapper.readUser(request.oauthId(), provider);
+
+        return (user != null) ? Pair.of(user.getId(), jwtAuthHelper.createToken(user)) : Pair.of(-1L, null);
+    }
+}
