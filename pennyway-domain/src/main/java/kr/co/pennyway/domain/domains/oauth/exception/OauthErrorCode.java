6618f624da4b199b3c185d82082fcc3d071fe230
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-03 17:10:11 +0900

    rename: oauth error code 주석 포맷 변경

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/oauth/exception/OauthErrorCode.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/oauth/exception/OauthErrorCode.java
@@ -10,10 +10,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum OauthErrorCode implements BaseErrorCode {
-    // 401 Unauthorized
+    /* 401 Unauthorized */
     NOT_MATCHED_OAUTH_ID(StatusCode.UNAUTHORIZED, ReasonCode.MISSING_OR_INVALID_AUTHENTICATION_CREDENTIALS, "OAuth ID가 일치하지 않습니다."),
 
-    // 422 Unprocessable Entity
+    /* 422 Unprocessable Entity */
     INVALID_PROVIDER(StatusCode.UNPROCESSABLE_CONTENT, ReasonCode.TYPE_MISMATCH_ERROR_IN_REQUEST_BODY, "유효하지 않은 제공자입니다.");
 
     private final StatusCode statusCode;
