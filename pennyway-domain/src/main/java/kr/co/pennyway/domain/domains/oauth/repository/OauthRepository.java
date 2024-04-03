25af1f3a34a9da861e6ff98e13c5bbfa71a1deb0
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-03 15:48:20 +0900

    feat: oauth repository 소셜 아이디 & 제공자 탐색 메서드 선언

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/oauth/repository/OauthRepository.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/oauth/repository/OauthRepository.java
@@ -1,7 +1,11 @@
 package kr.co.pennyway.domain.domains.oauth.repository;
 
 import kr.co.pennyway.domain.domains.oauth.domain.Oauth;
+import kr.co.pennyway.domain.domains.oauth.type.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OauthRepository extends JpaRepository<Oauth, Long> {
+    Optional<Oauth> findByOauthIdAndProvider(String oauthId, Provider provider);
 }
