65857d9e45570c7e3582066e32bc4f8874a432a1
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-03 18:47:38 +0900

    fix: 전화번호 인증 oauth provider 구분

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/common/redis/phone/PhoneVerificationType.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/common/redis/phone/PhoneVerificationType.java
@@ -1,12 +1,26 @@
 package kr.co.pennyway.domain.common.redis.phone;
 
+import kr.co.pennyway.domain.domains.oauth.type.Provider;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public enum PhoneVerificationType {
-    SIGN_UP("signUp"), OAUTH_SIGN_UP("oauthSignUp"), FIND_USERNAME("username"), FIND_PASSWORD("password");
+    SIGN_UP("signUp"),
+    OAUTH_SIGN_UP_KAKAO("oauthSignUp:kakao"),
+    OAUTH_SIGN_UP_GOOGLE("oauthSignUp:google"),
+    OAUTH_SIGN_UP_APPLE("oauthSignUp:apple"),
+    FIND_USERNAME("username"),
+    FIND_PASSWORD("password");
 
     private final String prefix;
+
+    public PhoneVerificationType getOauthSignUpTypeByProvider(Provider provider) {
+        return switch (provider) {
+            case KAKAO -> OAUTH_SIGN_UP_KAKAO;
+            case GOOGLE -> OAUTH_SIGN_UP_GOOGLE;
+            case APPLE -> OAUTH_SIGN_UP_APPLE;
+        };
+    }
 }
