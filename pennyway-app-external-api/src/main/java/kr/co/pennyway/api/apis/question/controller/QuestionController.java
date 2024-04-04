0defa67f9dd5e2cd917c63574c34eb12a225ba9e
asn6878 <asn6878@gmail.com>
2024-04-05 04:04:18 +0900

    fix: http 메서드 수정

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/question/controller/QuestionController.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/question/controller/QuestionController.java
@@ -10,10 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Tag(name = "[문의 API]")
@@ -24,7 +21,7 @@ public class QuestionController {
     private final QuestionUseCase questionUseCase;
 
     @Operation(summary = "문의 전송")
-    @GetMapping("/send")
+    @PostMapping("/send")
     @PreAuthorize("isAnonymous()")
     public ResponseEntity<?> sendQuestion(@RequestBody @Validated QuestionReq.General request){
         questionUseCase.sendQuestion(request);
