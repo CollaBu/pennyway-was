b1dc050f70bcf33de37e75bdcf4ae4a52ee65b42
DinoDeveloper <asn6878@gmail.com>
2024-04-04 18:16:02 +0900

    feat: question 엔티티, repository 작성

---
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/question/domain/Question.java
@@ -0,0 +1,34 @@
+package kr.co.pennyway.domain.domains.question.domain;
+
+import jakarta.persistence.*;
+import kr.co.pennyway.domain.domains.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "Question")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private QuestionCategory category;
+    private String content;
+
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @ColumnDefault("NULL")
+    private LocalDateTime deletedAt;
+}
---
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/question/domain/QuestionCategory.java
@@ -0,0 +1,16 @@
+package kr.co.pennyway.domain.domains.question.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public enum QuestionCategory {
+    UTILIZATION("이용 관련"),
+    BUG_REPORT("오류 신고"),
+    SUGGESTION("서비스 제안"),
+    ETC("기타");
+
+    private final String title;
+}
