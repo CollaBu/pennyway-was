66bebf9aec4ac38324895662eb9934dc6eb1c291
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-05 02:22:24 +0900

    test: 회원가입 유저 시나리오 통합 테스트 (동작 확인용)

--- pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/auth/controller/AuthControllerIntegrationTest.java
+++ pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/auth/controller/AuthControllerIntegrationTest.java
@@ -1,15 +1,75 @@
 package kr.co.pennyway.api.apis.auth.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.co.pennyway.api.apis.auth.dto.SignUpReq;
 import kr.co.pennyway.api.config.ExternalApiDBTestConfig;
 import kr.co.pennyway.api.config.ExternalApiIntegrationTest;
+import kr.co.pennyway.domain.common.redis.phone.PhoneVerificationService;
+import kr.co.pennyway.domain.common.redis.phone.PhoneVerificationType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExternalApiIntegrationTest
+@AutoConfigureMockMvc
 public class AuthControllerIntegrationTest extends ExternalApiDBTestConfig {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private PhoneVerificationService phoneVerificationService;
+
+    @BeforeEach
+    void setUp(WebApplicationContext webApplicationContext) {
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .defaultRequest(post("/**").with(csrf()))
+                .build();
+    }
+
     @Test
     @DisplayName("컨테이너 실행 테스트")
     void containerTest() {
         System.out.println("컨테이너 실행 테스트");
+    }
+
+    @Test
+    @WithAnonymousUser
+    @DisplayName("회원가입 통합 테스트")
+    void controllerTest() throws Exception {
+        // given
+        SignUpReq.General request = new SignUpReq.General("pennyway", "jayang", "dkssudgktpdy1", "010-1234-5678", "050505");
+        phoneVerificationService.create("010-1234-5678", "050505", PhoneVerificationType.SIGN_UP);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                post("/v1/auth/sign-up")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+        );
+
+        // then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(header().exists("Set-Cookie"))
+                .andExpect(header().exists("Authorization"))
+                .andExpect(jsonPath("$.data.user.id").value(1))
+                .andDo(print());
     }
 }
