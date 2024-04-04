ee9bf172878652fe892312f613d4b943cc55c6ae
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-04 21:43:20 +0900

    test: api 통합 테스트 실행 테스트

---
+++ pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/auth/controller/AuthControllerIntegrationTest.java
@@ -0,0 +1,17 @@
+package kr.co.pennyway.api.apis.auth.controller;
+
+import kr.co.pennyway.api.config.ExternalApiDBTestConfig;
+import kr.co.pennyway.api.config.ExternalApiIntegrationTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@ExternalApiIntegrationTest
+public class AuthControllerIntegrationTest extends ExternalApiDBTestConfig {
+    @Test
+    @DisplayName("컨테이너 실행 테스트")
+    void containerTest() {
+        System.out.println("컨테이너 실행 테스트");
+    }
+}
