afd13b6b92e835899bdb6803dd50832c364bde30
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-06 17:44:24 +0900

    🐛 OIDC signature 검증없이 header, payload 추출 로직 수정 (#35)
    * rename: oauth oidc provider imple 에러 메서드 명시
    
    * fix: token header, payload 추출 메서드 jjwt 라이브러리 의존 제거
    
    * rename: get oidc token body 메서드 내 aud 로그 제거
    
    * fix: oidc provider log 제거 및 get unsignedtoken() token 마지막에 . 제거

--- pennyway-infra/src/main/java/kr/co/pennyway/infra/common/oidc/OauthOidcProviderImpl.java
+++ pennyway-infra/src/main/java/kr/co/pennyway/infra/common/oidc/OauthOidcProviderImpl.java
@@ -1,11 +1,18 @@
 package kr.co.pennyway.infra.common.oidc;
 
-import io.jsonwebtoken.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
 import kr.co.pennyway.infra.common.exception.JwtErrorCode;
 import kr.co.pennyway.infra.common.exception.JwtErrorException;
 import kr.co.pennyway.infra.common.util.JwtErrorCodeUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 import java.math.BigInteger;
 import java.security.KeyFactory;
@@ -14,23 +21,25 @@ import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.RSAPublicKeySpec;
 import java.util.Base64;
+import java.util.Map;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class OauthOidcProviderImpl implements OauthOidcProvider {
     private static final String KID = "kid";
     private static final String RSA = "RSA";
+    private final ObjectMapper objectMapper;
 
     @Override
     public String getKidFromUnsignedTokenHeader(String token, String iss, String aud, String nonce) {
-        return (String) getUnsignedTokenClaims(token, iss, aud, nonce).getHeader().get(KID);
+        return getUnsignedTokenClaims(token, iss, aud, nonce).get("header").get(KID);
     }
 
     @Override
     public OidcDecodePayload getOIDCTokenBody(String token, String modulus, String exponent) {
         Claims body = getOIDCTokenJws(token, modulus, exponent).getPayload();
-        String aud = body.getAudience().iterator().next(); // TODO: 이전 버전과 다르게 aud가 Set<String>으로 변경되어 있음. 테스트 필요
-        log.debug("aud : {}", aud);
+        String aud = body.getAudience().iterator().next(); // aud가 여러개일 경우 첫 번째 aud를 사용
 
         return new OidcDecodePayload(
                 body.getIssuer(),
@@ -43,19 +52,26 @@ public class OauthOidcProviderImpl implements OauthOidcProvider {
      * ID Token의 header와 body를 Base64 방식으로 디코딩하는 메서드 <br/>
      * payload의 iss, aud, exp, nonce를 검증하고, 실패시 예외 처리
      */
-    private Jwt<Header, Claims> getUnsignedTokenClaims(String token, String iss, String aud, String nonce) {
+    private Map<String, Map<String, String>> getUnsignedTokenClaims(String token, String iss, String aud, String nonce) {
         try {
-            return Jwts.parser()
-                    .requireAudience(aud)
-                    .requireIssuer(iss)
-//                    .require("nonce", nonce) // 현재는 nonce를 사용하지 않음
-                    .build()
-                    .parseUnsecuredClaims(getUnsignedToken(token)); // TODO: 기존 방식은 parseClaimsJwt(getUnsignedToken(token)); -> 변경한 코드 정상 동작 여부 확인 필요
-        } catch (JwtException e) {
-            final JwtErrorCode errorCode = JwtErrorCodeUtil.determineErrorCode(e, JwtErrorCode.FAILED_AUTHENTICATION);
+            Base64.Decoder decoder = Base64.getUrlDecoder();
 
-            log.warn("Error code : {}, Error - {},  {}", errorCode, e.getClass(), e.getMessage());
-            throw new JwtErrorException(errorCode);
+            String unsignedToken = getUnsignedToken(token);
+            String headerJson = new String(decoder.decode(unsignedToken.split("\\.")[0]));
+            String payloadJson = new String(decoder.decode(unsignedToken.split("\\.")[1]));
+
+            @SuppressWarnings("unchecked")
+            Map<String, String> header = objectMapper.readValue(headerJson, Map.class);
+            @SuppressWarnings("unchecked")
+            Map<String, String> payload = objectMapper.readValue(payloadJson, Map.class);
+
+            Assert.isTrue(payload.get("aud").equals(aud), "aud is not matched. expected : " + aud + ", actual : " + payload.get("aud"));
+            Assert.isTrue(payload.get("iss").equals(iss), "iss is not matched. expected : " + iss + ", actual : " + payload.get("iss"));
+
+            return Map.of("header", header, "payload", payload);
+        } catch (JsonProcessingException e) {
+            log.warn("getUnsignedTokenClaims : Error - {},  {}", e.getClass(), e.getMessage());
+            throw new RuntimeException(e);
         }
     }
 
@@ -65,7 +81,7 @@ public class OauthOidcProviderImpl implements OauthOidcProvider {
     private String getUnsignedToken(String token) {
         String[] splitToken = token.split("\\.");
         if (splitToken.length != 3) throw new JwtErrorException(JwtErrorCode.MALFORMED_TOKEN);
-        return splitToken[0] + "." + splitToken[1] + ".";
+        return splitToken[0] + "." + splitToken[1];
     }
 
     /**
@@ -81,10 +97,10 @@ public class OauthOidcProviderImpl implements OauthOidcProvider {
         } catch (JwtException e) {
             final JwtErrorCode errorCode = JwtErrorCodeUtil.determineErrorCode(e, JwtErrorCode.FAILED_AUTHENTICATION);
 
-            log.warn("Error code : {}, Error - {},  {}", errorCode, e.getClass(), e.getMessage());
+            log.warn("getOIDCTokenJws : Error code : {}, Error - {},  {}", errorCode, e.getClass(), e.getMessage());
             throw new JwtErrorException(errorCode);
         } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-            log.warn("Error - {},  {}", e.getClass(), e.getMessage());
+            log.warn("getOIDCTokenJws : Error - {},  {}", e.getClass(), e.getMessage());
             throw new JwtErrorException(JwtErrorCode.MALFORMED_TOKEN);
         }
     }
