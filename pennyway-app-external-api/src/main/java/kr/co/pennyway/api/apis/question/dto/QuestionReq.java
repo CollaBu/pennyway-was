1e7d533ed3b3d4e1613d9fb3b06a4e0148fdcd68
DinoDeveloper <asn6878@gmail.com>
2024-04-08 16:44:38 +0900

    fix: dto email 필드 유효성 검사 추가

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/question/dto/QuestionReq.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/question/dto/QuestionReq.java
@@ -1,6 +1,7 @@
 package kr.co.pennyway.api.apis.question.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import kr.co.pennyway.domain.domains.question.domain.Question;
 import kr.co.pennyway.domain.domains.question.domain.QuestionCategory;
@@ -10,6 +11,7 @@ public class QuestionReq {
     public record General(
             @Schema(description = "믄의자 이메일", example = "foobar@gmail.com")
             @NotBlank(message = "이메일을 입력해주세요")
+            @Email(message = "이메일 형식이 올바르지 않습니다.")
             String email,
             @Schema(description = "문의 내용", example = "문의 내용입니다.")
             @NotBlank(message = "문의 내용을 입력해주세요")
