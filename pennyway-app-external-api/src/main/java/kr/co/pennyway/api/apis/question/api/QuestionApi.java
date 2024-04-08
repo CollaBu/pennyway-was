f1c01cba9d48e39eda9da2f023c2ce7109476a0b
DinoDeveloper <asn6878@gmail.com>
2024-04-08 16:31:50 +0900

    docs: swagger parameter 제거 및 schema 내용 수정

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/question/api/QuestionApi.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/question/api/QuestionApi.java
@@ -1,13 +1,6 @@
 package kr.co.pennyway.api.apis.question.api;
 
-import io.lettuce.core.dynamic.annotation.Param;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.co.pennyway.api.apis.question.dto.QuestionReq;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +11,5 @@ import org.springframework.web.bind.annotation.RequestBody;
 public interface QuestionApi {
 
     @Operation(summary = "문의 전송", description = "사용자는 관리자에게 문의 메일을 발송한다.")
-    @Parameter(name = "email", description = "문의자 이메일")
-    @Parameter(name = "content", description = "문의 내용")
-    @Parameter(name = "category", description = "문의 카테고리" , examples = {
-            @ExampleObject(name = "이용 관련", value = "UTILIZATION"), @ExampleObject(name = "오류 신고", value = "BUG_REPORT"), @ExampleObject(name = "서비스 제안", value = "SUGGESTION"), @ExampleObject(name = "기타", value = "ETC")
-    }, required = true, in = ParameterIn.QUERY)
-
     ResponseEntity<?> sendQuestion(@RequestBody @Validated QuestionReq.General request);
 }
--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/question/dto/QuestionReq.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/question/dto/QuestionReq.java
@@ -5,14 +5,13 @@ import kr.co.pennyway.domain.domains.question.domain.Question;
 import kr.co.pennyway.domain.domains.question.domain.QuestionCategory;
 
 public class QuestionReq {
-
-    @Schema(name = "QuestionReqGeneral", title = "문의 요청")
+    @Schema(name = "QuestionReqGeneral", title = "문의 요청", allowableValues = {"UTILIZATION", "BUG_REPORT", "SUGGESTION", "ETC"})
     public record General(
             String email,
             String content,
             QuestionCategory category
-    ){
-        public Question toEntity(){
+    ) {
+        public Question toEntity() {
             return Question.builder()
                     .email(email)
                     .content(content)
