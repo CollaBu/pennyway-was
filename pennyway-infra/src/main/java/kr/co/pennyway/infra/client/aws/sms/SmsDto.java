ec15dfdeb80c9c41af81a5ea1e7cacf36b93c774
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-09 13:44:37 +0900

    fix: sms request record 제거

--- pennyway-infra/src/main/java/kr/co/pennyway/infra/client/aws/sms/SmsDto.java
+++ pennyway-infra/src/main/java/kr/co/pennyway/infra/client/aws/sms/SmsDto.java
@@ -3,7 +3,6 @@ package kr.co.pennyway.infra.client.aws.sms;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 // FIXME: Naver Cloud Platform Snes 기준 DTO. AWS SNS 요청, 응답 포맷에 맞게 수정 필요
 public class SmsDto {
@@ -19,37 +18,6 @@ public class SmsDto {
 
         public String parsePhone() {
             return "+82" + phone.replaceAll("-", "");
-        }
-    }
-
-    @Builder
-    public record Request(
-            String type,
-            String contentType,
-            String countryCode,
-            String from,
-            String content,
-            List<To> messages
-    ) {
-        /**
-         * AWS SNS API 요청 객체 생성
-         *
-         * @param type        String : SMS | LMS | MMS
-         * @param contentType String : COMM | AD
-         * @param countryCode String : 국가번호
-         * @param from        String : 발신번호
-         * @param content     String : 메시지 내용
-         * @param messages    List<{@link To}> : 메시지 정보 (to: 수신번호, subject: 개별 메시지 제목, content: 개별 메시지 내용)
-         */
-        public static Request of(String type, String contentType, String countryCode, String from, String content, List<To> messages) {
-            return Request.builder()
-                    .type(type)
-                    .contentType(contentType)
-                    .countryCode(countryCode)
-                    .from(from)
-                    .content(content)
-                    .messages(messages)
-                    .build();
         }
     }
 
