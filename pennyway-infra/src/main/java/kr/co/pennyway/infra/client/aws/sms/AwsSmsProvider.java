e000d6e22ea8f4a5218226fcbdf2018a044400bc
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-09 19:27:47 +0900

    ✨ 인증코드 SMS 전송 (#37)
    * chore: infra 모듈 내 aws sdk, sns 의존성 주입
    
    * chore: aws sns 환경변수 설정
    
    * chore: aws sns config 설정
    
    * feat: sms dto에서 sms 전송을 위한 전화번호 파싱 메서드 추가
    
    * rename: pheon 파싱 메서드명 수정
    
    * fix: sms request record 제거
    
    * fix: sms dto response 객체 제거 및 불필요한 필드 정보 제거
    
    * feat: phone 인증 event 등록
    
    * fix: sms dto to 레코드명 변경 및 code 필드 추가
    
    * rename: phone verification event -> push code event
    
    * feat: push code 이벤트 핸들러 등록
    
    * rename: event명 변경으로 인한 수정
    
    * fix: 인증 코드 생성 로직 sms provider -> mapper로 변경
    
    * chore: infra 환경 설정 기본값 null 제거 -> 더미값 주입

--- pennyway-infra/src/main/java/kr/co/pennyway/infra/client/aws/sms/AwsSmsProvider.java
+++ pennyway-infra/src/main/java/kr/co/pennyway/infra/client/aws/sms/AwsSmsProvider.java
@@ -3,28 +3,37 @@ package kr.co.pennyway.infra.client.aws.sms;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
+import software.amazon.awssdk.services.sns.model.PublishResponse;
+import software.amazon.awssdk.services.sns.model.SnsException;
 
 import java.time.LocalDateTime;
-import java.util.concurrent.ThreadLocalRandom;
 
-// TODO: AWS SNS 인프라 설정 후 내부 구현
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class AwsSmsProvider implements SmsProvider {
+    private final SnsClient snsClient;
+
     @Override
-    public SmsDto.Info sendCode(SmsDto.To dto) {
-        String code = issueVerificationCode();
-        SmsDto.Response response = SmsDto.Response.builder().requestAt(LocalDateTime.now()).build();
-        return SmsDto.Info.from(response, code);
+    public SmsDto.Info sendCode(SmsDto.Request dto) {
+        PublishResponse response = publishCodeSms(dto.parsePhone(), dto.code());
+        
+        return SmsDto.Info.of(response.messageId(), dto.code(), LocalDateTime.now());
     }
 
-    private String issueVerificationCode() {
-        StringBuilder sb = new StringBuilder();
+    private PublishResponse publishCodeSms(String phone, String code) {
+        PublishRequest request = PublishRequest.builder()
+                .message("[Pennyway] 인증번호 [" + code + "]를 입력해주세요.")
+                .phoneNumber(phone)
+                .build();
 
-        for (int i = 0; i < 6; i++) {
-            sb.append(ThreadLocalRandom.current().nextInt(0, 10));
+        try {
+            return snsClient.publish(request);
+        } catch (SnsException e) {
+            log.error("SMS 전송 실패: {}", e.getMessage());
+            throw new RuntimeException("SMS 전송에 실패했습니다.");
         }
-        return sb.toString();
     }
 }
--- pennyway-infra/src/main/java/kr/co/pennyway/infra/client/aws/sms/SmsProvider.java
+++ pennyway-infra/src/main/java/kr/co/pennyway/infra/client/aws/sms/SmsProvider.java
@@ -4,8 +4,8 @@ public interface SmsProvider {
     /**
      * 인증번호를 수신자에게 SMS로 전송
      *
-     * @param dto {@link SmsDto.To} : 수신자 번호
+     * @param dto {@link SmsDto.Request} : 수신자 번호
      * @return {@link SmsDto.Info} : SNS 전송 정보
      */
-    SmsDto.Info sendCode(SmsDto.To dto);
+    SmsDto.Info sendCode(SmsDto.Request dto);
 }
