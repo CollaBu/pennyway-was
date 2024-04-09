889fbe4db9012c631d6b9fb8e65b8937c2cac8b3
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-09 14:03:32 +0900

    fix: sms dto response 객체 제거 및 불필요한 필드 정보 제거

--- pennyway-infra/src/main/java/kr/co/pennyway/infra/client/aws/sms/SmsDto.java
+++ pennyway-infra/src/main/java/kr/co/pennyway/infra/client/aws/sms/SmsDto.java
@@ -3,6 +3,7 @@ package kr.co.pennyway.infra.client.aws.sms;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 // FIXME: Naver Cloud Platform Snes 기준 DTO. AWS SNS 요청, 응답 포맷에 맞게 수정 필요
 public class SmsDto {
@@ -24,49 +25,23 @@ public class SmsDto {
     /**
      * AWS SNS API 요청에 대한 응답 객체
      *
-     * @param requestId  String : 요청 ID
-     * @param requestAt  LocalDateTime : 요청 시간
-     * @param statusCode String : 응답 코드
-     * @param statusName String : 응답 상태
-     */
-    @Builder
-    public record Response(
-            String requestId,
-            LocalDateTime requestAt,
-            String statusCode,
-            String statusName
-    ) {
-    }
-
-    /**
-     * 인증번호 전송 정보를 확인할 수 있는 DTO
-     *
-     * @param requestId  String : 요청 ID (NCP SMS API 요청 시 발급된 요청 ID)
-     * @param code       String : 발급된 인증번호 정수 6자리 문자열
-     * @param requestAt  LocalDateTime : 요청 시간
-     * @param statusCode String : 응답 코드
-     * @param statusName String : 응답 상태
+     * @param requestId String : 요청 ID
+     * @param requestAt LocalDateTime : 요청 시간
      */
     @Builder
     public record Info(
             String requestId,
             String code,
-            LocalDateTime requestAt,
-            String statusCode,
-            String statusName
+            LocalDateTime requestAt
     ) {
-        /**
-         * @param request {@link Response}
-         * @param code    String : 인증 코드 정수 6자리 문자열
-         */
-        public static Info from(Response request, String code) {
-            return Info.builder()
-                    .requestId(request.requestId())
-                    .code(code)
-                    .requestAt(request.requestAt())
-                    .statusCode(request.statusCode())
-                    .statusName(request.statusName())
-                    .build();
+        public Info {
+            Objects.requireNonNull(requestId);
+            Objects.requireNonNull(code);
+            Objects.requireNonNull(requestAt);
+        }
+
+        public static Info of(String requestId, String code, LocalDateTime requestAt) {
+            return new Info(requestId, code, requestAt);
         }
     }
 }
