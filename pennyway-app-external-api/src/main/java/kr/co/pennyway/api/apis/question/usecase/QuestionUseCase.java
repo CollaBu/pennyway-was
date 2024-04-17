646b3d246743a04979d0bb9d79810f3354cc5fd0
DinoDeveloper <asn6878@gmail.com>
2024-04-17 14:31:53 +0900

    refactor: transactionaleventlistener를 활용한 메일발송 이벤트처리

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/question/usecase/QuestionUseCase.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/question/usecase/QuestionUseCase.java
@@ -5,23 +5,23 @@ import kr.co.pennyway.api.apis.question.dto.QuestionReq;
 import kr.co.pennyway.common.annotation.UseCase;
 import kr.co.pennyway.domain.domains.question.domain.Question;
 import kr.co.pennyway.domain.domains.question.service.QuestionService;
-import kr.co.pennyway.infra.client.google.mail.GoogleMailSender;
+import kr.co.pennyway.infra.common.event.MailEvent;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 
 @Slf4j
 @UseCase
 @AllArgsConstructor
 public class QuestionUseCase {
     private final QuestionService questionService;
-    private final GoogleMailSender googleMailSender;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
     @Transactional
     public void sendQuestion(QuestionReq request) {
         Question question = request.toEntity();
 
         questionService.createQuestion(question);
-        googleMailSender.sendMail(request.email(), request.content(), request.category().getTitle());
-        // TODO : sendMail 메소드 이벤트 처리
+        applicationEventPublisher.publishEvent(MailEvent.of(request.email(), request.content(), request.category().getTitle()));
     }
 }
