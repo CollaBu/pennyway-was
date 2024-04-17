70c2b5f04b79c31164b3a1c81c354162efecd711
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-17 15:01:25 +0900

    fix: device repoistory 상속 대상을 cure -> list crud로 변경

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/device/repository/DeviceRepository.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/device/repository/DeviceRepository.java
@@ -1,7 +1,7 @@
 package kr.co.pennyway.domain.domains.device.repository;
 
 import kr.co.pennyway.domain.domains.device.domain.Device;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.ListCrudRepository;
 
-public interface DeviceRepository extends CrudRepository<Device, Long> {
+public interface DeviceRepository extends ListCrudRepository<Device, Long> {
 }
