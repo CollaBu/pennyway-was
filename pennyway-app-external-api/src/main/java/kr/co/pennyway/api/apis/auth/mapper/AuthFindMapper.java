2aa5e6bbd98d83740ab04c6c3fb4ba25e07a4eb3
Jinwoo Lee <binaryWooo@gmail.com>
2024-04-17 11:32:01 +0900

    fix: 일반 회원이 아닌 휴대폰 번호 조회 시 not-found 예외 처리

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/mapper/AuthFindMapper.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/mapper/AuthFindMapper.java
@@ -1,7 +1,12 @@
 package kr.co.pennyway.api.apis.auth.mapper;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import kr.co.pennyway.api.apis.auth.dto.AuthFindDto;
+import kr.co.pennyway.api.common.exception.AuthFindErrorCode;
+import kr.co.pennyway.api.common.exception.AuthFinderException;
 import kr.co.pennyway.common.annotation.Mapper;
+import kr.co.pennyway.domain.domains.user.domain.User;
 import kr.co.pennyway.domain.domains.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,10 +17,18 @@ import lombok.extern.slf4j.Slf4j;
 public class AuthFindMapper {
 	private final UserService userService;
 
+	@Transactional(readOnly = true)
 	public AuthFindDto.FindUsernameRes findUsername(String phone) {
-		return AuthFindDto.FindUsernameRes.of(userService.readUserByPhone(phone).orElseThrow(() -> {
+		User user = userService.readUserByPhone(phone).orElseThrow(() -> {
 			log.error("User not found by phone: {}", phone);
-			return new RuntimeException("User not found by phone: " + phone);
-		}));
+			return new AuthFinderException(AuthFindErrorCode.EXPIRED_OR_INVALID_PHONE);
+		});
+
+		if (user.getPassword() == null) {
+			log.error("User not found by phone: {}", phone);
+			throw new AuthFinderException(AuthFindErrorCode.EXPIRED_OR_INVALID_PHONE);
+		}
+
+		return AuthFindDto.FindUsernameRes.of(user);
 	}
 }
