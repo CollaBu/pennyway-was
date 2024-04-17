8af9d7fe8bf9cb1be96a86a1ccd065eaedd04fca
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-17 22:01:26 +0900

    feat: device custom exception 클래스 추가

---
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/device/exception/DeviceErrorCode.java
@@ -0,0 +1,28 @@
+package kr.co.pennyway.domain.domains.device.exception;
+
+import kr.co.pennyway.common.exception.BaseErrorCode;
+import kr.co.pennyway.common.exception.CausedBy;
+import kr.co.pennyway.common.exception.ReasonCode;
+import kr.co.pennyway.common.exception.StatusCode;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum DeviceErrorCode implements BaseErrorCode {
+    /* 400 BAD_REQUEST */
+    NOT_MATCH_DEVICE(StatusCode.BAD_REQUEST, ReasonCode.INVALID_REQUEST, "토큰의 디바이스 정보와 일치하지 않습니다."),
+    ;
+
+    private final StatusCode statusCode;
+    private final ReasonCode reasonCode;
+    private final String message;
+
+    @Override
+    public CausedBy causedBy() {
+        return CausedBy.of(statusCode, reasonCode);
+    }
+
+    @Override
+    public String getExplainError() throws NoSuchFieldError {
+        return message;
+    }
+}
