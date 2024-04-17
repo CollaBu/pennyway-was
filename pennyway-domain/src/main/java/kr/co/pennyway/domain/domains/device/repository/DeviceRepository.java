da09518b64b8df5491fd6e6b6e48d868ce6e67cf
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-17 15:03:48 +0900

    feat: read_all_by_user_id() 메서드 repository 내 선언

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/device/repository/DeviceRepository.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/device/repository/DeviceRepository.java
@@ -3,5 +3,8 @@ package kr.co.pennyway.domain.domains.device.repository;
 import kr.co.pennyway.domain.domains.device.domain.Device;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface DeviceRepository extends ListCrudRepository<Device, Long> {
+    List<Device> findAllByUser_Id(Long userId);
 }
