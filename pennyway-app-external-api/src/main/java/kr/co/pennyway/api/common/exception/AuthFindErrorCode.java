822a4f2f1499e6d944c9ea9924f2c3544c81f392
Jinwoo Lee <binaryWooo@gmail.com>
2024-04-17 11:11:58 +0900

    feat: 존재하지 않는 아이디 error-code 정의

---
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/common/exception/AuthFindErrorCode.java
@@ -0,0 +1,29 @@
+package kr.co.pennyway.api.common.exception;
+
+import kr.co.pennyway.common.exception.BaseErrorCode;
+import kr.co.pennyway.common.exception.CausedBy;
+import kr.co.pennyway.common.exception.ReasonCode;
+import kr.co.pennyway.common.exception.StatusCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum AuthFindErrorCode implements BaseErrorCode {
+	// 404 Not Found
+	EXPIRED_OR_INVALID_PHONE(StatusCode.NOT_FOUND, ReasonCode.REQUESTED_RESOURCE_NOT_FOUND, "일반 회원으로 가입되지 않은 휴대폰 정보입니다.");
+
+	private final StatusCode statusCode;
+	private final ReasonCode reasonCode;
+	private final String message;
+
+	@Override
+	public CausedBy causedBy() {
+		return CausedBy.of(statusCode, reasonCode);
+	}
+
+	@Override
+	public String getExplainError() throws NoSuchFieldError {
+		return null;
+	}
+}
