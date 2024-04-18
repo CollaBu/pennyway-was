11b1394a2522c6d4459bb3b7cc11b9cbe7d22e72
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-18 17:01:12 +0900

    fix: 디바이스 정보 제거 시, 매칭되는 디바이스가 없을 때 예외 종류 수정

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/users/usecase/UserAccountUseCase.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/users/usecase/UserAccountUseCase.java
@@ -4,6 +4,9 @@ import kr.co.pennyway.api.apis.users.dto.DeviceDto;
 import kr.co.pennyway.api.apis.users.service.DeviceRegisterService;
 import kr.co.pennyway.common.annotation.UseCase;
 import kr.co.pennyway.domain.domains.device.domain.Device;
+import kr.co.pennyway.domain.domains.device.exception.DeviceErrorCode;
+import kr.co.pennyway.domain.domains.device.exception.DeviceErrorException;
+import kr.co.pennyway.domain.domains.device.service.DeviceService;
 import kr.co.pennyway.domain.domains.user.domain.User;
 import kr.co.pennyway.domain.domains.user.exception.UserErrorCode;
 import kr.co.pennyway.domain.domains.user.exception.UserErrorException;
@@ -17,6 +20,8 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserAccountUseCase {
     private final UserService userService;
+    private final DeviceService deviceService;
+
     private final DeviceRegisterService deviceRegisterService;
 
     @Transactional
@@ -31,7 +36,15 @@ public class UserAccountUseCase {
     }
 
     @Transactional
-    public void unregisterDevice(Long userId, Long deviceId) {
+    public void unregisterDevice(Long userId, String token) {
+        User user = userService.readUser(userId).orElseThrow(
+                () -> new UserErrorException(UserErrorCode.NOT_FOUND)
+        );
 
+        Device device = deviceService.readDeviceByUserIdAndToken(user.getId(), token).orElseThrow(
+                () -> new DeviceErrorException(DeviceErrorCode.NOT_FOUND_DEVICE)
+        );
+
+        deviceService.deleteDevice(device);
     }
 }
