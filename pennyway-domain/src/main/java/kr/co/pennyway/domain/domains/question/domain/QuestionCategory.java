9eae3eea523b76149eae0c77b0259d481e6c24a7
DinoDeveloper <asn6878@gmail.com>
2024-04-19 23:30:14 +0900

    fix: 불필요한 getter 제거

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/question/domain/QuestionCategory.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/question/domain/QuestionCategory.java
@@ -9,16 +9,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public enum QuestionCategory implements LegacyCommonType {
-    UTILIZATION("1","이용 관련"),
-    BUG_REPORT("2","오류 신고"),
-    SUGGESTION("3","서비스 제안"),
-    ETC("4","기타");
+    UTILIZATION("1", "이용 관련"),
+    BUG_REPORT("2", "오류 신고"),
+    SUGGESTION("3", "서비스 제안"),
+    ETC("4", "기타");
 
     private final String code;
     private final String title;
-
-    @Override
-    public String getCode(){
-        return code;
-    }
 }
