bb2358d2b99a925d139b36be2385119eb16bae1b
DinoDeveloper <asn6878@gmail.com>
2024-04-20 18:00:08 +0900

    feat: swagger 성공 응답 명시

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/question/api/QuestionApi.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/question/api/QuestionApi.java
@@ -1,6 +1,9 @@
 package kr.co.pennyway.api.apis.question.api;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.co.pennyway.api.apis.question.dto.QuestionReq;
 import org.springframework.http.ResponseEntity;
@@ -11,5 +14,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 public interface QuestionApi {
 
     @Operation(summary = "문의 전송", description = "사용자는 관리자에게 문의 메일을 발송한다.")
+    @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", examples = {
+            @ExampleObject(name = "발신 성공", value = """
+                    {
+                        "code": "2000",
+                        "data": {}
+                    }
+                    """)
+    }))
     ResponseEntity<?> sendQuestion(@RequestBody @Validated QuestionReq request);
 }
