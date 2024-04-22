9db7e7657574b15d2513941bb11bb791f8e4cf67
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-22 17:32:42 +0900

    test: pre-condition 설정

--- pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/auth/usecase/UserAuthUseCaseUnitTest.java
+++ pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/auth/usecase/UserAuthUseCaseUnitTest.java
@@ -1,17 +1,32 @@
 package kr.co.pennyway.api.apis.auth.usecase;
 
+import kr.co.pennyway.api.apis.auth.helper.JwtAuthHelper;
+import kr.co.pennyway.api.common.security.jwt.access.AccessTokenClaim;
+import kr.co.pennyway.api.common.security.jwt.access.AccessTokenProvider;
+import kr.co.pennyway.infra.common.jwt.JwtClaims;
+import kr.co.pennyway.infra.common.jwt.JwtProvider;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.time.Duration;
 
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles("test")
 public class UserAuthUseCaseUnitTest {
+    private final String secretStr = "helloMyNameIsPennywayThisIsSecretKeyItNeedsToBeLongerThan256Bits";
+    private JwtProvider accessTokenProvider;
+    private JwtClaims jwtClaims;
     private UserAuthUseCase userAuthUseCase;
+    @Mock
+    private JwtAuthHelper jwtAuthHelper;
 
     @BeforeEach
     public void setUp() {
-        userAuthUseCase = new UserAuthUseCase();
+        accessTokenProvider = new AccessTokenProvider(secretStr, Duration.ofMinutes(5));
+        jwtClaims = AccessTokenClaim.of(1L, "ROLE_USER");
+        userAuthUseCase = new UserAuthUseCase(jwtAuthHelper, accessTokenProvider);
     }
 }
