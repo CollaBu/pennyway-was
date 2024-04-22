4e10e2dbc4235fd7f22e4dc4ac9551fe94aee757
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-22 17:45:27 +0900

    test: 토큰 유효성 검사 api 4가지 시나리오 작성

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/usecase/UserAuthUseCase.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/usecase/UserAuthUseCase.java
@@ -2,6 +2,7 @@ package kr.co.pennyway.api.apis.auth.usecase;
 
 import kr.co.pennyway.api.apis.auth.helper.JwtAuthHelper;
 import kr.co.pennyway.common.annotation.UseCase;
+import kr.co.pennyway.infra.common.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,6 +11,11 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class UserAuthUseCase {
     private final JwtAuthHelper jwtAuthHelper;
+    private final JwtProvider accessTokenProvider;
+
+    public boolean isSignedIn(String accessToken) {
+
+    }
 
     public void signOut(Long userId, String authHeader, String refreshToken) {
         jwtAuthHelper.removeAccessTokenAndRefreshToken(userId, authHeader, refreshToken);
--- pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/auth/usecase/UserAuthUseCaseUnitTest.java
+++ pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/auth/usecase/UserAuthUseCaseUnitTest.java
@@ -3,15 +3,21 @@ package kr.co.pennyway.api.apis.auth.usecase;
 import kr.co.pennyway.api.apis.auth.helper.JwtAuthHelper;
 import kr.co.pennyway.api.common.security.jwt.access.AccessTokenClaim;
 import kr.co.pennyway.api.common.security.jwt.access.AccessTokenProvider;
+import kr.co.pennyway.infra.common.exception.JwtErrorCode;
+import kr.co.pennyway.infra.common.exception.JwtErrorException;
 import kr.co.pennyway.infra.common.jwt.JwtClaims;
 import kr.co.pennyway.infra.common.jwt.JwtProvider;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles("test")
@@ -28,5 +34,51 @@ public class UserAuthUseCaseUnitTest {
         accessTokenProvider = new AccessTokenProvider(secretStr, Duration.ofMinutes(5));
         jwtClaims = AccessTokenClaim.of(1L, "ROLE_USER");
         userAuthUseCase = new UserAuthUseCase(jwtAuthHelper, accessTokenProvider);
+    }
+
+    @Test
+    @DisplayName("[1] Authorication 헤더가 없으면 false를 반환한다.")
+    public void isSignedInWithoutAuthorizationHeader() {
+        // when
+        boolean result = userAuthUseCase.isSignedIn(null);
+
+        // then
+        assertFalse(result);
+    }
+
+    @Test
+    @DisplayName("[2] 유효한 토큰이 아니면 false를 반환한다.")
+    public void isSignedInWithInvalidToken() {
+        // when
+        boolean result = userAuthUseCase.isSignedIn("Bearer invalidToken");
+
+        // then
+        assertFalse(result);
+    }
+
+    @Test
+    @DisplayName("[2-1] 유효하지 않으면서 만료된 토큰인 경우 ExpiredToken 예외를 던진다.")
+    public void isSignedInWithExpiredToken() {
+        // given
+        accessTokenProvider = new AccessTokenProvider(secretStr, Duration.ofSeconds(1));
+        userAuthUseCase = new UserAuthUseCase(jwtAuthHelper, accessTokenProvider);
+        String expiredToken = accessTokenProvider.generateToken(jwtClaims);
+
+        // when
+        JwtErrorException exception = assertThrows(JwtErrorException.class, () -> userAuthUseCase.isSignedIn("Bearer " + expiredToken));
+        assertEquals(JwtErrorCode.EXPIRED_TOKEN, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("[3] 유효한 토큰이면 true를 반환한다.")
+    public void isSignedInWithValidToken() {
+        // given
+        String token = accessTokenProvider.generateToken(jwtClaims);
+
+        // when
+        boolean result = userAuthUseCase.isSignedIn("Bearer " + token);
+
+        // then
+        assertFalse(result);
     }
 }
