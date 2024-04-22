aa809d5c0d36869bb931ac65bb96cb65c147b2ee
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-22 17:55:35 +0900

    feat: is_sign_in 구현

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/usecase/UserAuthUseCase.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/usecase/UserAuthUseCase.java
@@ -2,6 +2,8 @@ package kr.co.pennyway.api.apis.auth.usecase;
 
 import kr.co.pennyway.api.apis.auth.helper.JwtAuthHelper;
 import kr.co.pennyway.common.annotation.UseCase;
+import kr.co.pennyway.infra.common.exception.JwtErrorCode;
+import kr.co.pennyway.infra.common.exception.JwtErrorException;
 import kr.co.pennyway.infra.common.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,7 +16,10 @@ public class UserAuthUseCase {
     private final JwtProvider accessTokenProvider;
 
     public boolean isSignedIn(String accessToken) {
+        if (accessTokenProvider.isTokenExpired(accessToken))
+            throw new JwtErrorException(JwtErrorCode.EXPIRED_TOKEN);
 
+        return true;
     }
 
     public void signOut(Long userId, String authHeader, String refreshToken) {
