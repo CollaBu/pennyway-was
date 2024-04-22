2ddd8fe0121e7bbd2be5248ebdcab033d1fa196c
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-22 18:44:43 +0900

    test: access token을 보낸 의도가 확실할 때는 검증 실패 시 예외처리

--- pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/auth/usecase/UserAuthUseCaseUnitTest.java
+++ pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/auth/usecase/UserAuthUseCaseUnitTest.java
@@ -47,25 +47,27 @@ public class UserAuthUseCaseUnitTest {
     }
 
     @Test
-    @DisplayName("[2] 유효한 토큰이 아니면 false를 반환한다.")
+    @DisplayName("[2] 유효한 토큰이 아니면 예외를 반환한다.")
     public void isSignedInWithInvalidToken() {
         // when
-        boolean result = userAuthUseCase.isSignIn("Bearer invalidToken");
+        JwtErrorException exception = assertThrows(JwtErrorException.class, () -> userAuthUseCase.isSignIn("Bearer invalidToken"));
 
         // then
-        assertFalse(result);
+        System.out.println(exception.getErrorCode());
     }
 
     @Test
     @DisplayName("[2-1] 유효하지 않으면서 만료된 토큰인 경우 ExpiredToken 예외를 던진다.")
     public void isSignedInWithExpiredToken() {
         // given
-        accessTokenProvider = new AccessTokenProvider(secretStr, Duration.ofSeconds(1));
+        accessTokenProvider = new AccessTokenProvider(secretStr, Duration.ofMillis(0));
         userAuthUseCase = new UserAuthUseCase(jwtAuthHelper, accessTokenProvider);
         String expiredToken = accessTokenProvider.generateToken(jwtClaims);
 
         // when
         JwtErrorException exception = assertThrows(JwtErrorException.class, () -> userAuthUseCase.isSignIn("Bearer " + expiredToken));
+
+        // then
         assertEquals(JwtErrorCode.EXPIRED_TOKEN, exception.getErrorCode());
     }
 
@@ -79,6 +81,6 @@ public class UserAuthUseCaseUnitTest {
         boolean result = userAuthUseCase.isSignIn("Bearer " + token);
 
         // then
-        assertFalse(result);
+        assertTrue(result);
     }
 }
