110532e308bae5b1e9c1619e5fa2119c84597519
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-23 14:26:52 +0900

    test: jwt_auth_helper 실제 인스턴스 주입

--- pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/auth/usecase/UserAuthUseCaseUnitTest.java
+++ pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/auth/usecase/UserAuthUseCaseUnitTest.java
@@ -4,6 +4,8 @@ import kr.co.pennyway.api.apis.auth.dto.AuthStateDto;
 import kr.co.pennyway.api.apis.auth.helper.JwtAuthHelper;
 import kr.co.pennyway.api.common.security.jwt.access.AccessTokenClaim;
 import kr.co.pennyway.api.common.security.jwt.access.AccessTokenProvider;
+import kr.co.pennyway.domain.common.redis.forbidden.ForbiddenTokenService;
+import kr.co.pennyway.domain.common.redis.refresh.RefreshTokenService;
 import kr.co.pennyway.infra.common.exception.JwtErrorCode;
 import kr.co.pennyway.infra.common.exception.JwtErrorException;
 import kr.co.pennyway.infra.common.jwt.JwtClaims;
@@ -20,8 +22,6 @@ import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles("test")
@@ -30,14 +30,20 @@ public class UserAuthUseCaseUnitTest {
     private final JwtClaims jwtClaims = AccessTokenClaim.of(1L, "ROLE_USER");
     private JwtProvider accessTokenProvider;
     private UserAuthUseCase userAuthUseCase;
-    @Mock
     private JwtAuthHelper jwtAuthHelper;
+
+    @Mock
+    private JwtProvider refreshTokenProvider;
+    @Mock
+    private RefreshTokenService refreshTokenService;
+    @Mock
+    private ForbiddenTokenService forbiddenTokenService;
 
     @BeforeEach
     public void setUp() {
         accessTokenProvider = new AccessTokenProvider(secretStr, Duration.ofMinutes(5));
+        jwtAuthHelper = new JwtAuthHelper(accessTokenProvider, refreshTokenProvider, refreshTokenService, forbiddenTokenService);
         userAuthUseCase = new UserAuthUseCase(jwtAuthHelper, accessTokenProvider);
-
     }
 
     @Test
@@ -60,7 +66,6 @@ public class UserAuthUseCaseUnitTest {
     public void isSignedInWithValidToken() {
         // given
         String token = accessTokenProvider.generateToken(jwtClaims);
-        given(jwtAuthHelper.getClaimValue(any(), any(), any())).willReturn(1L);
 
         // when
         AuthStateDto result = userAuthUseCase.isSignIn("Bearer " + token);
