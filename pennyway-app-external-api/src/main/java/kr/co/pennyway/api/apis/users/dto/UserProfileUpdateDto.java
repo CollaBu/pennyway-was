814bccdc61a897acd6530c689d2409eb40ce9aa8
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-25 20:49:47 +0900

    feat: 사용자 아이디 변경 요청 dto 작성

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/users/dto/UserProfileUpdateDto.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/users/dto/UserProfileUpdateDto.java
@@ -13,4 +13,13 @@ public class UserProfileUpdateDto {
             String name
     ) {
     }
+
+    @Schema(title = "아이디 변경 요청 DTO")
+    public record UsernameReq(
+            @Schema(description = "아이디", example = "pennyway")
+            @NotBlank(message = "아이디를 입력해주세요")
+            @Pattern(regexp = "^[a-z-_.]{5,20}$", message = "5~20자의 영문 소문자, -, _, . 만 사용 가능합니다.")
+            String username
+    ) {
+    }
 }
