39ba31509c4ee63b0c60b44a5aa497dfa972ed25
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-26 18:18:34 +0900

    feat: 비밀번호 변경 요청 dto 정의

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/users/dto/UserProfileUpdateDto.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/users/dto/UserProfileUpdateDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import kr.co.pennyway.api.common.validator.Password;
 import kr.co.pennyway.domain.domains.user.domain.NotifySetting;
 
 public class UserProfileUpdateDto {
@@ -30,6 +31,18 @@ public class UserProfileUpdateDto {
             @Schema(description = "현재 비밀번호", example = "password")
             @NotBlank(message = "비밀번호를 입력해주세요")
             String password
+    ) {
+    }
+
+    @Schema(title = "비밀번호 변경 요청 DTO")
+    public record PasswordReq(
+            @Schema(description = "현재 비밀번호", example = "password")
+            @NotBlank(message = "현재 비밀번호를 입력해주세요")
+            String oldPassword,
+            @Schema(description = "새 비밀번호", example = "newPassword")
+            @NotBlank(message = "새 비밀번호를 입력해주세요")
+            @Password(message = "8~16자의 영문 대/소문자, 숫자, 특수문자를 사용해주세요. (적어도 하나의 영문 소문자, 숫자 포함)")
+            String newPassword
     ) {
     }
 
