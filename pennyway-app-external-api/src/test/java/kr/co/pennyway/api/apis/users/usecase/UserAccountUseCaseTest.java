fda1a2a3ba92bce276ed915270c4b15b50e95dab
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-26 18:55:31 +0900

    test: 비밀번호 변경 usecase unit test 작성

--- pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/users/usecase/UserAccountUseCaseTest.java
+++ pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/users/usecase/UserAccountUseCaseTest.java
@@ -375,7 +375,7 @@ class UserAccountUseCaseTest extends ExternalApiDBTestConfig {
 
         @Test
         @Transactional
-        @DisplayName("현재 비밀번호와 동일한 비밀번호로 변경할 수 없는 경우 CLIENT_ERROR 에러를 반환한다.")
+        @DisplayName("현재 비밀번호와 동일한 비밀번호로 변경을 시도하는 경우 CLIENT_ERROR 에러를 반환한다.")
         void updatePasswordWhenSamePassword() {
             // given
             User originUser = UserFixture.GENERAL_USER.toUser();
@@ -388,15 +388,57 @@ class UserAccountUseCaseTest extends ExternalApiDBTestConfig {
 
         @Test
         @Transactional
-        @DisplayName("비밀번호가 일치하는 경우 정상적으로 처리된다.")
+        @DisplayName("oldPassword와 newPassword가 일치하는 경우, CLIENT_ERROR 에러를 반환한다.")
+        void updatePasswordWhenOldPasswordAndNewPasswordIsSame() {
+            // given
+            User originUser = UserFixture.GENERAL_USER.toUser();
+            userService.createUser(originUser);
+
+            // when - then
+            UserErrorException ex = assertThrows(UserErrorException.class, () -> userAccountUseCase.updatePassword(originUser.getId(), originUser.getPassword(), originUser.getPassword()));
+            assertEquals("oldPassword와 newPassword가 일치하는 경우 Client Error를 반환한다.", UserErrorCode.PASSWORD_NOT_CHANGED, ex.getBaseErrorCode());
+        }
+
+        @Test
+        @Transactional
+        @DisplayName("비밀번호가 다른 경우 NOT_MATCHED_PASSWORD 에러를 반환한다.")
+        void updatePasswordWhenPasswordIsNotMatched() {
+            // given
+            User originUser = UserFixture.GENERAL_USER.toUser();
+            userService.createUser(originUser);
+            given(passwordEncoderHelper.isSamePassword(any(), any())).willReturn(false);
+
+            // when - then
+            UserErrorException ex = assertThrows(UserErrorException.class, () -> userAccountUseCase.updatePassword(originUser.getId(), "notMatchedPassword", "newPassword"));
+            assertEquals("비밀번호가 다른 경우 Not Matched Password를 반환한다.", UserErrorCode.NOT_MATCHED_PASSWORD, ex.getBaseErrorCode());
+        }
+
+        @Test
+        @Transactional
+        @DisplayName("사용자가 일반 회원가입 이력이 없는 소셜 계정인 경우, DO_NOT_GENERAL_SIGNED_UP 에러를 반환한다.")
+        void updatePasswordWhenUserIsNotGeneralSignedUp() {
+            // given
+            User originUser = UserFixture.OAUTH_USER.toUser();
+            userService.createUser(originUser);
+
+            // when - then
+            UserErrorException ex = assertThrows(UserErrorException.class, () -> userAccountUseCase.updatePassword(originUser.getId(), originUser.getPassword(), "newPassword"));
+            assertEquals("일반 회원가입 이력이 없는 경우 Do Not General Signed Up을 반환한다.", UserErrorCode.DO_NOT_GENERAL_SIGNED_UP, ex.getBaseErrorCode());
+        }
+
+        @Test
+        @Transactional
+        @DisplayName("정상적인 요청인 경우 비밀번호가 정상적으로 변경된다.")
         void updatePassword() {
             // given
             User originUser = UserFixture.GENERAL_USER.toUser();
             userService.createUser(originUser);
             given(passwordEncoderHelper.isSamePassword(any(), any())).willReturn(true);
+            given(passwordEncoderHelper.encodePassword(any())).willReturn("encodedPassword");
 
             // when - then
             assertDoesNotThrow(() -> userAccountUseCase.updatePassword(originUser.getId(), originUser.getPassword(), "newPassword"));
+            assertEquals("비밀번호가 정상적으로 변경되어 있어야 한다.", "encodedPassword", userService.readUser(originUser.getId()).orElseThrow().getPassword());
         }
     }
 }
