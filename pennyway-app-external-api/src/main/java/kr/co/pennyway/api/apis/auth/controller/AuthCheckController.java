fd9d507407772860a8a8706ef9432762bb122ef8
Jinwoo Lee <binaryWooo@gmail.com>
2024-04-26 16:53:31 +0900

    fix: 아이디 찾기 api의 query-string 유효성 검증을 위한 인자 수정

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/controller/AuthCheckController.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/controller/AuthCheckController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.validation.constraints.NotBlank;
 import kr.co.pennyway.api.apis.auth.api.AuthCheckApi;
+import kr.co.pennyway.api.apis.auth.dto.PhoneVerificationDto;
 import kr.co.pennyway.api.apis.auth.usecase.AuthCheckUseCase;
 import kr.co.pennyway.api.common.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
@@ -32,7 +32,7 @@ public class AuthCheckController implements AuthCheckApi {
 
 	@GetMapping("/find/username")
 	@PreAuthorize("isAnonymous()")
-	public ResponseEntity<?> findUsername(@RequestParam @NotBlank String phone, @RequestParam @NotBlank String code) {
-		return ResponseEntity.ok(SuccessResponse.from("user", authCheckUseCase.findUsername(phone, code)));
+	public ResponseEntity<?> findUsername(@Validated PhoneVerificationDto.VerifyCodeReq request) {
+		return ResponseEntity.ok(SuccessResponse.from("user", authCheckUseCase.findUsername(request)));
 	}
 }
