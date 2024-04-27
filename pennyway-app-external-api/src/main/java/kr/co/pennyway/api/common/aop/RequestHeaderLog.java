5a96e52858a8b7327cc739a814c73b6a152812da
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-27 11:48:17 +0900

    fix: request header log 어노테이션 인증 헤더 제거

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/common/aop/RequestHeaderLog.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/common/aop/RequestHeaderLog.java
@@ -6,12 +6,9 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface RequestHeaderLog {
-    boolean hasAuthorization() default true;
-
-    boolean hasCookie() default false;
-
-    enum Type {
-        AUTHORIZATION,
-        COOKIE
-    }
+    /**
+     * 요청 헤더에 쿠키가 포함되어 있는 지 여부 <br/>
+     * {@link org.springframework.web.bind.annotation.CookieValue}를 사용하는 컨트롤러에서는 반드시 해당 어노테이션을 명시해야 한다.
+     */
+    boolean hasCookie() default true;
 }
