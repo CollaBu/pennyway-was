6ef00d694841090e224ceeae4ee611c31b11dc7f
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-29 19:20:27 +0900

    test: refresh token service 단위 테스트 작성

--- pennyway-domain/src/test/java/kr/co/pennyway/domain/common/redis/refresh/RefreshTokenServiceUnitTest.java
+++ pennyway-domain/src/test/java/kr/co/pennyway/domain/common/redis/refresh/RefreshTokenServiceUnitTest.java
@@ -13,6 +13,8 @@ import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
 @Slf4j
 @RedisUnitTest
 @DataRedisTest(properties = "spring.config.location=classpath:application-domain.yml")
@@ -44,6 +46,28 @@ public class RefreshTokenServiceUnitTest extends ContainerRedisTestConfig {
         refreshTokenService.save(refreshToken);
 
         // then
-        log.info("test : {}", refreshTokenRepository.findById(1L));
+        RefreshToken savedRefreshToken = refreshTokenRepository.findById(1L).orElse(null);
+        assertEquals("저장된 리프레시 토큰이 일치하지 않습니다.", refreshToken, savedRefreshToken);
+        log.info("저장된 리프레시 토큰 정보 : {}", savedRefreshToken);
+    }
+
+    @Test
+    @DisplayName("리프레시 토큰 갱신 테스트")
+    void refreshTest() {
+        // given
+        RefreshToken refreshToken = RefreshToken.builder()
+                .userId(1L)
+                .token("refreshToken")
+                .ttl(1000L)
+                .build();
+        refreshTokenService.save(refreshToken);
+
+        // when
+        refreshTokenService.refresh(1L, "refreshToken", "newRefreshToken");
+
+        // then
+        RefreshToken savedRefreshToken = refreshTokenRepository.findById(1L).orElse(null);
+        assertEquals("갱신된 리프레시 토큰이 일치하지 않습니다.", "newRefreshToken", savedRefreshToken.getToken());
+        log.info("갱신된 리프레시 토큰 정보 : {}", savedRefreshToken);
     }
 }
