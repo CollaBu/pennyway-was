c9ddb66c4c31919911fd0c5b58bc5b0010f26a5a
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-30 12:13:14 +0900

    fix: 소셜 계정 연동 usecase 불필요 의존성 제거

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/usecase/UserAuthUseCase.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/usecase/UserAuthUseCase.java
@@ -10,25 +10,18 @@ import kr.co.pennyway.api.common.security.jwt.access.AccessTokenClaimKeys;
 import kr.co.pennyway.common.annotation.UseCase;
 import kr.co.pennyway.domain.domains.oauth.exception.OauthErrorCode;
 import kr.co.pennyway.domain.domains.oauth.exception.OauthException;
-import kr.co.pennyway.domain.domains.oauth.service.OauthService;
 import kr.co.pennyway.domain.domains.oauth.type.Provider;
-import kr.co.pennyway.domain.domains.user.domain.User;
-import kr.co.pennyway.domain.domains.user.exception.UserErrorCode;
-import kr.co.pennyway.domain.domains.user.exception.UserErrorException;
-import kr.co.pennyway.domain.domains.user.service.UserService;
 import kr.co.pennyway.infra.common.jwt.JwtClaims;
 import kr.co.pennyway.infra.common.jwt.JwtProvider;
 import kr.co.pennyway.infra.common.oidc.OidcDecodePayload;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @UseCase
 @RequiredArgsConstructor
 public class UserAuthUseCase {
-    private final UserService userService;
-    private final OauthService oauthService;
-
     private final UserOauthSignService userOauthSignService;
 
     private final JwtAuthHelper jwtAuthHelper;
@@ -50,23 +43,14 @@ public class UserAuthUseCase {
         jwtAuthHelper.removeAccessTokenAndRefreshToken(userId, authHeader, refreshToken);
     }
 
+    @Transactional
     public void linkOauth(Provider provider, SignInReq.Oauth request, Long userId) {
-        // 1. 페이로드를 추출한다.
         OidcDecodePayload payload = oauthOidcHelper.getPayload(provider, request.idToken(), request.nonce());
 
-        // 2. 요청을 검증하고 사용자가 연동 가능한 지 validation한다.
         if (!request.oauthId().equals(payload.sub()))
             throw new OauthException(OauthErrorCode.NOT_MATCHED_OAUTH_ID);
-        if (oauthService.isExistOauthAccount(userId, provider))
-            throw new OauthException(OauthErrorCode.ALREADY_SIGNUP_OAUTH);
 
-        // 3. 사용자를 읽어온다.
-        User user = userService.readUser(userId).orElseThrow(
-                () -> new UserErrorException(UserErrorCode.NOT_FOUND)
-        );
-
-        // 4. 사용자 로그인 정보에 Oauth 정보를 추가한다.
-        UserSyncDto userSync = UserSyncDto.of(true, true, user.getId(), user.getUsername());
+        UserSyncDto userSync = userOauthSignService.isLinkAllowed(userId, provider);
         userOauthSignService.saveUser(null, userSync, provider, request.oauthId());
     }
 }
