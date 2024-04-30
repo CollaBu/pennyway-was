049a7f0f417fa966dd3f5d9215813b2b8af1e4b8
asn6878 <asn6878@gmail.com>
2024-05-01 04:59:45 +0900

    feat: 비밀번호 변경 경로 설계

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/api/AuthCheckApi.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/api/AuthCheckApi.java
@@ -1,9 +1,5 @@
 package kr.co.pennyway.api.apis.auth.api;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
@@ -12,44 +8,51 @@ import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import kr.co.pennyway.api.apis.auth.dto.AuthFindDto;
 import kr.co.pennyway.api.apis.auth.dto.PhoneVerificationDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "[계정 검사 API]")
 public interface AuthCheckApi {
-	@Operation(summary = "닉네임 중복 검사")
-	ResponseEntity<?> checkUsername(@RequestParam @Validated String username);
+    @Operation(summary = "닉네임 중복 검사")
+    ResponseEntity<?> checkUsername(@RequestParam @Validated String username);
 
-	@Operation(summary = "일반 회원 아이디 찾기")
-	@Parameter(name = "phone", description = "휴대폰 번호", required = true, in = ParameterIn.QUERY, example = "010-1234-5678")
-	@ApiResponses({
-			@ApiResponse(responseCode = "200", description = "성공", content = @Content(mediaType = "application/json", examples = {
-					@ExampleObject(name = "일반 회원으로 등록된 휴대폰 번호일 경우", value = """
-							    {
-							        "code": "2000",
-							        "data": {
-							            "user": {
-							                "username": "pennyway"
-							            }
-							        }
-							    }
-							""")
-			})),
-			@ApiResponse(responseCode = "404", description = "일반 회원으로 등록되지 않은 휴대폰 번호일 경우", content = @Content(mediaType = "application/json", examples = {
-					@ExampleObject(name = "일반 회원으로 등록되지 않은 휴대폰 번호일 경우", value = """
-							    {
-							        "code": "4040",
-							        "message": "일반 회원으로 등록되지 않은 휴대폰 번호입니다."
-							    }
-							""")
-			})),
-			@ApiResponse(responseCode = "404", description = "인증번호 만료 또는 유효하지 않은 경우", content = @Content(mediaType = "application/json", examples = {
-					@ExampleObject(name = "인증번호 만료 또는 유효하지 않은 경우", value = """
-							    {
-							        "code": "4042",
-							        "message": "인증번호가 만료되었거나 유효하지 않습니다."
-							    }
-							""")
-			})),
-	})
-	ResponseEntity<?> findUsername(@Validated PhoneVerificationDto.VerifyCodeReq request);
+    @Operation(summary = "일반 회원 아이디 찾기")
+    @Parameter(name = "phone", description = "휴대폰 번호", required = true, in = ParameterIn.QUERY, example = "010-1234-5678")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공", content = @Content(mediaType = "application/json", examples = {
+                    @ExampleObject(name = "일반 회원으로 등록된 휴대폰 번호일 경우", value = """
+                                {
+                                    "code": "2000",
+                                    "data": {
+                                        "user": {
+                                            "username": "pennyway"
+                                        }
+                                    }
+                                }
+                            """)
+            })),
+            @ApiResponse(responseCode = "404", description = "일반 회원으로 등록되지 않은 휴대폰 번호일 경우", content = @Content(mediaType = "application/json", examples = {
+                    @ExampleObject(name = "일반 회원으로 등록되지 않은 휴대폰 번호일 경우", value = """
+                                {
+                                    "code": "4040",
+                                    "message": "일반 회원으로 등록되지 않은 휴대폰 번호입니다."
+                                }
+                            """)
+            })),
+            @ApiResponse(responseCode = "404", description = "인증번호 만료 또는 유효하지 않은 경우", content = @Content(mediaType = "application/json", examples = {
+                    @ExampleObject(name = "인증번호 만료 또는 유효하지 않은 경우", value = """
+                                {
+                                    "code": "4042",
+                                    "message": "인증번호가 만료되었거나 유효하지 않습니다."
+                                }
+                            """)
+            })),
+    })
+    ResponseEntity<?> findUsername(@Validated PhoneVerificationDto.VerifyCodeReq request);
+
+    @Operation(summary = "일반 회원 아이디 찾기")
+    ResponseEntity<?> findPassword(@Validated PhoneVerificationDto.VerifyCodeReq request, @Validated AuthFindDto.PasswordReq passwordReq);
 }
--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/usecase/AuthCheckUseCase.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/usecase/AuthCheckUseCase.java
@@ -1,7 +1,5 @@
 package kr.co.pennyway.api.apis.auth.usecase;
 
-import org.springframework.transaction.annotation.Transactional;
-
 import kr.co.pennyway.api.apis.auth.dto.AuthFindDto;
 import kr.co.pennyway.api.apis.auth.dto.PhoneVerificationDto;
 import kr.co.pennyway.api.apis.auth.service.AuthFindService;
@@ -12,25 +10,33 @@ import kr.co.pennyway.domain.common.redis.phone.PhoneCodeService;
 import kr.co.pennyway.domain.domains.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @UseCase
 @RequiredArgsConstructor
 public class AuthCheckUseCase {
-	private final UserService userService;
-	private final AuthFindService authFindService;
-	private final PhoneVerificationService phoneVerificationService;
-	private final PhoneCodeService phoneCodeService;
+    private final UserService userService;
+    private final AuthFindService authFindService;
+    private final PhoneVerificationService phoneVerificationService;
+    private final PhoneCodeService phoneCodeService;
 
-	@Transactional(readOnly = true)
-	public boolean checkUsernameDuplicate(String username) {
-		return userService.isExistUsername(username);
-	}
+    @Transactional(readOnly = true)
+    public boolean checkUsernameDuplicate(String username) {
+        return userService.isExistUsername(username);
+    }
 
-	@Transactional(readOnly = true)
-	public AuthFindDto.FindUsernameRes findUsername(PhoneVerificationDto.VerifyCodeReq request) {
-		phoneVerificationService.isValidCode(request, PhoneCodeKeyType.FIND_USERNAME);
-		phoneCodeService.delete(request.phone(), PhoneCodeKeyType.FIND_USERNAME);
-		return authFindService.findUsername(request.phone());
-	}
+    @Transactional(readOnly = true)
+    public AuthFindDto.FindUsernameRes findUsername(PhoneVerificationDto.VerifyCodeReq request) {
+        phoneVerificationService.isValidCode(request, PhoneCodeKeyType.FIND_USERNAME);
+        phoneCodeService.delete(request.phone(), PhoneCodeKeyType.FIND_USERNAME);
+        return authFindService.findUsername(request.phone());
+    }
+
+    @Transactional
+    public void findPassword(PhoneVerificationDto.VerifyCodeReq request, AuthFindDto.PasswordReq passwordReq) {
+        phoneVerificationService.isValidCode(request, PhoneCodeKeyType.FIND_PASSWORD);
+        phoneCodeService.delete(request.phone(), PhoneCodeKeyType.FIND_PASSWORD);
+        authFindService.updatePassword(request.phone(), passwordReq.newPassword());
+    }
 }
