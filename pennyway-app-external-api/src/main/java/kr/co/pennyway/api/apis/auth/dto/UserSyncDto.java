b312b263d6af7a04ff9775c95e2347a3e7f15989
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-04-30 14:31:40 +0900

    fix: user_sync_dto 연동하기 위한 oauth 정보를 추가로 받는 내부 record 추가

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/dto/UserSyncDto.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/dto/UserSyncDto.java
@@ -1,5 +1,8 @@
 package kr.co.pennyway.api.apis.auth.dto;
 
+import kr.co.pennyway.domain.domains.oauth.type.Provider;
+import kr.co.pennyway.domain.domains.user.domain.User;
+
 /**
  * 전화번호 검증 후, 시나리오 분기 정보를 위한 DTO
  */
@@ -8,24 +11,37 @@ public record UserSyncDto(
         boolean isSignUpAllowed,
         boolean isExistAccount,
         Long userId,
-        String username
+        String username,
+        /* 계정과 연동하기 위한 oauth 정보. 없다면 null */
+        OauthSync oauthSync
 ) {
     /**
      * @param isSignUpAllowed boolean : 회원가입 시나리오 가능 여부 (true: 회원가입 혹은 계정 연동 가능, false: 불가능)
      * @param isExistAccount  boolean : 이미 존재하는 계정 여부
-     * @param username        boolean : 사용자명 (isExistAccount이 true인 경우에만 존재)
+     * @param user            {@link User} : 사용자 정보
+     * @param oauthSync       {@link OauthSync} : 연동할 Oauth 정보. 없다면 null
      */
-    public static UserSyncDto of(boolean isSignUpAllowed, boolean isExistAccount, Long userId, String username) {
-        return new UserSyncDto(isSignUpAllowed, isExistAccount, userId, username);
+    public static UserSyncDto of(boolean isSignUpAllowed, boolean isExistAccount, User user, OauthSync oauthSync) {
+        return new UserSyncDto(isSignUpAllowed, isExistAccount, user.getId(), user.getUsername(), oauthSync);
     }
 
     /**
      * 이미 회원이 존재하는 경우 사용하는 편의용 메서드. <br/>
-     * 내부에서 {@link UserSyncDto#of(boolean, boolean, Long, String)}를 호출한다.
+     * 내부에서 {@link UserSyncDto#of(boolean, boolean, User, OauthSync)}를 호출한다.
      *
-     * @param username String : 사용자명
+     * @param user {@link User} : 사용자 정보
      */
-    public static UserSyncDto abort(Long userId, String username) {
-        return UserSyncDto.of(false, true, userId, username);
+    public static UserSyncDto abort(User user) {
+        return UserSyncDto.of(false, true, user, null);
+    }
+
+    public record OauthSync(
+            Long id,
+            String oauthId,
+            Provider provider
+    ) {
+        public static OauthSync of(Long id, String oauthId, Provider provider) {
+            return new OauthSync(id, oauthId, provider);
+        }
     }
 }
