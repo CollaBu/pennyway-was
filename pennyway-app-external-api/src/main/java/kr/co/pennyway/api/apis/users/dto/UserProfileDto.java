86956b822888abca299be00585db0ff8c0902a8a
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-02 11:05:54 +0900

    feat: oauth account 정보를 담을 dto 정의

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/users/dto/UserProfileDto.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/users/dto/UserProfileDto.java
@@ -42,7 +42,10 @@ public record UserProfileDto(
         @Schema(description = "계정 생성 일시", type = "string", example = "yyyy-MM-dd HH:mm:ss")
         @JsonSerialize(using = LocalDateTimeSerializer.class)
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-        LocalDateTime createdAt
+        LocalDateTime createdAt,
+        @Schema(description = "Oauth 계정 정보")
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        OauthAccountDto oauthAccount
 ) {
     public UserProfileDto {
         Objects.requireNonNull(id);
@@ -69,6 +72,24 @@ public record UserProfileDto(
                 .notifySetting(user.getNotifySetting())
                 .isOauthAccount(user.getPassword() == null)
                 .createdAt(user.getCreatedAt())
+                .oauthAccount(null)
+                .build();
+    }
+
+    public static UserProfileDto from(User user, OauthAccountDto oauthAccount) {
+        return UserProfileDto.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .name(user.getName())
+                .passwordUpdatedAt(user.getPasswordUpdatedAt())
+                .profileImageUrl(Objects.toString(user.getProfileImageUrl(), ""))
+                .phone(user.getPhone())
+                .profileVisibility(user.getProfileVisibility())
+                .locked(user.getLocked())
+                .notifySetting(user.getNotifySetting())
+                .isOauthAccount(user.getPassword() == null)
+                .createdAt(user.getCreatedAt())
+                .oauthAccount(oauthAccount)
                 .build();
     }
 }
