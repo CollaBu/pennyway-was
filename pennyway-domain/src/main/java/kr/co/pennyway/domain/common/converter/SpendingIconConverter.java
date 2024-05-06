414428d8d5652ef94143e4bfdbe1fe35e6c51936
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-07 01:08:01 +0900

    feat: 지출 아이콘 컨버터 정의

---
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/common/converter/SpendingIconConverter.java
@@ -0,0 +1,13 @@
+package kr.co.pennyway.domain.common.converter;
+
+import jakarta.persistence.Converter;
+import kr.co.pennyway.domain.domains.ledger.type.SpendingIcon;
+
+@Converter
+public class SpendingIconConverter extends AbstractLegacyEnumAttributeConverter<SpendingIcon> {
+    private static final String ENUM_NAME = "지출 아이콘";
+
+    public SpendingIconConverter() {
+        super(SpendingIcon.class, false, ENUM_NAME);
+    }
+}
