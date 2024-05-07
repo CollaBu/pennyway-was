52560f8b8aec62b91cea53013493e542bd91e984
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-07 11:43:56 +0900

    feat: 지출 entity 정의

---
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/spending/domain/Spending.java
@@ -0,0 +1,37 @@
+package kr.co.pennyway.domain.domains.spending.domain;
+
+import jakarta.persistence.*;
+import kr.co.pennyway.domain.common.converter.SpendingIconConverter;
+import kr.co.pennyway.domain.common.model.DateAuditable;
+import kr.co.pennyway.domain.domains.spending.type.SpendingIcon;
+import kr.co.pennyway.domain.domains.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "spending")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE spending SET deleted_at = NOW() WHERE id = ?")
+public class Spending extends DateAuditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer amount;
+    @Convert(converter = SpendingIconConverter.class)
+    private SpendingIcon icon;
+    private LocalDateTime spendAt;
+    private String accountName;
+    private String memo;
+    private LocalDateTime deletedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
