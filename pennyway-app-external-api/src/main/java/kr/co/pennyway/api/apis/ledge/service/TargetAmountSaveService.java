22e89eb105fcdb7e905ef95131372b34d30631de
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-08 18:34:08 +0900

    refactor: target_amount_save_service 분리

---
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledge/service/TargetAmountSaveService.java
@@ -0,0 +1,38 @@
+package kr.co.pennyway.api.apis.ledge.service;
+
+import kr.co.pennyway.domain.domains.target.domain.TargetAmount;
+import kr.co.pennyway.domain.domains.target.service.TargetAmountService;
+import kr.co.pennyway.domain.domains.user.domain.User;
+import kr.co.pennyway.domain.domains.user.exception.UserErrorCode;
+import kr.co.pennyway.domain.domains.user.exception.UserErrorException;
+import kr.co.pennyway.domain.domains.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TargetAmountSaveService {
+    private final UserService userService;
+    private final TargetAmountService targetAmountService;
+
+    /**
+     * 사용자에게 당월 목표 금액이 있으면 amount를 수정하고, 없으면 새로 생성한다.
+     */
+    @Transactional
+    public void saveTargetAmount(Long userId, LocalDate date, Integer amount) {
+        Optional<TargetAmount> targetAmount = targetAmountService.readTargetAmountThatMonth(userId, date);
+
+        if (targetAmount.isPresent()) {
+            targetAmount.get().updateAmount(amount);
+        } else {
+            User user = userService.readUser(userId).orElseThrow(() -> new UserErrorException(UserErrorCode.NOT_FOUND));
+            targetAmountService.createTargetAmount(TargetAmount.of(amount, user));
+        }
+    }
+}
--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledge/usecase/TargetAmountUseCase.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledge/usecase/TargetAmountUseCase.java
@@ -1,35 +1,21 @@
 package kr.co.pennyway.api.apis.ledge.usecase;
 
+import kr.co.pennyway.api.apis.ledge.service.TargetAmountSaveService;
 import kr.co.pennyway.common.annotation.UseCase;
-import kr.co.pennyway.domain.domains.target.domain.TargetAmount;
-import kr.co.pennyway.domain.domains.target.service.TargetAmountService;
-import kr.co.pennyway.domain.domains.user.domain.User;
-import kr.co.pennyway.domain.domains.user.exception.UserErrorCode;
-import kr.co.pennyway.domain.domains.user.exception.UserErrorException;
-import kr.co.pennyway.domain.domains.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.util.Optional;
 
 @Slf4j
 @UseCase
 @RequiredArgsConstructor
 public class TargetAmountUseCase {
-    private final UserService userService;
-    private final TargetAmountService targetAmountService;
+    private final TargetAmountSaveService targetAmountSaveService;
 
     @Transactional
     public void updateTargetAmount(Long userId, LocalDate date, Integer amount) {
-        Optional<TargetAmount> targetAmount = targetAmountService.readTargetAmountThatMonth(userId, date);
-
-        if (targetAmount.isPresent()) {
-            targetAmount.get().updateAmount(amount);
-        } else {
-            User user = userService.readUser(userId).orElseThrow(() -> new UserErrorException(UserErrorCode.NOT_FOUND));
-            targetAmountService.createTargetAmount(TargetAmount.of(amount, user));
-        }
+        targetAmountSaveService.saveTargetAmount(userId, date, amount);
     }
 }
