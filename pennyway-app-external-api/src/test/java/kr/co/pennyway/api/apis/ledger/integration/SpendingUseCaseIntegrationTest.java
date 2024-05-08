68581bbf0c18f1d9765d03867bd540bdf89241dc
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-09 01:10:45 +0900

    test: 월별 지출 내역 조회 통합 테스트 작성

---
+++ pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/ledger/integration/SpendingUseCaseIntegrationTest.java
@@ -0,0 +1,65 @@
+package kr.co.pennyway.api.apis.ledger.integration;
+
+import kr.co.pennyway.api.config.ExternalApiDBTestConfig;
+import kr.co.pennyway.api.config.ExternalApiIntegrationTest;
+import kr.co.pennyway.api.config.fixture.SpendingFixture;
+import kr.co.pennyway.api.config.fixture.UserFixture;
+import kr.co.pennyway.api.config.supporter.WithSecurityMockUser;
+import kr.co.pennyway.domain.domains.user.domain.User;
+import kr.co.pennyway.domain.domains.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Slf4j
+@ExternalApiIntegrationTest
+@AutoConfigureMockMvc
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
+public class SpendingUseCaseIntegrationTest extends ExternalApiDBTestConfig {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private NamedParameterJdbcTemplate jdbcTemplate;
+
+    @Order(1)
+    @Nested
+    @DisplayName("월별 지출 내역 조회")
+    class GetSpendingListAtYearAndMonth {
+        @Test
+        @DisplayName("월별 지출 내역 조회")
+        @WithSecurityMockUser
+        void getSpendingListAtYearAndMonthSuccess() throws Exception {
+            // given
+            User user = userService.createUser(UserFixture.GENERAL_USER.toUser());
+            SpendingFixture.bulkInsertSpending(user, 150, jdbcTemplate);
+
+            // when
+            ResultActions resultActions = performGetSpendingListAtYearAndMonthSuccess();
+
+            // then
+            resultActions
+                    .andDo(print())
+                    .andExpect(status().isOk());
+        }
+
+        private ResultActions performGetSpendingListAtYearAndMonthSuccess() throws Exception {
+            LocalDate now = LocalDate.now();
+            return mockMvc.perform(MockMvcRequestBuilders.get("/v2/spendings")
+                    .param("year", String.valueOf(now.getYear()))
+                    .param("month", String.valueOf(now.getMonthValue())));
+        }
+    }
+}
