1914e5a5a6e66814c2cd25a0aedfe7861b2c333d
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-08 23:40:43 +0900

    feat: spending domain service 내 read_spendings() 추가

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/spending/service/SpendingService.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/spending/service/SpendingService.java
@@ -1,11 +1,16 @@
 package kr.co.pennyway.domain.domains.spending.service;
 
+import com.querydsl.core.types.Predicate;
 import kr.co.pennyway.common.annotation.DomainService;
+import kr.co.pennyway.domain.common.repository.QueryHandler;
 import kr.co.pennyway.domain.domains.spending.domain.Spending;
 import kr.co.pennyway.domain.domains.spending.repository.SpendingRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @DomainService
@@ -16,5 +21,10 @@ public class SpendingService {
     @Transactional
     public Spending createSpending(Spending spending) {
         return spendingRepository.save(spending);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Spending> readSpendings(Predicate predicate, QueryHandler queryHandler, Sort sort) {
+        return spendingRepository.findList(predicate, queryHandler, sort);
     }
 }
