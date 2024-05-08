49e1c0bfadfb21eb717c611661187f5786877aca
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-08 20:51:26 +0900

    feat: 지출(spending) 컨트롤러 작성

---
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledge/controller/SpendingController.java
@@ -0,0 +1,29 @@
+package kr.co.pennyway.api.apis.ledge.controller;
+
+import kr.co.pennyway.api.apis.ledge.api.SpendingApi;
+import kr.co.pennyway.api.common.response.SuccessResponse;
+import kr.co.pennyway.api.common.security.authentication.SecurityUserDetails;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v2/spendings")
+public class SpendingController implements SpendingApi {
+    private final SpendingUseCase spendingUseCase;
+
+    @Override
+    @GetMapping("")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<?> getSpendingListAtMonth(@RequestParam("year") int year, @RequestParam("month") int month, @AuthenticationPrincipal SecurityUserDetails user) {
+        return ResponseEntity.ok(SuccessResponse.from("spendings", spendingUseCase.getSpendingsAtMonth(year, month, user)));
+    }
+}
