0f968e8687977dd9bdfaa1257c00ebff49366850
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-10 00:02:32 +0900

    test: 소비 내역 등록 controller unit test

---
+++ pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/ledger/controller/SpendingControllerUnitTest.java
@@ -0,0 +1,120 @@
+package kr.co.pennyway.api.apis.ledger.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.co.pennyway.api.apis.ledger.usecase.SpendingUseCase;
+import kr.co.pennyway.api.config.supporter.WithSecurityMockUser;
+import kr.co.pennyway.domain.domains.spending.type.SpendingCategory;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = SpendingController.class)
+@ActiveProfiles("test")
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
+public class SpendingControllerUnitTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private SpendingUseCase spendingUseCase;
+
+    @Order(1)
+    @Nested
+    @DisplayName("지출 내역 추가하기")
+    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+    class postSpending {
+        @Test
+        @DisplayName("금액이 0이하의 정수인 경우 422 Unprocessable Entity를 반환한다.")
+        @WithSecurityMockUser
+        void whenAmountIsZeroOrNegative() throws Exception {
+            // given
+            int amount = 0;
+            SpendingReq request = new SpendingReq(amount, -1L, SpendingCategory.FOOD, LocalDate.now(), "소비처", "메모");
+
+            // when
+            ResultActions result = performPostSpending(request);
+
+            // then
+            result.andExpect(status().isUnprocessableEntity());
+        }
+
+        @Test
+        @DisplayName("아이콘이 OTHER이면서 categoryId가 -1인 경우 400 Bad Request를 반환한다.")
+        @WithSecurityMockUser
+        void whenCategoryIsNotDefined() throws Exception {
+            // given
+            Long categoryId = -1L;
+            SpendingCategory icon = SpendingCategory.OTHER;
+            SpendingReq request = new SpendingReq(10000, categoryId, icon, LocalDate.now(), "소비처", "메모");
+
+            // when
+            ResultActions result = performPostSpending(request);
+
+            // then
+            result.andExpect(status().isUnprocessableEntity());
+        }
+
+        @Test
+        @DisplayName("지출일이 현재보다 미래인 경우 422 Unprocessable Entity를 반환한다.")
+        @WithSecurityMockUser
+        void whenSpendAtIsFuture() throws Exception {
+            // given
+            LocalDate spendAt = LocalDate.now().plusDays(1);
+            SpendingReq request = new SpendingReq(10000, -1L, SpendingCategory.FOOD, spendAt, "소비처", "메모");
+
+            // when
+            ResultActions result = performPostSpending(request);
+
+            // then
+            result.andExpect(status().isUnprocessableEntity());
+        }
+
+        @Test
+        @DisplayName("소비처가 null이 아니면서 20자를 초과하는 경우 422 Unprocessable Entity를 반환한다.")
+        @WithSecurityMockUser
+        void whenAccountNameIsNotNullAndOver20() throws Exception {
+            // given
+            String accountName = "123456789012345678901";
+            SpendingReq request = new SpendingReq(10000, -1L, SpendingCategory.FOOD, LocalDate.now(), accountName, "메모");
+
+            // when
+            ResultActions result = performPostSpending(request);
+
+            // then
+            result.andExpect(status().isUnprocessableEntity());
+        }
+
+        @Test
+        @DisplayName("메모가 null이 아니면서 100자를 초과하는 경우 422 Unprocessable Entity를 반환한다.")
+        @WithSecurityMockUser
+        void whenMemoIsNotNullAndOver100() throws Exception {
+            // given
+            String memo = "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890";
+            SpendingReq request = new SpendingReq(10000, -1L, SpendingCategory.FOOD, LocalDate.now(), "소비처", memo);
+
+            // when
+            ResultActions result = performPostSpending(request);
+
+            // then
+            result.andExpect(status().isUnprocessableEntity());
+        }
+
+        private ResultActions performPostSpending(SpendingReq request) throws Exception {
+            return mockMvc.perform(post("/v2/spendings")
+                    .contentType("application/json")
+                    .content(objectMapper.writeValueAsString(request)));
+        }
+    }
+}
