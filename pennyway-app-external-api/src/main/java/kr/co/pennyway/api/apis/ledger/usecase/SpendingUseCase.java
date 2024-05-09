51cb85a0199b910ee9dbdaeb612736c16f4321b7
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-10 02:10:32 +0900

    refactor: spending_save_service 분리

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/usecase/SpendingUseCase.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/usecase/SpendingUseCase.java
@@ -3,14 +3,10 @@ package kr.co.pennyway.api.apis.ledger.usecase;
 import kr.co.pennyway.api.apis.ledger.dto.SpendingReq;
 import kr.co.pennyway.api.apis.ledger.dto.SpendingSearchRes;
 import kr.co.pennyway.api.apis.ledger.mapper.SpendingMapper;
+import kr.co.pennyway.api.apis.ledger.service.SpendingSaveService;
 import kr.co.pennyway.api.apis.ledger.service.SpendingSearchService;
 import kr.co.pennyway.common.annotation.UseCase;
 import kr.co.pennyway.domain.domains.spending.domain.Spending;
-import kr.co.pennyway.domain.domains.spending.domain.SpendingCustomCategory;
-import kr.co.pennyway.domain.domains.spending.exception.SpendingErrorCode;
-import kr.co.pennyway.domain.domains.spending.exception.SpendingErrorException;
-import kr.co.pennyway.domain.domains.spending.service.SpendingCustomCategoryService;
-import kr.co.pennyway.domain.domains.spending.service.SpendingService;
 import kr.co.pennyway.domain.domains.user.domain.User;
 import kr.co.pennyway.domain.domains.user.exception.UserErrorCode;
 import kr.co.pennyway.domain.domains.user.exception.UserErrorException;
@@ -25,25 +21,17 @@ import java.util.List;
 @UseCase
 @RequiredArgsConstructor
 public class SpendingUseCase {
+    private final SpendingSaveService spendingSaveService;
     private final SpendingSearchService spendingSearchService;
 
     private final UserService userService;
-    private final SpendingService spendingService;
-    private final SpendingCustomCategoryService spendingCustomCategoryService;
+
 
     @Transactional
     public SpendingSearchRes.Individual createSpending(Long userId, SpendingReq request) {
         User user = userService.readUser(userId).orElseThrow(() -> new UserErrorException(UserErrorCode.NOT_FOUND));
 
-        Spending spending;
-        if (request.categoryId().equals(-1L)) {
-            spending = spendingService.createSpending(request.toEntity(user));
-        } else {
-            SpendingCustomCategory customCategory = spendingCustomCategoryService.readSpendingCustomCategory(request.categoryId())
-                    .orElseThrow(() -> new SpendingErrorException(SpendingErrorCode.NOT_FOUND_CUSTOM_CATEGORY));
-
-            spending = spendingService.createSpending(request.toEntity(user, customCategory));
-        }
+        Spending spending = spendingSaveService.createSpending(user, request);
 
         return SpendingMapper.toSpendingSearchResIndividual(spending);
     }
