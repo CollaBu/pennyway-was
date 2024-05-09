d40a6abcf1bd77658ca72f9ad0a42f1176dbc04b
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-10 01:35:22 +0900

    test: request의 categoryId가 -1인 경우, spendingCustomCategory가 null인 Spending을 생성한다

--- pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/ledger/integration/SpendingUseCaseIntegrationTest.java
+++ pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/ledger/integration/SpendingUseCaseIntegrationTest.java
@@ -1,10 +1,13 @@
 package kr.co.pennyway.api.apis.ledger.integration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.co.pennyway.api.apis.ledger.dto.SpendingReq;
 import kr.co.pennyway.api.config.ExternalApiDBTestConfig;
 import kr.co.pennyway.api.config.ExternalApiIntegrationTest;
 import kr.co.pennyway.api.config.fixture.SpendingFixture;
 import kr.co.pennyway.api.config.fixture.UserFixture;
 import kr.co.pennyway.api.config.supporter.WithSecurityMockUser;
+import kr.co.pennyway.domain.domains.spending.type.SpendingCategory;
 import kr.co.pennyway.domain.domains.user.domain.User;
 import kr.co.pennyway.domain.domains.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
@@ -30,17 +33,46 @@ public class SpendingUseCaseIntegrationTest extends ExternalApiDBTestConfig {
     @Autowired
     private MockMvc mockMvc;
     @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
     private UserService userService;
     @Autowired
     private NamedParameterJdbcTemplate jdbcTemplate;
 
     @Order(1)
     @Nested
+    @DisplayName("지출 내역 추가하기")
+    class CreateSpending {
+        @Test
+        @DisplayName("request의 categoryId가 -1인 경우, spendingCustomCategory가 null인 Spending을 생성한다.")
+        @WithSecurityMockUser(userId = "1")
+        void createSpendingSuccess() throws Exception {
+            // given
+            userService.createUser(UserFixture.GENERAL_USER.toUser());
+            SpendingReq request = new SpendingReq(10000, -1L, SpendingCategory.FOOD, LocalDate.now(), "소비처", "메모");
+
+            // when
+            ResultActions resultActions = performCreateSpendingSuccess(request);
+
+            // then
+            resultActions.andDo(print()).andExpect(status().isOk());
+        }
+
+        private ResultActions performCreateSpendingSuccess(SpendingReq req) throws Exception {
+            return mockMvc.perform(MockMvcRequestBuilders
+                    .post("/v2/spendings")
+                    .contentType("application/json")
+                    .content(objectMapper.writeValueAsString(req)));
+        }
+    }
+
+    @Order(2)
+    @Nested
     @DisplayName("월별 지출 내역 조회")
     class GetSpendingListAtYearAndMonth {
         @Test
         @DisplayName("월별 지출 내역 조회")
-        @WithSecurityMockUser
+        @WithSecurityMockUser(userId = "2")
         void getSpendingListAtYearAndMonthSuccess() throws Exception {
             // given
             User user = userService.createUser(UserFixture.GENERAL_USER.toUser());
