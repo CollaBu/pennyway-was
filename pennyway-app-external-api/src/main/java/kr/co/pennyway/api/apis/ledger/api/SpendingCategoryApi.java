4ab3c9cba3656839251bb44562207f33e7027e2c
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-09 20:33:40 +0900

    docs: param query object 스웨거 상에서 안 보이도록 처리

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/api/SpendingCategoryApi.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/api/SpendingCategoryApi.java
@@ -17,12 +17,13 @@ public interface SpendingCategoryApi {
     @Operation(summary = "지출 내역 카테고리 등록", method = "POST", description = "사용자 커스텀 지출 카테고리를 생성합니다.")
     @Parameters({
             @Parameter(name = "name", description = "카테고리 이름", required = true, in = ParameterIn.QUERY),
-            @Parameter(name = "icon", description = "카테고리 아이콘", required = true, in = ParameterIn.QUERY, examples = {
+            @Parameter(name = "icon", description = "카테고리 아이콘. 대문자만 허용합니다.", required = true, in = ParameterIn.QUERY, examples = {
                     @ExampleObject(name = "식사", value = "FOOD"), @ExampleObject(name = "교통", value = "TRANSPORTATION"), @ExampleObject(name = "뷰티/패션", value = "BEAUTY_OR_FASHION"),
                     @ExampleObject(name = "편의점/마트", value = "CONVENIENCE_STORE"), @ExampleObject(name = "교육", value = "EDUCATION"), @ExampleObject(name = "생활", value = "LIVING"),
                     @ExampleObject(name = "건강", value = "HEALTH"), @ExampleObject(name = "취미/여가", value = "HOBBY"), @ExampleObject(name = "여행/숙박", value = "TRAVEL"),
                     @ExampleObject(name = "술/유흥", value = "ALCOHOL_OR_ENTERTAINMENT"), @ExampleObject(name = "회비/경조사", value = "MEMBERSHIP_OR_FAMILY_EVENT")
-            })
+            }),
+            @Parameter(name = "param", hidden = true)
     })
     ResponseEntity<?> postSpendingCategory(@Validated SpendingCategoryDto.CreateParamReq param, @AuthenticationPrincipal SecurityUserDetails user);
 }
