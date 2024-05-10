4ad684b41a537bb93017a9cca790cd18c4a5b8ab
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-10 13:28:25 +0900

    rename: icon -> category

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/common/converter/SpendingCategoryConverter.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/common/converter/SpendingCategoryConverter.java
@@ -4,10 +4,10 @@ import jakarta.persistence.Converter;
 import kr.co.pennyway.domain.domains.spending.type.SpendingCategory;
 
 @Converter
-public class SpendingIconConverter extends AbstractLegacyEnumAttributeConverter<SpendingCategory> {
-    private static final String ENUM_NAME = "지출 아이콘";
+public class SpendingCategoryConverter extends AbstractLegacyEnumAttributeConverter<SpendingCategory> {
+    private static final String ENUM_NAME = "지출 카테고리";
 
-    public SpendingIconConverter() {
+    public SpendingCategoryConverter() {
         super(SpendingCategory.class, false, ENUM_NAME);
     }
 }
