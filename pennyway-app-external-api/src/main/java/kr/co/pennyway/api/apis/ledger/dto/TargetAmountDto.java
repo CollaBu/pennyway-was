23f58bb2bfbdf2d91f69d92dfc6118882c4f0138
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-13 20:59:55 +0900

    feat: 목표 금액 조회 응답 dto 정의

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/dto/TargetAmountDto.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/dto/TargetAmountDto.java
@@ -35,4 +35,21 @@ public class TargetAmountDto {
     ) {
 
     }
+
+    @Schema(title = "목표 금액 조회 응답")
+    public record GetResponse(
+            @Schema(description = "목표 금액 날짜", example = "2024-05-08", requiredMode = Schema.RequiredMode.REQUIRED)
+            @NotNull(message = "date 값은 필수입니다.")
+            @JsonSerialize(using = LocalDateSerializer.class)
+            @JsonFormat(pattern = "yyyy-MM-dd")
+            LocalDate date,
+            @Schema(description = "목표 금액", example = "50000", requiredMode = Schema.RequiredMode.REQUIRED)
+            int targetAmount,
+            @Schema(description = "총 지출 금액", example = "100000", requiredMode = Schema.RequiredMode.REQUIRED)
+            int totalSpending,
+            @Schema(description = "목표 금액과 총 지출 금액의 차액(총 치줄 금액 - 목표 금액). 양수면 초과, 음수면 절약", example = "-50000", requiredMode = Schema.RequiredMode.REQUIRED)
+            int diffAmount
+    ) {
+        
+    }
 }
