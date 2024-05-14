3667115cb088cec52087f9db7464581ec0042ebd
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-14 18:40:30 +0900

    fix: view_type 제거

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/common/query/ViewType.java
+++
@@ -1,5 +0,0 @@
-package kr.co.pennyway.api.common.query;
-
-public enum ViewType {
-    SUMMARY, DETAIL
-}
--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/config/WebConfig.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/config/WebConfig.java
@@ -3,7 +3,6 @@ package kr.co.pennyway.api.config;
 import kr.co.pennyway.api.common.converter.NotifyTypeConverter;
 import kr.co.pennyway.api.common.converter.ProviderConverter;
 import kr.co.pennyway.api.common.converter.VerificationTypeConverter;
-import kr.co.pennyway.api.common.converter.ViewTypeConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,6 +15,5 @@ public class WebConfig implements WebMvcConfigurer {
         registrar.addConverter(new ProviderConverter());
         registrar.addConverter(new VerificationTypeConverter());
         registrar.addConverter(new NotifyTypeConverter());
-        registrar.addConverter(new ViewTypeConverter());
     }
 }
