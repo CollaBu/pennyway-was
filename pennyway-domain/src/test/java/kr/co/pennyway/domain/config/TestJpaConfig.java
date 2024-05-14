7fc923970b7be95e564865779916f8dc69cc9759
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-15 02:02:04 +0900

    chore: jpa_query_factory 테스트 환경 설정용 빈 등록

---
+++ pennyway-domain/src/test/java/kr/co/pennyway/domain/config/TestJpaConfig.java
@@ -0,0 +1,18 @@
+package kr.co.pennyway.domain.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration
+public class TestJpaConfig {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
+}
