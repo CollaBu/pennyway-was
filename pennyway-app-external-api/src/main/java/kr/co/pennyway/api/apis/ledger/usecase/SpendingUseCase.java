ce35060e4d3f93a1715434eccfd00d52e844d30e
asn6878 <asn6878@gmail.com>
2024-05-14 23:23:58 +0900

    feat: usecase 작성

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/usecase/SpendingUseCase.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/usecase/SpendingUseCase.java
@@ -7,6 +7,8 @@ import kr.co.pennyway.api.apis.ledger.service.SpendingSaveService;
 import kr.co.pennyway.api.apis.ledger.service.SpendingSearchService;
 import kr.co.pennyway.common.annotation.UseCase;
 import kr.co.pennyway.domain.domains.spending.domain.Spending;
+import kr.co.pennyway.domain.domains.spending.exception.SpendingErrorCode;
+import kr.co.pennyway.domain.domains.spending.exception.SpendingErrorException;
 import kr.co.pennyway.domain.domains.user.domain.User;
 import kr.co.pennyway.domain.domains.user.exception.UserErrorCode;
 import kr.co.pennyway.domain.domains.user.exception.UserErrorException;
@@ -41,5 +43,16 @@ public class SpendingUseCase {
         List<Spending> spendings = spendingSearchService.readSpendings(userId, year, month);
 
         return SpendingMapper.toSpendingSearchResMonth(spendings, year, month);
+    }
+
+    @Transactional(readOnly = true)
+    public SpendingSearchRes.Individual getSpedingDetail(Long userId, Long spendingId) {
+        Spending spending = spendingSearchService.readSpending(spendingId);
+
+        if (!(userId == spending.getUser().getId())) {
+            throw new SpendingErrorException(SpendingErrorCode.FORBIDDEN_SPENDING);
+        }
+
+        return SpendingMapper.toSpendingSearchResIndividual(spending);
     }
 }
