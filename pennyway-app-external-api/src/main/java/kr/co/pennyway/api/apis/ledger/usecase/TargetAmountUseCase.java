f3082a18e9125c3917a89ef7892f71d479285af9
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-15 12:16:21 +0900

    feat: target_amount 삭제 usecase 추가

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/usecase/TargetAmountUseCase.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/usecase/TargetAmountUseCase.java
@@ -2,6 +2,10 @@ package kr.co.pennyway.api.apis.ledger.usecase;
 
 import kr.co.pennyway.api.apis.ledger.service.TargetAmountSaveService;
 import kr.co.pennyway.common.annotation.UseCase;
+import kr.co.pennyway.domain.domains.target.domain.TargetAmount;
+import kr.co.pennyway.domain.domains.target.exception.TargetAmountErrorCode;
+import kr.co.pennyway.domain.domains.target.exception.TargetAmountErrorException;
+import kr.co.pennyway.domain.domains.target.service.TargetAmountService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,10 +16,24 @@ import java.time.LocalDate;
 @UseCase
 @RequiredArgsConstructor
 public class TargetAmountUseCase {
+    private final TargetAmountService targetAmountService;
+
     private final TargetAmountSaveService targetAmountSaveService;
 
     @Transactional
     public void updateTargetAmount(Long userId, LocalDate date, Integer amount) {
         targetAmountSaveService.saveTargetAmount(userId, date, amount);
+    }
+
+    @Transactional
+    public void deleteTargetAmount(Long userId, LocalDate date) {
+        TargetAmount targetAmount = targetAmountService.readTargetAmountThatMonth(userId, date)
+                .orElseThrow(() -> new TargetAmountErrorException(TargetAmountErrorCode.NOT_FOUND_TARGET_AMOUNT));
+
+        if (!targetAmount.isAllocatedAmount()) {
+            throw new TargetAmountErrorException(TargetAmountErrorCode.NOT_FOUND_TARGET_AMOUNT);
+        }
+
+        targetAmountService.deleteTargetAmount(targetAmount);
     }
 }
