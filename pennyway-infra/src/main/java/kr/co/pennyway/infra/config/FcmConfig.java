5ac1eda1eec1c40473a140c2e9cacbf4b7761ce6
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-16 14:35:46 +0900

    feat: fcm_config 정의

--- pennyway-infra/src/main/java/kr/co/pennyway/infra/config/FcmConfig.java
+++ pennyway-infra/src/main/java/kr/co/pennyway/infra/config/FcmConfig.java
@@ -1,6 +1,43 @@
 package kr.co.pennyway.infra.config;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import jakarta.annotation.PostConstruct;
 import kr.co.pennyway.infra.common.importer.PennywayInfraConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
 
+import java.io.IOException;
+
+@Slf4j
 public class FcmConfig implements PennywayInfraConfig {
+    private final ClassPathResource firebaseResource;
+    private final String projectId;
+
+    public FcmConfig(@Value("${app.firebase.config.file}") String firebaseConfigPath,
+                     @Value("${app.firebase.project.id}") String projectId) {
+        this.firebaseResource = new ClassPathResource(firebaseConfigPath);
+        this.projectId = projectId;
+    }
+
+    @PostConstruct
+    public void init() throws IOException {
+        FirebaseOptions option = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(firebaseResource.getInputStream()))
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(option);
+            log.info("FirebaseApp is initialized");
+        }
+    }
+    
+    @Bean
+    FirebaseMessaging firebaseMessaging() {
+        return FirebaseMessaging.getInstance(FirebaseApp.getInstance());
+    }
 }
