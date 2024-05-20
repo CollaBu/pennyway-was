e0459064bca33f5fa39a42dc1c826d979e5fc8f3
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-20 17:30:53 +0900

    ✨ Firebase Cloud Messaging 기본 설정 (#94)
    * chore: firebase build 종속성 추가
    
    * chore: fcm_config import selector group 설정
    
    * chore: external-api infra_config에서 fcm_config 빈 스캔
    
    * chore: firebase admin key 환경변수 등록
    
    * feat: fcm_config 정의
    
    * chore: firebase admin-key .gitignore 등록
    
    * fix: fcm_config 실행 환경에서 test 제거
    
    * rename: fcm config @profile todo 주석 추가
    
    * chore: cd pipeline 내 fcm admin sdk json 파일 생성 step 추가
    
    * chore: cd pipeline json 삽입 동작 확인
    
    * chore: json name, dir 속성 분리
    
    * chore: resource firebase 디렉토리 .gitkeep 생성 및 .gitignore 범위 수정
    
    * chore: feature 브랜치 cd 감지 제거

---
+++ pennyway-infra/src/main/java/kr/co/pennyway/infra/config/FcmConfig.java
@@ -0,0 +1,46 @@
+package kr.co.pennyway.infra.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import jakarta.annotation.PostConstruct;
+import kr.co.pennyway.infra.common.importer.PennywayInfraConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+@Slf4j
+@Profile({"local", "dev", "prod"})
+// TODO: 2024.05.17 우선 테스트 통과를 위해 임시로 처리함. Push Notification 기능 테스트 시 문제가 발생하면 수정이 필요함.
+public class FcmConfig implements PennywayInfraConfig {
+    private final ClassPathResource firebaseResource;
+    private final String projectId;
+
+    public FcmConfig(@Value("${app.firebase.config.file}") String firebaseConfigPath,
+                     @Value("${app.firebase.project.id}") String projectId) {
+        this.firebaseResource = new ClassPathResource(firebaseConfigPath);
+        this.projectId = projectId;
+    }
+
+    @PostConstruct
+    public void init() throws IOException {
+        FirebaseOptions option = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(firebaseResource.getInputStream()))
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(option);
+            log.info("FirebaseApp is initialized");
+        }
+    }
+
+    @Bean
+    FirebaseMessaging firebaseMessaging() {
+        return FirebaseMessaging.getInstance(FirebaseApp.getInstance());
+    }
+}
