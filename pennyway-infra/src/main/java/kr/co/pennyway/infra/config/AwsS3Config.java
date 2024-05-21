a7fa9ecaa170cc13f1cf8c590240dd8c1e7f9b4b
Jinwoo Lee <binaryWooo@gmail.com>
2024-05-21 15:52:19 +0900

    fix: s3 접근을 위한 credentials 선언 및 적용

--- pennyway-infra/src/main/java/kr/co/pennyway/infra/config/AwsS3Config.java
+++ pennyway-infra/src/main/java/kr/co/pennyway/infra/config/AwsS3Config.java
@@ -1,30 +1,43 @@
 package kr.co.pennyway.infra.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.Getter;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Getter
 @Configuration
 public class AwsS3Config {
+	private final String accessKey;
+	private final String secretKey;
+	private final String region;
+
+	public AwsS3Config(
+			@Value("${spring.cloud.aws.s3.credentials.access-key}") String accessKey,
+			@Value("${spring.cloud.aws.s3.credentials.secret-key}") String secretKey,
+			@Value("${spring.cloud.aws.s3.region.static}") String region
+	) {
+		this.accessKey = accessKey;
+		this.secretKey = secretKey;
+		this.region = region;
+	}
+
 	@Bean
-	public S3Client s3Client() {
-		return S3Client.builder()
-				.region(Region.AP_NORTHEAST_2)
-				.credentialsProvider(ProfileCredentialsProvider.create())
-				.build();
+	public AwsCredentials awsS3Credentials() {
+		return AwsBasicCredentials.create(accessKey, secretKey);
 	}
 
 	@Bean
 	public S3Presigner s3Presigner() {
 		return S3Presigner.builder()
-				.region(Region.AP_NORTHEAST_2)
-				.credentialsProvider(ProfileCredentialsProvider.create())
+				.region(Region.of(region))
+				.credentialsProvider(StaticCredentialsProvider.create(awsS3Credentials()))
 				.build();
 	}
 }
