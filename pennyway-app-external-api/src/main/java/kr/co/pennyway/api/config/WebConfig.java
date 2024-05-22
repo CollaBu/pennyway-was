5e567ec34cbbb76cd5a09f40885e601c2ad43fce
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-22 11:59:41 +0900

    feat: web_mvc_config sign_event_log_interceptor 등록

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/config/WebConfig.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/config/WebConfig.java
@@ -3,17 +3,29 @@ package kr.co.pennyway.api.config;
 import kr.co.pennyway.api.common.converter.NotifyTypeConverter;
 import kr.co.pennyway.api.common.converter.ProviderConverter;
 import kr.co.pennyway.api.common.converter.VerificationTypeConverter;
+import kr.co.pennyway.domain.common.redis.sign.SignEventLogService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    private final SignEventLogService signEventLogService;
+
     @Override
     public void addFormatters(FormatterRegistry registrar) {
 
         registrar.addConverter(new ProviderConverter());
         registrar.addConverter(new VerificationTypeConverter());
         registrar.addConverter(new NotifyTypeConverter());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SignEventLogInterceptor(signEventLogService))
+                .addPathPatterns("/v1/auth/sign-in", "/v1/auth/oauth/sign-up", "/v1/auth/refresh");
     }
 }
