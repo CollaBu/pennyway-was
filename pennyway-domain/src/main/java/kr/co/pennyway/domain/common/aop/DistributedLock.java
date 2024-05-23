4e014c2ef844544512d494762ceeb36c4170051c
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-23 20:37:41 +0900

    feat: 분산락 커스텀 어노테이션 생성

---
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/common/aop/DistributedLock.java
@@ -0,0 +1,33 @@
+package kr.co.pennyway.domain.common.aop;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DistributedLock {
+    /**
+     * Lock 이름
+     */
+    String key();
+
+    /**
+     * Lock 유지 시간 (초)
+     */
+    TimeUnit unit() default TimeUnit.SECONDS;
+
+    /**
+     * Lock 유지 시간 (DEFAULT: 5초)
+     * LOCK 획득을 위해 waitTime만큼 대기한다.
+     */
+    long waitTime() default 5L;
+
+    /**
+     * Lock 임대 시간 (DEFAULT: 3초)
+     * LOCK 획득 이후 leaseTime이 지나면 LOCK을 해제한다.
+     */
+    long leaseTime() default 3L;
+}
