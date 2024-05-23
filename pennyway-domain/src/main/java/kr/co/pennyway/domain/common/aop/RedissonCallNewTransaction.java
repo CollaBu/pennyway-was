252b3c8db125c26cd0295b3548be3ed4d6682654
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-23 21:53:08 +0900

    fix: redisson_call_new_transaction timout 및 주석 수정

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/common/aop/RedissonCallNewTransaction.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/common/aop/RedissonCallNewTransaction.java
@@ -11,9 +11,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class RedissonCallNewTransaction implements CallTransaction {
     /**
      * 다른 트랜잭션이 실행 중인 경우에도 새로운 트랜잭션을 생성하여 이 메서드를 실행한다.
+     * 동시성 환경에서 데이터 정합성을 보장하기 위해 트랜잭션 커밋 이후 락이 해제된다.
      */
     @Override
-    @Transactional(propagation = Propagation.REQUIRES_NEW, timeout = 2)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public Object proceed(ProceedingJoinPoint joinPoint) throws Throwable {
         return null;
     }
