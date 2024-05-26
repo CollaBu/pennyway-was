dc271d2e6b2901b0b3163361b76df54caf3b3ce1
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-26 23:58:40 +0900

    test: 분산 락 테스트용 더미 service 구현

---
+++ pennyway-domain/src/test/java/kr/co/pennyway/domain/common/redisson/TestCouponDecreaseService.java
@@ -0,0 +1,35 @@
+package kr.co.pennyway.domain.common.redisson;
+
+import jakarta.persistence.EntityManager;
+import kr.co.pennyway.domain.common.aop.DistributedLock;
+import org.springframework.transaction.annotation.Transactional;
+
+public class TestCouponDecreaseService {
+    private final EntityManager em;
+
+    TestCouponDecreaseService(EntityManager em) {
+        this.em = em;
+    }
+
+    @Transactional
+    public void decreaseStock(Long couponId) {
+        TestCoupon coupon = em.find(TestCoupon.class, couponId);
+
+        if (coupon == null) {
+            throw new IllegalArgumentException("존재하지 않는 쿠폰입니다.");
+        }
+
+        coupon.decreaseStock();
+    }
+
+    @DistributedLock(key = "#lockName")
+    public void decreaseStockWithLock(Long couponId, String lockName) {
+        TestCoupon coupon = em.find(TestCoupon.class, couponId);
+
+        if (coupon == null) {
+            throw new IllegalArgumentException("존재하지 않는 쿠폰입니다.");
+        }
+
+        coupon.decreaseStock();
+    }
+}
