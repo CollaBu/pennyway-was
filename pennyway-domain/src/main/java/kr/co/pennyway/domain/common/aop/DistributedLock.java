8717756610993e1115f861350c29d88cffba12c4
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-27 22:04:02 +0900

    fix: lock 획득 대기시간 연장

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/common/aop/DistributedLock.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/common/aop/DistributedLock.java
@@ -20,16 +20,16 @@ public @interface DistributedLock {
     TimeUnit timeUnit() default TimeUnit.SECONDS;
 
     /**
-     * Lock 유지 시간 (DEFAULT: 5초)
+     * Lock 유지 시간 (DEFAULT: 10초)
      * LOCK 획득을 위해 waitTime만큼 대기한다.
      */
-    long waitTime() default 5L;
+    long waitTime() default 10L;
 
     /**
-     * Lock 임대 시간 (DEFAULT: 3초)
+     * Lock 임대 시간 (DEFAULT: 5초)
      * LOCK 획득 이후 leaseTime이 지나면 LOCK을 해제한다.
      */
-    long leaseTime() default 3L;
+    long leaseTime() default 5L;
 
     /**
      * 동일한 트랜잭션에서 Lock을 획득할지 여부 (DEFAULT: true) <br/>
