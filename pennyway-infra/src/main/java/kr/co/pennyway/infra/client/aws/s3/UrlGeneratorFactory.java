7d7c29d43575774088af7b53e531fba810b9ba87
Jinwoo Lee <binaryWooo@gmail.com>
2024-05-27 20:28:31 +0900

    refactor: object-key 생성을 위한 url-generator 정의

---
+++ pennyway-infra/src/main/java/kr/co/pennyway/infra/client/aws/s3/UrlGeneratorFactory.java
@@ -0,0 +1,20 @@
+package kr.co.pennyway.infra.client.aws.s3;
+
+public class UrlGeneratorFactory {
+	public static UrlGenerator getUrlGenerator(ObjectKeyType type) {
+		switch (type) {
+			case PROFILE:
+				return new ProfileUrlGenerator();
+			case FEED:
+				return new FeedUrlGenerator();
+			case CHATROOM_PROFILE:
+				return new ChatroomProfileUrlGenerator();
+			case CHAT:
+				return new ChatUrlGenerator();
+			case CHAT_PROFILE:
+				return new ChatProfileUrlGenerator();
+			default:
+				throw new IllegalArgumentException("Invalid type: " + type);
+		}
+	}
+}
