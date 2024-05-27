c24ca1eff3bfdcf116d67707f1181b8a90b26d43
Jinwoo Lee <binaryWooo@gmail.com>
2024-05-27 15:30:03 +0900

    fix: user error-code와 중복된 코드 제거

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/common/exception/StorageErrorCode.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/common/exception/StorageErrorCode.java
@@ -11,10 +11,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum StorageErrorCode implements BaseErrorCode {
 	// 400 Bad Request
-	MISSING_REQUIRED_PARAMETER(StatusCode.BAD_REQUEST, ReasonCode.MISSING_REQUIRED_PARAMETER, "필수 파라미터가 누락되었습니다."),
-
-	// 404 Not Found
-	USER_NOT_FOUND(StatusCode.NOT_FOUND, ReasonCode.RESOURCE_DELETED_OR_MOVED, "사용자를 찾을 수 없습니다.");
+	MISSING_REQUIRED_PARAMETER(StatusCode.BAD_REQUEST, ReasonCode.MISSING_REQUIRED_PARAMETER, "필수 파라미터가 누락되었습니다.");
 
 	private final StatusCode statusCode;
 	private final ReasonCode reasonCode;
