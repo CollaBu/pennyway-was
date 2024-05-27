d6725d82b2bc5aec07eac1ef64490c8e8f490be0
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-27 20:50:59 +0900

    test: 분산 락이 없는 테스트 케이스 추가

--- pennyway-domain/src/test/java/kr/co/pennyway/domain/common/redisson/CouponDecreaseLockTest.java
+++ pennyway-domain/src/test/java/kr/co/pennyway/domain/common/redisson/CouponDecreaseLockTest.java
@@ -1,6 +1,5 @@
 package kr.co.pennyway.domain.common.redisson;
 
-import jakarta.persistence.EntityManager;
 import kr.co.pennyway.domain.config.ContainerDBTestConfig;
 import kr.co.pennyway.domain.config.DomainIntegrationTest;
 import kr.co.pennyway.domain.domains.coupon.TestCoupon;
@@ -11,7 +10,6 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
@@ -26,8 +24,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 @EntityScan(basePackageClasses = {TestCoupon.class})
 public class CouponDecreaseLockTest extends ContainerDBTestConfig {
     @Autowired
-    private EntityManager em;
-    @Autowired
     private TestCouponDecreaseService testCouponDecreaseService;
     @Autowired
     private TestCouponRepository testCouponRepository;
@@ -41,7 +37,6 @@ public class CouponDecreaseLockTest extends ContainerDBTestConfig {
 
     @Test
     @Order(1)
-    @Transactional
     void 쿠폰차감_분산락_적용_동시성_300명_테스트() throws InterruptedException {
         // given
         int threadCount = 300;
@@ -61,33 +56,33 @@ public class CouponDecreaseLockTest extends ContainerDBTestConfig {
         latch.await();
 
         // then
-        TestCoupon persistedCoupon = em.find(TestCoupon.class, coupon.getId());
+        TestCoupon persistedCoupon = testCouponRepository.findById(coupon.getId()).orElseThrow(IllegalArgumentException::new);
         assertThat(persistedCoupon.getAvailableStock()).isZero();
         log.debug("잔여 쿠폰 수량: " + persistedCoupon.getAvailableStock());
     }
 
-//    @Test
-//    @Order(2)
-//    void 쿠폰차감_분산락_미적용_동시성_100명_테스트() throws InterruptedException {
-//        // given
-//        int threadCount = 100;
-//        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
-//        CountDownLatch latch = new CountDownLatch(threadCount);
-//
-//        // when
-//        for (int i = 0; i < threadCount; i++) {
-//            executorService.submit(() -> {
-//                try {
-//                    testCouponDecreaseService.decreaseStock(coupon.getId());
-//                } finally {
-//                    latch.countDown();
-//                }
-//            });
-//        }
-//        latch.await();
-//
-//        // then
-//        TestCoupon persistedCoupon = em.find(TestCoupon.class, coupon.getId());
-//        log.debug("잔여 쿠폰 수량: " + persistedCoupon.getAvailableStock());
-//    }
+    @Test
+    @Order(2)
+    void 쿠폰차감_분산락_미적용_동시성_300명_테스트() throws InterruptedException {
+        // given
+        int threadCount = 300;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    testCouponDecreaseService.decreaseStock(coupon.getId());
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        // then
+        TestCoupon persistedCoupon = testCouponRepository.findById(coupon.getId()).orElseThrow(IllegalArgumentException::new);
+        log.debug("잔여 쿠폰 수량: " + persistedCoupon.getAvailableStock());
+    }
 }
