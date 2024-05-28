00f07e4a3a0e892298d16644702a4cd0f195bee0
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-05-28 21:38:27 +0900

    fix: batch application 클래스 main 함수 추가

--- pennyway-batch/src/main/java/kr/co/pennyway/PennywayBatchApplication.java
+++ pennyway-batch/src/main/java/kr/co/pennyway/PennywayBatchApplication.java
@@ -1,7 +1,11 @@
 package kr.co.pennyway;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PennywayBatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PennywayBatchApplication.class, args);
+    }
 }
