4a9e06d382ac46f8ef143e995874573bc56a9141
Jinwoo Lee <binaryWooo@gmail.com>
2024-06-04 02:56:39 +0900

    feat: user entity에 profile-image-url 수정 로직 추가

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/user/domain/User.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/user/domain/User.java
@@ -1,6 +1,23 @@
 package kr.co.pennyway.domain.domains.user.domain;
 
-import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import jakarta.persistence.Convert;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import kr.co.pennyway.domain.common.converter.ProfileVisibilityConverter;
 import kr.co.pennyway.domain.common.converter.RoleConverter;
 import kr.co.pennyway.domain.common.model.DateAuditable;
@@ -11,14 +28,6 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
-import org.hibernate.annotations.DynamicInsert;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.SQLRestriction;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
@@ -28,73 +37,78 @@ import java.util.List;
 @SQLRestriction("deleted_at IS NULL")
 @SQLDelete(sql = "UPDATE user SET deleted_at = NOW() WHERE id = ?")
 public class User extends DateAuditable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private String username;
-    private String name;
-    @ColumnDefault("NULL")
-    private String password;
-    @ColumnDefault("NULL")
-    private LocalDateTime passwordUpdatedAt;
-    @ColumnDefault("NULL")
-    private String profileImageUrl;
-    private String phone;
-    @Convert(converter = RoleConverter.class)
-    private Role role;
-    @Convert(converter = ProfileVisibilityConverter.class)
-    private ProfileVisibility profileVisibility;
-    @ColumnDefault("false")
-    private Boolean locked;
-    @Embedded
-    private NotifySetting notifySetting;
-    @ColumnDefault("NULL")
-    private LocalDateTime deletedAt;
+	private String username;
+	private String name;
+	@ColumnDefault("NULL")
+	private String password;
+	@ColumnDefault("NULL")
+	private LocalDateTime passwordUpdatedAt;
+	@ColumnDefault("NULL")
+	private String profileImageUrl;
+	private String phone;
+	@Convert(converter = RoleConverter.class)
+	private Role role;
+	@Convert(converter = ProfileVisibilityConverter.class)
+	private ProfileVisibility profileVisibility;
+	@ColumnDefault("false")
+	private Boolean locked;
+	@Embedded
+	private NotifySetting notifySetting;
+	@ColumnDefault("NULL")
+	private LocalDateTime deletedAt;
 
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
-    private List<Device> devices = new ArrayList<>();
+	@OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+	private List<Device> devices = new ArrayList<>();
 
-    @Builder
-    private User(String username, String name, String password, LocalDateTime passwordUpdatedAt, String profileImageUrl, String phone, Role role, ProfileVisibility profileVisibility, NotifySetting notifySetting, Boolean locked, LocalDateTime deletedAt) {
-        this.username = username;
-        this.name = name;
-        this.password = password;
-        this.passwordUpdatedAt = passwordUpdatedAt;
-        this.profileImageUrl = profileImageUrl;
-        this.phone = phone;
-        this.role = role;
-        this.profileVisibility = profileVisibility;
-        this.notifySetting = notifySetting;
-        this.locked = locked;
-        this.deletedAt = deletedAt;
-    }
+	@Builder
+	private User(String username, String name, String password, LocalDateTime passwordUpdatedAt, String profileImageUrl, String phone, Role role,
+			ProfileVisibility profileVisibility, NotifySetting notifySetting, Boolean locked, LocalDateTime deletedAt) {
+		this.username = username;
+		this.name = name;
+		this.password = password;
+		this.passwordUpdatedAt = passwordUpdatedAt;
+		this.profileImageUrl = profileImageUrl;
+		this.phone = phone;
+		this.role = role;
+		this.profileVisibility = profileVisibility;
+		this.notifySetting = notifySetting;
+		this.locked = locked;
+		this.deletedAt = deletedAt;
+	}
 
-    public void updatePassword(String password) {
-        this.password = password;
-        this.passwordUpdatedAt = LocalDateTime.now();
-    }
+	public void updatePassword(String password) {
+		this.password = password;
+		this.passwordUpdatedAt = LocalDateTime.now();
+	}
 
-    public void updateName(String name) {
-        this.name = name;
-    }
+	public void updateName(String name) {
+		this.name = name;
+	}
 
-    public void updateUsername(String username) {
-        this.username = username;
-    }
+	public void updateUsername(String username) {
+		this.username = username;
+	}
 
-    public boolean isGeneralSignedUpUser() {
-        return password != null;
-    }
+	public void updateProfileImageUrl(String profileImageUrl) {
+		this.profileImageUrl = profileImageUrl;
+	}
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", name='" + name + '\'' +
-                ", role=" + role +
-                ", deletedAt=" + deletedAt +
-                '}';
-    }
+	public boolean isGeneralSignedUpUser() {
+		return password != null;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"id=" + id +
+				", username='" + username + '\'' +
+				", name='" + name + '\'' +
+				", role=" + role +
+				", deletedAt=" + deletedAt +
+				'}';
+	}
 }
