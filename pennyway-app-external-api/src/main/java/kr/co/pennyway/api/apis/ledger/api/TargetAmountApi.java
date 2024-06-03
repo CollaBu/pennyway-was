d0e0d2c0375e3b9da1928aaba904b59ef40f21c1
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-06-03 20:42:48 +0900

    docs: target api 조회 시 404 예외 문서화

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/api/TargetAmountApi.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/api/TargetAmountApi.java
@@ -46,8 +46,17 @@ public interface TargetAmountApi {
 
     @Operation(summary = "임의의 년/월에 대한 목표 금액 및 총 사용 금액 조회", method = "GET", description = "일수는 무시하고 년/월 정보만 사용한다. 일반적으로 당월 정보 요청에 사용하는 API이다.")
     @Parameter(name = "date", description = "현재 날짜(yyyy-MM-dd)", required = true, example = "2024-05-08", in = ParameterIn.PATH)
-    @ApiResponse(responseCode = "200", description = "목표 금액 및 총 사용 금액 조회 성공", content = @Content(
-            schemaProperties = @SchemaProperty(name = "targetAmount", schema = @Schema(implementation = TargetAmountDto.WithTotalSpendingRes.class))))
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "목표 금액 및 총 사용 금액 조회 성공", content = @Content(
+                    schemaProperties = @SchemaProperty(name = "targetAmount", schema = @Schema(implementation = TargetAmountDto.WithTotalSpendingRes.class)))),
+            @ApiResponse(responseCode = "404", content = @Content(mediaType = "application/json", examples = {
+                    @ExampleObject(name = "목표 금액 조회 실패", description = "목표 금액 데이터가 없거나, 이미 삭제(amount=-1)인 경우", value = """
+                            {
+                                "code": "4040",
+                                "message": "해당 월의 목표 금액이 존재하지 않습니다."
+                            }
+                            """)}))
+    })
     ResponseEntity<?> getTargetAmountAndTotalSpending(@PathVariable LocalDate date, @AuthenticationPrincipal SecurityUserDetails user);
 
     @Operation(summary = "사용자 가입 이후 현재까지의 목표 금액 및 총 사용 금액 리스트 조회", method = "GET", description = "일수는 무시하고 년/월 정보만 사용한다. 데이터가 존재하지 않을 때 더미 값을 사용하며, 최신 데이터 순으로 정렬된 응답을 반환한다.")
