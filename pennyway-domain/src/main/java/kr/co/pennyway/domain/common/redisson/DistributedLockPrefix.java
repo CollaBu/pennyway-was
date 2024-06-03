51a3d2138c42a5bc0a8a398d19b394edd2aaf078
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-06-03 11:32:31 +0900

    fix: lock prefix 수정

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/common/redisson/DistributedLockPrefix.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/common/redisson/DistributedLockPrefix.java
@@ -4,5 +4,5 @@ package kr.co.pennyway.domain.common.redisson;
  * 분산 락을 위한 prefix
  */
 public class DistributedLockPrefix {
-    public static final String TARGET_AMOUNT_USER = "TargetAmount:User:";
+    public static final String TARGET_AMOUNT_USER = "TargetAmount_User_";
 }
