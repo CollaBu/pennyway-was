e68e7a521a85499690daf25c949ed7771fef870c
Jinwoo Lee <binaryWooo@gmail.com>
2024-06-04 23:31:56 +0900

    rename: s3 파일 존재 여부 메서드명 수정

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/users/service/UserProfileUpdateService.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/users/service/UserProfileUpdateService.java
@@ -1,8 +1,5 @@
 package kr.co.pennyway.api.apis.users.service;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import kr.co.pennyway.api.apis.users.helper.PasswordEncoderHelper;
 import kr.co.pennyway.domain.domains.user.domain.NotifySetting;
 import kr.co.pennyway.domain.domains.user.domain.User;
@@ -14,56 +11,58 @@ import kr.co.pennyway.infra.common.exception.StorageErrorCode;
 import kr.co.pennyway.infra.common.exception.StorageException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserProfileUpdateService {
-	private final PasswordEncoderHelper passwordEncoderHelper;
-	private final AwsS3Provider awsS3Provider;
+    private final PasswordEncoderHelper passwordEncoderHelper;
+    private final AwsS3Provider awsS3Provider;
 
-	@Transactional
-	public void updateName(User user, String newName) {
-		user.updateName(newName);
-	}
+    @Transactional
+    public void updateName(User user, String newName) {
+        user.updateName(newName);
+    }
 
-	@Transactional
-	public void updateUsername(User user, String newUsername) {
-		user.updateUsername(newUsername);
-	}
+    @Transactional
+    public void updateUsername(User user, String newUsername) {
+        user.updateUsername(newUsername);
+    }
 
-	@Transactional
-	public void updatePassword(User user, String oldPassword, String newPassword) {
-		if (passwordEncoderHelper.isSamePassword(user.getPassword(), newPassword)) {
-			log.info("기존과 동일한 비밀번호로는 변경할 수 없습니다.");
-			throw new UserErrorException(UserErrorCode.PASSWORD_NOT_CHANGED);
-		}
+    @Transactional
+    public void updatePassword(User user, String oldPassword, String newPassword) {
+        if (passwordEncoderHelper.isSamePassword(user.getPassword(), newPassword)) {
+            log.info("기존과 동일한 비밀번호로는 변경할 수 없습니다.");
+            throw new UserErrorException(UserErrorCode.PASSWORD_NOT_CHANGED);
+        }
 
-		user.updatePassword(passwordEncoderHelper.encodePassword(newPassword));
-	}
+        user.updatePassword(passwordEncoderHelper.encodePassword(newPassword));
+    }
 
-	@Transactional
-	public void updateProfileImage(User user, String profileImageUrl) {
-		// Profile Image 존재 여부 확인
-		if (!awsS3Provider.doesObjectExist(profileImageUrl)) {
-			log.info("프로필 이미지 URL이 유효하지 않습니다.");
-			throw new StorageException(StorageErrorCode.NOT_FOUND);
-		}
+    @Transactional
+    public void updateProfileImage(User user, String profileImageUrl) {
+        // Profile Image 존재 여부 확인
+        if (!awsS3Provider.isObjectExist(profileImageUrl)) {
+            log.info("프로필 이미지 URL이 유효하지 않습니다.");
+            throw new StorageException(StorageErrorCode.NOT_FOUND);
+        }
 
-		// Profile Image 원본 저장
-		awsS3Provider.copyObject(ObjectKeyType.PROFILE, profileImageUrl);
+        // Profile Image 원본 저장
+        awsS3Provider.copyObject(ObjectKeyType.PROFILE, profileImageUrl);
 
-		// Profile Image URL 업데이트
-		String originKey = ObjectKeyType.PROFILE.convertDeleteKeyToOriginKey(profileImageUrl);
-		user.updateProfileImageUrl(getObjectPrefix() + originKey);
-	}
+        // Profile Image URL 업데이트
+        String originKey = ObjectKeyType.PROFILE.convertDeleteKeyToOriginKey(profileImageUrl);
+        user.updateProfileImageUrl(getObjectPrefix() + originKey);
+    }
 
-	@Transactional
-	public void updateNotifySetting(User user, NotifySetting.NotifyType type, Boolean flag) {
-		user.getNotifySetting().updateNotifySetting(type, flag);
-	}
+    @Transactional
+    public void updateNotifySetting(User user, NotifySetting.NotifyType type, Boolean flag) {
+        user.getNotifySetting().updateNotifySetting(type, flag);
+    }
 
-	private String getObjectPrefix() {
-		return "https://cdn.dev.pennyway.co.kr/";
-	}
+    private String getObjectPrefix() {
+        return "https://cdn.dev.pennyway.co.kr/";
+    }
 }
--- pennyway-infra/src/main/java/kr/co/pennyway/infra/client/aws/s3/AwsS3Provider.java
+++ pennyway-infra/src/main/java/kr/co/pennyway/infra/client/aws/s3/AwsS3Provider.java
@@ -1,145 +1,144 @@
 package kr.co.pennyway.infra.client.aws.s3;
 
-import java.net.URI;
-import java.time.Duration;
-import java.util.Map;
-import java.util.Set;
-
-import org.springframework.stereotype.Component;
-
 import kr.co.pennyway.infra.common.exception.StorageErrorCode;
 import kr.co.pennyway.infra.common.exception.StorageException;
 import kr.co.pennyway.infra.config.AwsS3Config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.CopyObjectRequest;
-import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
-import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
-import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.StorageClass;
+import software.amazon.awssdk.services.s3.model.*;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+
+import java.net.URI;
+import java.time.Duration;
+import java.util.Map;
+import java.util.Set;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class AwsS3Provider {
-	private static final Set<String> extensionSet = Set.of("jpg", "png", "jpeg");
+    private static final Set<String> extensionSet = Set.of("jpg", "png", "jpeg");
 
-	private final AwsS3Config awsS3Config;
-	private final S3Presigner s3Presigner;
-	private final S3Client s3Client;
+    private final AwsS3Config awsS3Config;
+    private final S3Presigner s3Presigner;
+    private final S3Client s3Client;
 
-	/**
-	 * type에 해당하는 확장자를 가진 파일을 S3에 저장하기 위한 Presigned URL을 생성한다.
-	 * @param type : ObjectKeyType (PROFILE, FEED, CHATROOM_PROFILE, CHAT, CHAT_PROFILE)
-	 * @param ext : 파일 확장자 (jpg, png, jpeg)
-	 * @param userId : 사용자 ID (PK) - PROFILE, CHAT_PROFILE
-	 * @param chatroomId : 채팅방 ID (PK) - CHATROOM_PROFILE, CHAT, CHAT_PROFILE
-	 * @return Presigned URL
-	 * @throws Exception
-	 */
-	public URI generatedPresignedUrl(String type, String ext, String userId, String chatroomId) {
-		try {
-			if (!extensionSet.contains(ext)) {
-				throw new StorageException(StorageErrorCode.INVALID_EXTENSION);
-			}
+    /**
+     * type에 해당하는 확장자를 가진 파일을 S3에 저장하기 위한 Presigned URL을 생성한다.
+     *
+     * @param type       : ObjectKeyType (PROFILE, FEED, CHATROOM_PROFILE, CHAT, CHAT_PROFILE)
+     * @param ext        : 파일 확장자 (jpg, png, jpeg)
+     * @param userId     : 사용자 ID (PK) - PROFILE, CHAT_PROFILE
+     * @param chatroomId : 채팅방 ID (PK) - CHATROOM_PROFILE, CHAT, CHAT_PROFILE
+     * @return Presigned URL
+     * @throws Exception
+     */
+    public URI generatedPresignedUrl(String type, String ext, String userId, String chatroomId) {
+        try {
+            if (!extensionSet.contains(ext)) {
+                throw new StorageException(StorageErrorCode.INVALID_EXTENSION);
+            }
 
-			PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-					.bucket(awsS3Config.getBucketName())
-					.key(generateObjectKey(type, ext, userId, chatroomId))
-					.build();
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(awsS3Config.getBucketName())
+                    .key(generateObjectKey(type, ext, userId, chatroomId))
+                    .build();
 
-			PresignedPutObjectRequest presignedRequest = s3Presigner.presignPutObject(r -> r.putObjectRequest(putObjectRequest)
-					.signatureDuration(Duration.ofMinutes(10)));
+            PresignedPutObjectRequest presignedRequest = s3Presigner.presignPutObject(r -> r.putObjectRequest(putObjectRequest)
+                    .signatureDuration(Duration.ofMinutes(10)));
 
-			return presignedRequest.url().toURI();
-		} catch (Exception e) {
-			log.error("Presigned URL 생성 중 오류 발생", e);
-			throw new StorageException(StorageErrorCode.MISSING_REQUIRED_PARAMETER);
-		}
-	}
+            return presignedRequest.url().toURI();
+        } catch (Exception e) {
+            log.error("Presigned URL 생성 중 오류 발생", e);
+            throw new StorageException(StorageErrorCode.MISSING_REQUIRED_PARAMETER);
+        }
+    }
 
-	/**
-	 * type에 해당하는 ObjectKeyTemplate을 적용하여 ObjectKey(S3에 저장하기 위한 정적 파일의 경로 및 이름)를 생성한다.
-	 * @param type : ObjectKeyType (PROFILE, FEED, CHATROOM_PROFILE, CHAT, CHAT_PROFILE)
-	 * @param ext : 파일 확장자 (jpg, png, jpeg)
-	 * @param userId : 사용자 ID (PK) - PROFILE, CHAT_PROFILE
-	 * @param chatroomId : 채팅방 ID (PK) - CHATROOM_PROFILE, CHAT, CHAT_PROFILE
-	 * @return ObjectKey
-	 */
-	private String generateObjectKey(String type, String ext, String userId, String chatroomId) {
-		ObjectKeyTemplate objectKeyTemplate = new ObjectKeyTemplate(ObjectKeyType.valueOf(type).getDeleteTemplate());
-		Map<String, String> variables = generateObjectKeyVariables(type, ext, userId, chatroomId);
-		String objectKey = objectKeyTemplate.apply(variables);
-		return objectKey;
+    /**
+     * type에 해당하는 ObjectKeyTemplate을 적용하여 ObjectKey(S3에 저장하기 위한 정적 파일의 경로 및 이름)를 생성한다.
+     *
+     * @param type       : ObjectKeyType (PROFILE, FEED, CHATROOM_PROFILE, CHAT, CHAT_PROFILE)
+     * @param ext        : 파일 확장자 (jpg, png, jpeg)
+     * @param userId     : 사용자 ID (PK) - PROFILE, CHAT_PROFILE
+     * @param chatroomId : 채팅방 ID (PK) - CHATROOM_PROFILE, CHAT, CHAT_PROFILE
+     * @return ObjectKey
+     */
+    private String generateObjectKey(String type, String ext, String userId, String chatroomId) {
+        ObjectKeyTemplate objectKeyTemplate = new ObjectKeyTemplate(ObjectKeyType.valueOf(type).getDeleteTemplate());
+        Map<String, String> variables = generateObjectKeyVariables(type, ext, userId, chatroomId);
+        String objectKey = objectKeyTemplate.apply(variables);
+        return objectKey;
 
-	}
+    }
 
-	/**
-	 * ObjectKey에 사용될 변수들을 Template에 적용하기 위한 Map에 담아 반환한다.
-	 * @param type : ObjectKeyType (PROFILE, FEED, CHATROOM_PROFILE, CHAT, CHAT_PROFILE)
-	 * @param ext : 파일 확장자 (jpg, png, jpeg)
-	 * @param userId : 사용자 ID (PK) - PROFILE, CHAT_PROFILE
-	 * @param chatroomId : 채팅방 ID (PK) - CHATROOM_PROFILE, CHAT, CHAT_PROFILE
-	 * @return
-	 */
-	private Map<String, String> generateObjectKeyVariables(String type, String ext, String userId, String chatroomId) {
-		ObjectKeyType objectType;
-		try {
-			objectType = ObjectKeyType.valueOf(type);
-		} catch (IllegalArgumentException e) {
-			throw new StorageException(StorageErrorCode.INVALID_TYPE);
-		}
+    /**
+     * ObjectKey에 사용될 변수들을 Template에 적용하기 위한 Map에 담아 반환한다.
+     *
+     * @param type       : ObjectKeyType (PROFILE, FEED, CHATROOM_PROFILE, CHAT, CHAT_PROFILE)
+     * @param ext        : 파일 확장자 (jpg, png, jpeg)
+     * @param userId     : 사용자 ID (PK) - PROFILE, CHAT_PROFILE
+     * @param chatroomId : 채팅방 ID (PK) - CHATROOM_PROFILE, CHAT, CHAT_PROFILE
+     * @return
+     */
+    private Map<String, String> generateObjectKeyVariables(String type, String ext, String userId, String chatroomId) {
+        ObjectKeyType objectType;
+        try {
+            objectType = ObjectKeyType.valueOf(type);
+        } catch (IllegalArgumentException e) {
+            throw new StorageException(StorageErrorCode.INVALID_TYPE);
+        }
 
-		UrlGenerator urlGenerator = UrlGeneratorFactory.getUrlGenerator(objectType);
-		return urlGenerator.generate(type, ext, userId, chatroomId);
-	}
+        UrlGenerator urlGenerator = UrlGeneratorFactory.getUrlGenerator(objectType);
+        return urlGenerator.generate(type, ext, userId, chatroomId);
+    }
 
-	/**
-	 * S3에 파일이 존재하는지 확인한다.
-	 * @param key : S3 버킷 내의 파일 키
-	 * @return 파일이 존재하면 true, 존재하지 않으면 false
-	 */
-	public boolean doesObjectExist(String key) {
-		try {
-			HeadObjectRequest headObjectRequest = HeadObjectRequest.builder()
-					.bucket(awsS3Config.getBucketName())
-					.key(key)
-					.build();
+    /**
+     * S3에 파일이 존재하는지 확인한다.
+     *
+     * @param key : S3 버킷 내의 파일 키
+     * @return 파일이 존재하면 true, 존재하지 않으면 false
+     */
+    public boolean isObjectExist(String key) {
+        try {
+            HeadObjectRequest headObjectRequest = HeadObjectRequest.builder()
+                    .bucket(awsS3Config.getBucketName())
+                    .key(key)
+                    .build();
 
-			HeadObjectResponse headObjectResponse = s3Client.headObject(headObjectRequest);
-			return true;
-		} catch (NoSuchKeyException e) {
-			return false;
-		} catch (Exception e) {
-			log.error("파일 존재 여부 확인 중 오류 발생", e);
-			throw new StorageException(StorageErrorCode.NOT_FOUND);
-		}
-	}
+            HeadObjectResponse headObjectResponse = s3Client.headObject(headObjectRequest);
+            return true;
+        } catch (NoSuchKeyException e) {
+            return false;
+        } catch (Exception e) {
+            log.error("파일 존재 여부 확인 중 오류 발생", e);
+            throw new StorageException(StorageErrorCode.NOT_FOUND);
+        }
+    }
 
-	/**
-	 * S3에 저장된 파일을 복사한다.
-	 * @param type : ObjectKeyType (PROFILE, FEED, CHATROOM_PROFILE, CHAT, CHAT_PROFILE)
-	 * @param sourceKey : 복사할 파일의 키
-	 * @return 복사된 파일의 키
-	 */
-	public void copyObject(ObjectKeyType type, String sourceKey) {
-		try {
-			CopyObjectRequest copyObjRequest = CopyObjectRequest.builder()
-					.sourceBucket(awsS3Config.getBucketName())
-					.sourceKey(sourceKey)
-					.destinationBucket(awsS3Config.getBucketName())
-					.destinationKey(type.convertDeleteKeyToOriginKey(sourceKey))
-					.storageClass(StorageClass.ONEZONE_IA)
-					.build();
+    /**
+     * S3에 저장된 파일을 복사한다.
+     *
+     * @param type      : ObjectKeyType (PROFILE, FEED, CHATROOM_PROFILE, CHAT, CHAT_PROFILE)
+     * @param sourceKey : 복사할 파일의 키
+     * @return 복사된 파일의 키
+     */
+    public void copyObject(ObjectKeyType type, String sourceKey) {
+        try {
+            CopyObjectRequest copyObjRequest = CopyObjectRequest.builder()
+                    .sourceBucket(awsS3Config.getBucketName())
+                    .sourceKey(sourceKey)
+                    .destinationBucket(awsS3Config.getBucketName())
+                    .destinationKey(type.convertDeleteKeyToOriginKey(sourceKey))
+                    .storageClass(StorageClass.ONEZONE_IA)
+                    .build();
 
-			s3Client.copyObject(copyObjRequest);
-		} catch (Exception e) {
-			log.error("파일 복사 중 오류 발생", e);
-			throw new StorageException(StorageErrorCode.INVALID_FILE);
-		}
-	}
+            s3Client.copyObject(copyObjRequest);
+        } catch (Exception e) {
+            log.error("파일 복사 중 오류 발생", e);
+            throw new StorageException(StorageErrorCode.INVALID_FILE);
+        }
+    }
 }
