5bc4703d491ab39296fc3345ffa0839409e887a5
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-06-04 20:03:39 +0900

    test: device token 단위 테스트 분리

--- pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/users/usecase/UserAccountUseCaseTest.java
+++ pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/users/usecase/UserAccountUseCaseTest.java
@@ -1,7 +1,6 @@
 package kr.co.pennyway.api.apis.users.usecase;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import kr.co.pennyway.api.apis.users.dto.DeviceTokenDto;
 import kr.co.pennyway.api.apis.users.helper.PasswordEncoderHelper;
 import kr.co.pennyway.api.apis.users.service.UserDeleteService;
 import kr.co.pennyway.api.apis.users.service.UserProfileUpdateService;
@@ -65,81 +64,6 @@ class UserAccountUseCaseTest extends ExternalApiDBTestConfig {
 
     @MockBean
     private JPAQueryFactory queryFactory;
-
-    @Order(1)
-    @Nested
-    @DisplayName("[1] 디바이스 등록 테스트")
-    class DeviceTokenRegisterTest {
-        private User requestUser;
-
-        @BeforeEach
-        void setUp() {
-            User user = User.builder().role(Role.USER).profileVisibility(ProfileVisibility.PUBLIC).build();
-            requestUser = userService.createUser(user);
-        }
-
-        @Test
-        @Transactional
-        @DisplayName("[1] token 등록 요청이 들어왔을 때, 새로운 디바이스 토큰을 등록한다.")
-        void registerNewDevice() {
-            // given
-            DeviceTokenDto.RegisterReq request = DeviceTokenFixture.INIT.toRegisterReq();
-
-            // when
-            DeviceTokenDto.RegisterRes response = userAccountUseCase.registerDeviceToken(requestUser.getId(), request);
-
-            // then
-            deviceTokenService.readDeviceByUserIdAndToken(requestUser.getId(), request.token()).ifPresentOrElse(
-                    device -> {
-                        assertEquals("요청한 디바이스 토큰과 동일해야 한다.", response.token(), device.getToken());
-                        assertEquals("디바이스 ID가 일치해야 한다.", response.id(), device.getId());
-                        assertTrue("디바이스가 사용자 ID와 연결되어 있어야 한다.", device.getUser().getId().equals(requestUser.getId()));
-                        System.out.println("device = " + device);
-                    },
-                    () -> fail("신규 디바이스가 등록되어 있어야 한다.")
-            );
-        }
-
-        @Test
-        @Transactional
-        @DisplayName("[2] token에 대한 활성화 디바이스 토큰이 이미 존재하는 경우 기존 데이터를 반환한다.")
-        void registerNewDeviceWhenDeviceIsAlreadyExists() {
-            // given
-            DeviceToken originDeviceToken = DeviceTokenFixture.INIT.toDevice(requestUser);
-            deviceTokenService.createDevice(originDeviceToken);
-            DeviceTokenDto.RegisterReq request = DeviceTokenFixture.INIT.toRegisterReq();
-
-            // when
-            DeviceTokenDto.RegisterRes response = userAccountUseCase.registerDeviceToken(requestUser.getId(), request);
-
-            // then
-            deviceTokenService.readDeviceByUserIdAndToken(requestUser.getId(), request.token()).ifPresentOrElse(
-                    device -> {
-                        assertEquals("요청한 디바이스 토큰과 동일해야 한다.", response.token(), device.getToken());
-                        assertEquals("디바이스 ID가 일치해야 한다.", originDeviceToken.getId(), device.getId());
-                        assertTrue("디바이스가 사용자 ID와 연결되어 있어야 한다.", device.getUser().getId().equals(requestUser.getId()));
-                        assertTrue("디바이스가 활성화 상태여야 한다.", device.getActivated());
-                        System.out.println("device = " + device);
-                    },
-                    () -> fail("신규 디바이스가 등록되어 있어야 한다.")
-            );
-        }
-
-        @Test
-        @Transactional
-        @DisplayName("[3] token 등록 요청이 들어왔을 때, 활성화되지 않은 디바이스 토큰이 존재하는 경우 NOT_ACTIVATED_DEVICE 에러를 반환한다.")
-        void registerNewDeviceWhenDeviceIsNotActivated() {
-            // given
-            DeviceToken originDeviceToken = DeviceTokenFixture.INIT.toDevice(requestUser);
-            originDeviceToken.deactivate();
-            deviceTokenService.createDevice(originDeviceToken);
-            DeviceTokenDto.RegisterReq request = DeviceTokenFixture.INIT.toRegisterReq();
-
-            // when - then
-            DeviceTokenErrorException ex = assertThrows(DeviceTokenErrorException.class, () -> userAccountUseCase.registerDeviceToken(requestUser.getId(), request));
-            assertEquals("활성화되지 않은 디바이스 토큰이 존재하는 경우 Not Activated Device를 반환한다.", DeviceTokenErrorCode.NOT_ACTIVATED_DEVICE, ex.getBaseErrorCode());
-        }
-    }
 
     @Order(2)
     @Nested
