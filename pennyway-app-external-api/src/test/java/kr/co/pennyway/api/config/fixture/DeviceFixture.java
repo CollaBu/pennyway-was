3d168c31c5367bddc9a160a5b1fb60a2dba38c1b
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-06-04 12:23:34 +0900

    fix: device_fixture model, os 필드 제거

--- pennyway-app-external-api/src/test/java/kr/co/pennyway/api/config/fixture/DeviceFixture.java
+++ pennyway-app-external-api/src/test/java/kr/co/pennyway/api/config/fixture/DeviceFixture.java
@@ -5,22 +5,15 @@ import kr.co.pennyway.domain.domains.device.domain.Device;
 import kr.co.pennyway.domain.domains.user.domain.User;
 
 public enum DeviceFixture {
-    INIT("originToken", "originToken", "modelA", "Windows 11"),
-    ORIGIN_DEVICE("originToken", "originToken", "modelA", "Windows 11"),
-    ONLY_TOKEN_CHANGED("originToken", "newToken", "modelA", "Windows 11"),
-    ONLY_MODEL_AND_OS_CHANGED("originToken", "originToken", "modelB", "Windows 11"),
-    ALL_CHANGED("originToken", "newToken", "modelB", "Mac OS X");
+    INIT("originToken", "originToken"),
+    ONLY_TOKEN_CHANGED("originToken", "newToken");
 
     private final String originToken;
     private final String newToken;
-    private final String model;
-    private final String os;
 
-    DeviceFixture(String originToken, String newToken, String model, String os) {
+    DeviceFixture(String originToken, String newToken) {
         this.originToken = originToken;
         this.newToken = newToken;
-        this.model = model;
-        this.os = os;
     }
 
     public Device toDevice(User user) {
