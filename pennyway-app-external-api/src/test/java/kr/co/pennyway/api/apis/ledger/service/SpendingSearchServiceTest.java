27a3a4afdfe74c99b5a9e523c8af37b81d349134
DinoDeveloper <asn6878@gmail.com>
2024-06-06 19:41:50 +0900

    test: 테스트 assertion 수정

--- pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/ledger/service/SpendingSearchServiceTest.java
+++ pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/ledger/service/SpendingSearchServiceTest.java
@@ -1,5 +1,7 @@
 package kr.co.pennyway.api.apis.ledger.service;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import kr.co.pennyway.api.config.ExternalApiDBTestConfig;
 import kr.co.pennyway.api.config.ExternalApiIntegrationTest;
 import kr.co.pennyway.api.config.fixture.SpendingFixture;
@@ -8,8 +10,10 @@ import kr.co.pennyway.domain.domains.spending.domain.Spending;
 import kr.co.pennyway.domain.domains.user.domain.User;
 import kr.co.pennyway.domain.domains.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.stat.Statistics;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +22,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @Slf4j
 @ExtendWith(MockitoExtension.class)
@@ -32,6 +34,23 @@ class SpendingSearchServiceTest extends ExternalApiDBTestConfig {
     @Autowired
     private NamedParameterJdbcTemplate jdbcTemplate;
 
+    @PersistenceContext
+    private EntityManager entityManager;
+    private Statistics statistics;
+
+
+    @BeforeEach
+    public void setUp() {
+        SessionFactoryImplementor sessionFactory = (SessionFactoryImplementor) ((SessionImplementor) entityManager.getDelegate()).getSessionFactory();
+        statistics = sessionFactory.getStatistics();
+        statistics.setStatisticsEnabled(true);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        statistics.clear();
+    }
+
     @Test
     @Transactional
     @DisplayName("커스텀 카테고리 지출 내역을 기간별 조회시 카테고리를 바로 fetch 한다.")
@@ -40,11 +59,21 @@ class SpendingSearchServiceTest extends ExternalApiDBTestConfig {
         User user = userService.createUser(UserFixture.GENERAL_USER.toUser());
         SpendingFixture.bulkInsertSpending(user, 100, true, jdbcTemplate);
 
-        // when - then
+        // when
         List<Spending> spendings = spendingSearchService.readSpendings(user.getId(), LocalDate.now().getYear(), LocalDate.now().getMonthValue());
-        assertDoesNotThrow(() -> {
-            String categoryName = spendings.get(0).getSpendingCustomCategory().getName();
-            log.info("{}", categoryName);
-        });
+        int size = spendings.size();
+        for (Spending spending : spendings) {
+            log.info("지출내역 id : {} 커스텀 카테고리 id : {} 커스텀 카테고리 name : {}",
+                    spending.getId(),
+                    spending.getSpendingCustomCategory().getId(),
+                    spending.getSpendingCustomCategory().getName()
+            );
+        }
+
+        // then
+        log.info("쿼리문 실행 횟수: {}", statistics.getPrepareStatementCount());
+        log.info("readSpendings로 조회해온 지출 내역 개수: {}", size);
+
+        Assertions.assertEquals(2, statistics.getPrepareStatementCount());
     }
 }
