1dc696ee7bd99752371cd0b2f5debfdd31919093
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-06-25 12:30:09 +0900

    refactor: get_spending_categories usecase 리팩토링

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/mapper/SpendingCategoryMapper.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/mapper/SpendingCategoryMapper.java
@@ -5,9 +5,17 @@ import kr.co.pennyway.common.annotation.Mapper;
 import kr.co.pennyway.domain.domains.spending.domain.SpendingCustomCategory;
 import kr.co.pennyway.domain.domains.spending.dto.CategoryInfo;
 
+import java.util.List;
+
 @Mapper
 public class SpendingCategoryMapper {
-    public static SpendingCategoryDto.Res toRes(SpendingCustomCategory category) {
+    public static SpendingCategoryDto.Res toResponse(SpendingCustomCategory category) {
         return SpendingCategoryDto.Res.from(CategoryInfo.of(category.getId(), category.getName(), category.getIcon()));
+    }
+
+    public static List<SpendingCategoryDto.Res> toResponses(List<SpendingCustomCategory> categories) {
+        return categories.stream()
+                .map(SpendingCategoryMapper::toResponse)
+                .toList();
     }
 }
