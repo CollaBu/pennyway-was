8ed7c8c0c90db7cbb6e2701383c152de3a5b1c7a
DinoDeveloper <79460319+asn6878@users.noreply.github.com>
2024-06-26 22:46:06 +0900

    fix: ✏️ 문의하기 API content 글자 수 제한 및 사용자 이름 정규식 수정 (#116)
    * fix: 문의하기 api content dto 글자수 제한
    
    * test: content 글자수 제한 테스트 작성
    
    * fix: username 정규표현식 수정
    
    * test: 수정된 username 조건에 맞추어 test 수정

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/dto/SignUpReq.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/auth/dto/SignUpReq.java
@@ -64,7 +64,7 @@ public class SignUpReq {
             String username,
             @Schema(description = "이름", example = "페니웨이")
             @NotBlank(message = "이름을 입력해주세요")
-            @Pattern(regexp = "^[가-힣a-z]{2,8}$", message = "2~8자의 한글, 영문 소문자만 사용 가능합니다.")
+            @Pattern(regexp = "^[가-힣a-z0-9]{2,8}$", message = "2~8자의 한글, 영문 소문자, 숫자만 사용 가능합니다.")
             String name,
             @Schema(description = "비밀번호", example = "pennyway1234")
             @NotBlank(message = "비밀번호를 입력해주세요")
--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/question/dto/QuestionReq.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/question/dto/QuestionReq.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import kr.co.pennyway.domain.domains.question.domain.Question;
 import kr.co.pennyway.domain.domains.question.domain.QuestionCategory;
 
@@ -13,7 +14,7 @@ public record QuestionReq(
         @Email(message = "이메일 형식이 올바르지 않습니다.")
         String email,
         @Schema(description = "문의 내용", example = "문의 내용입니다.")
-        @NotBlank(message = "문의 내용을 입력해주세요")
+        @Size(min = 1, max = 5000, message = "문의 내용은 1자 이상 5000자 이하로 입력해주세요")
         String content,
         @Schema(description = "문의 카테고리", example = "UTILIZATION")
         @NotNull(message = "문의 카테고리를 입력해주세요")
