51e482a96994abd2422d53590bbbbc917fa73369
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-01 20:54:33 +0900

    feat: 지출 카테고리 타입 상수 web-config conveter 정의 후 등록

---
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/common/converter/SpendingCategoryTypeConverter.java
@@ -0,0 +1,17 @@
+package kr.co.pennyway.api.common.converter;
+
+import kr.co.pennyway.api.common.query.SpendingCategoryType;
+import kr.co.pennyway.domain.domains.spending.exception.SpendingErrorCode;
+import kr.co.pennyway.domain.domains.spending.exception.SpendingErrorException;
+import org.springframework.core.convert.converter.Converter;
+
+public class SpendingCategoryTypeConverter implements Converter<String, SpendingCategoryType> {
+    @Override
+    public SpendingCategoryType convert(String type) {
+        try {
+            return SpendingCategoryType.valueOf(type.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new SpendingErrorException(SpendingErrorCode.INVALIE_CATEGORY_TYPE);
+        }
+    }
+}
