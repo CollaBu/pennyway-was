6bca2cdacabd449df0c687270cd6132e45e1d8b6
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-01 20:29:04 +0900

    test: controller unit test 작성

---
+++ pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/ledger/controller/GetSpendingsByCategoryControllerTest.java
@@ -0,0 +1,41 @@
+package kr.co.pennyway.api.apis.ledger.controller;
+
+import kr.co.pennyway.api.apis.ledger.usecase.SpendingCategoryUseCase;
+import kr.co.pennyway.api.config.WebConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+
+@WebMvcTest(controllers = SpendingCategoryController.class, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebConfig.class)})
+@ActiveProfiles("test")
+public class GetSpendingsByCategoryControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SpendingCategoryUseCase spendingCategoryUseCase;
+
+    @BeforeEach
+    void setUp(WebApplicationContext webApplicationContext) {
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .defaultRequest(MockMvcRequestBuilders.get("/**").with(csrf()))
+                .build();
+    }
+
+    private ResultActions performGetSpendingsByCategory(Long categoryId) throws Exception {
+        return mockMvc.perform(MockMvcRequestBuilders.get("/api/v2/spending-categories/{categoryId}/spendings", categoryId));
+    }
+}
