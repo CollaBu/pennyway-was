7f5c17f29daa4013aae3b7c718b7413644b547f9
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-01 21:47:37 +0900

    test: 400 error -> 422 에러 수정

--- pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/ledger/controller/GetSpendingsByCategoryControllerTest.java
+++ pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/ledger/controller/GetSpendingsByCategoryControllerTest.java
@@ -2,15 +2,14 @@ package kr.co.pennyway.api.apis.ledger.controller;
 
 import kr.co.pennyway.api.apis.ledger.usecase.SpendingCategoryUseCase;
 import kr.co.pennyway.api.common.query.SpendingCategoryType;
-import kr.co.pennyway.api.config.WebConfig;
+import kr.co.pennyway.domain.common.redis.sign.SignEventLogService;
+import kr.co.pennyway.infra.common.jwt.JwtProvider;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -19,10 +18,10 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = SpendingCategoryController.class, excludeFilters = {
-        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebConfig.class)})
+@WebMvcTest(controllers = SpendingCategoryController.class)
 @ActiveProfiles("test")
 public class GetSpendingsByCategoryControllerTest {
     @Autowired
@@ -30,6 +29,10 @@ public class GetSpendingsByCategoryControllerTest {
 
     @MockBean
     private SpendingCategoryUseCase spendingCategoryUseCase;
+    @MockBean
+    private SignEventLogService signEventLogService;
+    @MockBean
+    private JwtProvider accessTokenProvider;
 
     @BeforeEach
     void setUp(WebApplicationContext webApplicationContext) {
@@ -44,15 +47,16 @@ public class GetSpendingsByCategoryControllerTest {
     void getSpendingsByCategory() throws Exception {
         performGetSpendingsByCategory(1L, SpendingCategoryType.DEFAULT.name())
                 .andExpect(status().isOk());
-        performGetSpendingsByCategory(1L, SpendingCategoryType.CUSTOM.name())
+        performGetSpendingsByCategory(1L, SpendingCategoryType.CUSTOM.name().toLowerCase())
                 .andExpect(status().isOk());
     }
 
     @Test
-    @DisplayName("잘못된 타입을 조회하면 400 에러가 발생한다.")
+    @DisplayName("잘못된 타입을 조회하면 422 에러가 발생한다.")
     void getSpendingsByCategory_InvalidType() throws Exception {
         performGetSpendingsByCategory(1L, "invalid")
-                .andExpect(status().isBadRequest());
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
     }
 
     private ResultActions performGetSpendingsByCategory(Long categoryId, String type) throws Exception {
