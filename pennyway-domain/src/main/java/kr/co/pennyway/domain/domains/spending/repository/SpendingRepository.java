befe7b78f96ad17deb8d7b23bc1602830d33b515
asn6878 <asn6878@gmail.com>
2024-07-02 20:34:51 +0900

    fix: 삭제 연산 쿼리 횟수 개선을 위한 jpql 사용

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/spending/repository/SpendingRepository.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/spending/repository/SpendingRepository.java
@@ -2,6 +2,8 @@ package kr.co.pennyway.domain.domains.spending.repository;
 
 import kr.co.pennyway.domain.common.repository.ExtendedRepository;
 import kr.co.pennyway.domain.domains.spending.domain.Spending;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -12,4 +14,9 @@ public interface SpendingRepository extends ExtendedRepository<Spending, Long>, 
 
     @Transactional(readOnly = true)
     Integer countByUserIdAndIdIn(Long userId, List<Long> spendingIds);
+
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query("UPDATE Spending s SET s.deletedAt = NOW() where s.id IN :spendingIds")
+    void deleteAllById(List<Long> spendingIds);
 }
