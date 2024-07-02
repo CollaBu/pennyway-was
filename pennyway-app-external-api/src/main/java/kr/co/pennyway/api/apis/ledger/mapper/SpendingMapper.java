09f998929e42b798fe2aa6852fec54eac6bea245
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-02 13:49:37 +0900

    feat: mapper 내 카테고리별 지출 리스트 응답 dto 메서드 추가 && daily-list 정렬 메서드 추가

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/mapper/SpendingMapper.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/mapper/SpendingMapper.java
@@ -3,6 +3,7 @@ package kr.co.pennyway.api.apis.ledger.mapper;
 import kr.co.pennyway.api.apis.ledger.dto.SpendingSearchRes;
 import kr.co.pennyway.common.annotation.Mapper;
 import kr.co.pennyway.domain.domains.spending.domain.Spending;
+import org.springframework.data.domain.Slice;
 
 import java.util.List;
 import java.util.concurrent.ConcurrentMap;
@@ -10,11 +11,35 @@ import java.util.stream.Collectors;
 
 @Mapper
 public class SpendingMapper {
+    public static List<SpendingSearchRes.Month> toSpendingByCategory(Slice<Spending> spendings) {
+        List<Spending> spendingList = spendings.getContent();
+
+        ConcurrentMap<Integer, List<Spending>> groupSpendingsByYearAndMonth = spendingList.stream()
+                .collect(Collectors.groupingByConcurrent(
+                        spending -> spending.getSpendAt().getYear() * 100 + spending.getSpendAt().getMonthValue()
+                ));
+
+        return groupSpendingsByYearAndMonth.entrySet().stream()
+                .map(entry -> {
+                    int year = entry.getKey() / 100;
+                    int month = entry.getKey() % 100;
+                    return toSpendingSearchResMonth(entry.getValue(), year, month);
+                })
+                .sorted((a, b) -> {
+                    if (a.year() == b.year()) {
+                        return b.month() - a.month();
+                    }
+                    return b.year() - a.year();
+                })
+                .toList();
+    }
+
     public static SpendingSearchRes.Month toSpendingSearchResMonth(List<Spending> spendings, int year, int month) {
         ConcurrentMap<Integer, List<Spending>> groupSpendingsByDay = spendings.stream().collect(Collectors.groupingByConcurrent(Spending::getDay));
 
         List<SpendingSearchRes.Daily> dailySpendings = groupSpendingsByDay.entrySet().stream()
                 .map(entry -> toSpendingSearchResDaily(entry.getKey(), entry.getValue()))
+                .sorted((a, b) -> b.day() - a.day())
                 .toList();
 
         return SpendingSearchRes.Month.builder()
