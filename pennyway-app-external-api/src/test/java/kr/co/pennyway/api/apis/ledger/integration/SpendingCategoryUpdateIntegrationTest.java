c4e1ef89b8a475de968a7e439a80b933992c4fdc
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-03 19:53:58 +0900

    test: 쿼리 파라미터 한글 깨짐 수정

--- pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/ledger/integration/SpendingCategoryUpdateIntegrationTest.java
+++ pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/ledger/integration/SpendingCategoryUpdateIntegrationTest.java
@@ -6,7 +6,9 @@ import kr.co.pennyway.api.common.response.SuccessResponse;
 import kr.co.pennyway.api.common.security.jwt.access.AccessTokenClaim;
 import kr.co.pennyway.api.config.ExternalApiDBTestConfig;
 import kr.co.pennyway.api.config.ExternalApiIntegrationTest;
+import kr.co.pennyway.api.config.fixture.SpendingCustomCategoryFixture;
 import kr.co.pennyway.api.config.fixture.UserFixture;
+import kr.co.pennyway.domain.domains.spending.domain.SpendingCustomCategory;
 import kr.co.pennyway.domain.domains.spending.service.SpendingCustomCategoryService;
 import kr.co.pennyway.domain.domains.spending.type.SpendingCategory;
 import kr.co.pennyway.domain.domains.user.domain.User;
@@ -19,10 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import static org.springframework.test.util.AssertionErrors.assertEquals;
@@ -58,10 +57,27 @@ public class SpendingCategoryUpdateIntegrationTest extends ExternalApiDBTestConf
         assertEquals("403 Forbidden 예외가 발생해야 합니다.", response.getStatusCode(), HttpStatus.FORBIDDEN);
     }
 
+    @Test
+    @DisplayName("지출 카테고리가 수정되면, 수정된 정보를 응답 받는다.")
+    void success() {
+        // given
+        User user = userService.createUser(UserFixture.GENERAL_USER.toUser());
+        SpendingCustomCategory category = spendingCustomCategoryService.createSpendingCustomCategory(SpendingCustomCategoryFixture.GENERAL_SPENDING_CUSTOM_CATEGORY.toCustomSpendingCategory(user));
+        String expectedName = "뉴 카테고리";
+
+        // when
+        ResponseEntity<SuccessResponse<SpendingCategoryDto.Res>> response = successRequest(user, category.getId(), expectedName, SpendingCategory.HEALTH.name());
+
+        // then
+        assertEquals("200 OK 응답을 받아야 합니다.", response.getStatusCode(), HttpStatus.OK);
+        assertEquals("수정된 지출 카테고리 이름이 일치해야 합니다.", expectedName, response.getBody().getData().name());
+        assertEquals("수정된 지출 카테고리 아이콘이 일치해야 합니다.", SpendingCategory.HEALTH.name(), response.getBody().getData().icon());
+    }
+
     private ResponseEntity<SuccessResponse<SpendingCategoryDto.Res>> successRequest(User user, Long spendingCategoryId, String name, String icon) {
         return restTemplate.exchange(
-                createUriComponentsBuilder(spendingCategoryId, name, icon).toUriString(),
-                org.springframework.http.HttpMethod.PATCH,
+                createUriComponentsBuilder(spendingCategoryId, name, icon),
+                HttpMethod.PATCH,
                 createHttpEntity(user),
                 new ParameterizedTypeReference<SuccessResponse<SpendingCategoryDto.Res>>() {
                 }
@@ -70,21 +86,22 @@ public class SpendingCategoryUpdateIntegrationTest extends ExternalApiDBTestConf
 
     private ResponseEntity<ErrorResponse> failureRequest(User user, Long spendingCategoryId, String name, String icon) {
         return restTemplate.exchange(
-                createUriComponentsBuilder(spendingCategoryId, name, icon).toUriString(),
-                org.springframework.http.HttpMethod.PATCH,
+                createUriComponentsBuilder(spendingCategoryId, name, icon),
+                HttpMethod.PATCH,
                 createHttpEntity(user),
                 ErrorResponse.class
         );
     }
 
-    private UriComponentsBuilder createUriComponentsBuilder(Long spendingCategoryId, String name, String icon) {
+    private String createUriComponentsBuilder(Long spendingCategoryId, String name, String icon) {
         // 기본 URL 설정
-        String baseUrl = "http://localhost:" + port + "/v2/spending-categories/" + spendingCategoryId;
+        String baseUrl = "http://localhost:" + port + "/v2/spending-categories/" + spendingCategoryId.toString();
 
         // 쿼리 파라미터 설정
         return UriComponentsBuilder.fromHttpUrl(baseUrl)
                 .queryParam("name", name)
-                .queryParam("icon", icon);
+                .queryParam("icon", icon)
+                .build(false).toUriString(); // encoded false 옵션으로 한글 깨짐 방지
     }
 
     private HttpEntity<?> createHttpEntity(User user) {
