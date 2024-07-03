1ce1e4c11ddbbc49184c4a0ff9df22c499fba7fc
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-03 16:19:04 +0900

    feat: 카테고리 update 유즈 케이스 정의

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/usecase/SpendingCategoryUseCase.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/usecase/SpendingCategoryUseCase.java
@@ -4,6 +4,7 @@ import kr.co.pennyway.api.apis.ledger.dto.SpendingCategoryDto;
 import kr.co.pennyway.api.apis.ledger.mapper.SpendingCategoryMapper;
 import kr.co.pennyway.api.apis.ledger.service.SpendingCategorySaveService;
 import kr.co.pennyway.api.apis.ledger.service.SpendingCategorySearchService;
+import kr.co.pennyway.api.common.query.SpendingCategoryType;
 import kr.co.pennyway.common.annotation.UseCase;
 import kr.co.pennyway.domain.domains.spending.domain.SpendingCustomCategory;
 import kr.co.pennyway.domain.domains.spending.type.SpendingCategory;
@@ -32,5 +33,12 @@ public class SpendingCategoryUseCase {
         List<SpendingCustomCategory> categories = spendingCategorySearchService.readSpendingCustomCategories(userId);
 
         return SpendingCategoryMapper.toResponses(categories);
+    }
+
+    @Transactional
+    public SpendingCategoryDto.Res updateSpendingCategory(Long userId, Long categoryId, SpendingCategoryType type) {
+        SpendingCustomCategory category = spendingCategorySaveService.update(userId, categoryId, type);
+
+        return SpendingCategoryMapper.toResponse(category);
     }
 }
