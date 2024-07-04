db0e1f18d50a9d3a1042e47b4b011acb115fbdde
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-04 17:25:02 +0900

    fix: 푸시 알림 이벤트 class -> record 타입 변경 및 편의성 메서드 추가

--- pennyway-infra/src/main/java/kr/co/pennyway/infra/common/event/NotificationEvent.java
+++ pennyway-infra/src/main/java/kr/co/pennyway/infra/common/event/NotificationEvent.java
@@ -1,5 +1,8 @@
 package kr.co.pennyway.infra.common.event;
 
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.MulticastMessage;
+import com.google.firebase.messaging.Notification;
 import lombok.Getter;
 import org.springframework.util.StringUtils;
 
@@ -11,24 +14,51 @@ import java.util.List;
  * 제목, 내용, 디바이스 토큰 리스트, 푸시 알림 이미지를 필드로 갖는다.
  */
 @Getter
-public class NotificationEvent {
-    private final String title;
-    private final String content;
-    private final List<String> deviceTokens;
-    private final String imageUrl;
-
-    private NotificationEvent(String title, String content, List<String> deviceTokens, String imageUrl) {
-        if (!StringUtils.hasText(title) || !StringUtils.hasText(content) || deviceTokens == null) {
-            throw new IllegalArgumentException("NotificationEvent는 반드시 null 혹은 공백 문자가 아닌 title, content와 null이 아닌 deviceTokens를 가져야 합니다.");
+public record NotificationEvent(
+        String title,
+        String content,
+        List<String> deviceTokens,
+        String imageUrl
+) {
+    public NotificationEvent {
+        if (!StringUtils.hasText(title)) {
+            throw new IllegalArgumentException("제목은 반드시 null 혹은 공백이 아니어야 합니다.");
         }
-
-        this.title = title;
-        this.content = content;
-        this.deviceTokens = deviceTokens;
-        this.imageUrl = imageUrl;
+        if (!StringUtils.hasText(content)) {
+            throw new IllegalArgumentException("내용은 반드시 null 혹은 공백이 아니어야 합니다.");
+        }
+        if (deviceTokens == null) {
+            throw new IllegalArgumentException("디바이스 토큰은 반드시 null이 아니어야 합니다.");
+        }
     }
 
     public static NotificationEvent of(String title, String content, List<String> deviceTokens, String imageUrl) {
         return new NotificationEvent(title, content, deviceTokens, imageUrl);
+    }
+
+    public int deviceTokensSize() {
+        return deviceTokens.size();
+    }
+
+    /**
+     * 단일 메시지를 전송하기 위한 Message.Builder를 생성한다.
+     */
+    public Message.Builder buildSingleMessage() {
+        return Message.builder().setNotification(toNotification()).setToken(deviceTokens.get(0));
+    }
+
+    /**
+     * 다중 메시지를 전송하기 위한 MulticastMessage.Builder를 생성한다.
+     */
+    public MulticastMessage.Builder buildMulticastMessage() {
+        return MulticastMessage.builder().setNotification(toNotification()).addAllTokens(deviceTokens);
+    }
+
+    private Notification toNotification() {
+        return Notification.builder()
+                .setTitle(title)
+                .setBody(content)
+                .setImage(imageUrl)
+                .build();
     }
 }
