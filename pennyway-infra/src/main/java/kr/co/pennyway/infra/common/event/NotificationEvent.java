9abe74fbcdd09311a5913ca8a73a9d660c200b23
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-04 17:17:00 +0900

    fix: 알림 이벤트 객체 image_url 필드 추가

--- pennyway-infra/src/main/java/kr/co/pennyway/infra/common/event/NotificationEvent.java
+++ pennyway-infra/src/main/java/kr/co/pennyway/infra/common/event/NotificationEvent.java
@@ -8,15 +8,16 @@ import java.util.List;
 /**
  * FCM 푸시 알림에 필요한 정보를 담은 Event 클래스
  * <p>
- * 제목, 내용, 디바이스 토큰 리스트를 필드로 갖는다.
+ * 제목, 내용, 디바이스 토큰 리스트, 푸시 알림 이미지를 필드로 갖는다.
  */
 @Getter
 public class NotificationEvent {
     private final String title;
     private final String content;
     private final List<String> deviceTokens;
+    private final String imageUrl;
 
-    private NotificationEvent(String title, String content, List<String> deviceTokens) {
+    private NotificationEvent(String title, String content, List<String> deviceTokens, String imageUrl) {
         if (!StringUtils.hasText(title) || !StringUtils.hasText(content) || deviceTokens == null) {
             throw new IllegalArgumentException("NotificationEvent는 반드시 null 혹은 공백 문자가 아닌 title, content와 null이 아닌 deviceTokens를 가져야 합니다.");
         }
@@ -24,9 +25,10 @@ public class NotificationEvent {
         this.title = title;
         this.content = content;
         this.deviceTokens = deviceTokens;
+        this.imageUrl = imageUrl;
     }
 
-    public static NotificationEvent of(String title, String content, List<String> deviceTokens) {
-        return new NotificationEvent(title, content, deviceTokens);
+    public static NotificationEvent of(String title, String content, List<String> deviceTokens, String imageUrl) {
+        return new NotificationEvent(title, content, deviceTokens, imageUrl);
     }
 }
