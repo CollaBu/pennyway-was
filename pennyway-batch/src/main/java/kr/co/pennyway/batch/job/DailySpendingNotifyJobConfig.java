8345c74ae8c6ae1d48be6d1d208fd5278d9476bc
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-04 18:33:40 +0900

    feat: 정치 지출 관리 알림 job 설정

---
+++ pennyway-batch/src/main/java/kr/co/pennyway/batch/job/DailySpendingNotifyJobConfig.java
@@ -0,0 +1,23 @@
+package kr.co.pennyway.batch.job;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class DailySpendingNotifyJobConfig {
+    private final JobRepository jobRepository;
+    private final SendSpendingNotifyStepConfig sendSpendingNotifyStepConfig;
+
+    @Bean
+    public Job dailyNotificationJob(PlatformTransactionManager transactionManager) {
+        return new JobBuilder("dailyNotificationJob", jobRepository)
+                .start(sendSpendingNotifyStepConfig.execute(transactionManager))
+                .build();
+    }
+}
