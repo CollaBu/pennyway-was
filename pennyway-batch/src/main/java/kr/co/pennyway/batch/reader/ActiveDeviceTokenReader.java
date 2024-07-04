ab94ce146769db30fafc8154a25e4e41eeba7825
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-04 18:41:14 +0900

    feat: 활성화된 디바이스 토큰 탐색 reader 정의

---
+++ pennyway-batch/src/main/java/kr/co/pennyway/batch/reader/ActiveDeviceTokenReader.java
@@ -0,0 +1,31 @@
+package kr.co.pennyway.batch.reader;
+
+import kr.co.pennyway.domain.domains.device.domain.DeviceToken;
+import kr.co.pennyway.domain.domains.device.repository.DeviceTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.item.data.RepositoryItemReader;
+import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+@Component
+@RequiredArgsConstructor
+public class ActiveDeviceTokenReader {
+    private final DeviceTokenRepository deviceTokenRepository;
+
+    @Bean
+    public RepositoryItemReader<DeviceToken> activeDeviceTokenReader() {
+        return new RepositoryItemReaderBuilder<DeviceToken>()
+                .name("activeDeviceTokenReader")
+                .repository(deviceTokenRepository)
+                .methodName("findAll")
+                .pageSize(100)
+                .sorts(new HashMap<String, Sort.Direction>() {{
+                    put("id", Sort.Direction.ASC);
+                }})
+                .build();
+    }
+}
