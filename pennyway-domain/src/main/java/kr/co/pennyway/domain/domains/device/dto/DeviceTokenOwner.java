748e8a6abde704015b69e5798de542128ea61e81
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-05 19:48:57 +0900

    style: 인자 순서 변경

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/device/dto/DeviceTokenOwner.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/device/dto/DeviceTokenOwner.java
@@ -1,11 +1,13 @@
 package kr.co.pennyway.domain.domains.device.dto;
 
+import java.util.List;
+
 /**
  * 디바이스 토큰과 유저 아이디를 담은 DTO
  */
 public record DeviceTokenOwner(
-        String deviceToken,
         Long userId,
-        String name
+        String name,
+        List<String> deviceTokens
 ) {
 }
