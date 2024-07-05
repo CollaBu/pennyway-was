a3f410785f8c625a06449e1a686647c3d8d04274
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-05 20:16:42 +0900

    feat: 알림 벌크 insert 메서드 추가

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/notification/repository/NotificationRepository.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/notification/repository/NotificationRepository.java
@@ -2,6 +2,26 @@ package kr.co.pennyway.domain.domains.notification.repository;
 
 import kr.co.pennyway.domain.domains.notification.domain.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query(value = "INSERT INTO notification (readAt, type, announcement, receiver, sender) " +
+            "SELECT false, :notification.type, :notification.announcement, :receiverId, :senderId" +
+            "WHERE NOT EXISTS (SELECT 1 FROM notification " +
+            "WHERE receiver = :receiverId AND DATE_FORMAT(created_at, '%Y-%m-%d') = :currentDate " +
+            "AND type = :notification.type AND announcement = :notification.announcement",
+            nativeQuery = true)
+    void insertIfNotExistsInQuery(
+            @Param("notification") Notification notification,
+            @Param("receiverId") Long receiverId,
+            @Param("senderId") Long senderId,
+            @Param("currentDate") LocalDate currentDate
+    );
 }
