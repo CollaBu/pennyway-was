f36ccfe24413eb19524955643910f232bbaf50c9
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-05 12:17:37 +0900

    rename: device_token dto 이름 수정

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/device/dto/DeviceTokenOwner.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/device/dto/DeviceTokenOwner.java
@@ -3,7 +3,7 @@ package kr.co.pennyway.domain.domains.device.dto;
 /**
  * 디바이스 토큰과 유저 아이디를 담은 DTO
  */
-public record DeviceInfo(
+public record DeviceTokenOwner(
         String deviceToken,
         Long userId
 ) {
