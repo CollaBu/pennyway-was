c1313047c06d39d880c8b7237f03e13d10f1d7f0
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-06 17:33:06 +0900

    feat: notification entity builder 클래스 생성

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/notification/domain/Notification.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/notification/domain/Notification.java
@@ -7,13 +7,17 @@ import kr.co.pennyway.domain.common.model.DateAuditable;
 import kr.co.pennyway.domain.domains.notification.type.Announcement;
 import kr.co.pennyway.domain.domains.notification.type.NoticeType;
 import kr.co.pennyway.domain.domains.user.domain.User;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Getter
 @Table(name = "notification")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Notification extends DateAuditable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,4 +36,53 @@ public class Notification extends DateAuditable {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "sender")
     private User sender;
+
+    private Notification(LocalDateTime readAt, NoticeType type, Announcement announcement, User receiver, User sender) {
+        this.readAt = Objects.requireNonNull(readAt);
+        this.type = Objects.requireNonNull(type);
+        this.announcement = Objects.requireNonNull(announcement);
+        this.receiver = receiver;
+        this.sender = sender;
+    }
+
+    @Override
+    public String toString() {
+        return "Notification{" +
+                "id=" + id +
+                ", readAt=" + readAt +
+                ", type=" + type +
+                ", announcement=" + announcement +
+                '}';
+    }
+
+    public static class Builder {
+        private Long id;
+        private LocalDateTime readAt;
+        private NoticeType type;
+        private Announcement announcement;
+
+        private User receiver = null;
+        private User sender = null;
+
+        public Builder(Long id, LocalDateTime readAt, NoticeType type, Announcement announcement) {
+            this.id = id;
+            this.readAt = readAt;
+            this.type = type;
+            this.announcement = announcement;
+        }
+
+        public Builder receiver(User receiver) {
+            this.receiver = receiver;
+            return this;
+        }
+
+        public Builder sender(User sender) {
+            this.sender = sender;
+            return this;
+        }
+
+        public Notification build() {
+            return new Notification(readAt, type, announcement, receiver, sender);
+        }
+    }
 }
