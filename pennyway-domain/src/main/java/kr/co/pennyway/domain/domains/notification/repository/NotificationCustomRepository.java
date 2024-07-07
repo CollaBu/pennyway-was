67ba74dbea7dfc822cb846cb0748df533cf1efd5
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-07 13:19:26 +0900

    rename: 정기 지줄 등록 알림 벌크 삽입 메서드 주석 추가

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/notification/repository/NotificationCustomRepository.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/notification/repository/NotificationCustomRepository.java
@@ -6,5 +6,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface NotificationCustomRepository {
+    /**
+     * 사용자들에게 정기 지출 등록 알림을 저장한다.
+     *
+     * @param userIds      : 등록할 사용자 아이디 목록
+     * @param publishedAt  : 알림 발송 시간, 공지 알림 중복 저장 방지를 위해 조건식에 사용
+     * @param announcement : 알림 타입 {@link Announcement}
+     */
     void saveDailySpendingAnnounceInBulk(List<Long> userIds, LocalDateTime publishedAt, Announcement announcement);
 }
