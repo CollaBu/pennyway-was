599f14eaa0412807766c392f5dce5516a31a777c
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-07 13:09:45 +0900

    feat: 정기 지출 등록 알림 벌크 삽입 연산 메서드 추가

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/notification/repository/NotificationCustomRepository.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/notification/repository/NotificationCustomRepository.java
@@ -1,11 +1,10 @@
 package kr.co.pennyway.domain.domains.notification.repository;
 
 import kr.co.pennyway.domain.domains.notification.type.Announcement;
-import kr.co.pennyway.domain.domains.notification.type.NoticeType;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface NotificationCustomRepository {
-    void saveDailySpendingAnnounceInBulk(List<Long> userIds, LocalDateTime publishedAt, NoticeType noticeType, Announcement announcement);
+    void saveDailySpendingAnnounceInBulk(List<Long> userIds, LocalDateTime publishedAt, Announcement announcement);
 }
---
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/notification/repository/NotificationCustomRepositoryImpl.java
@@ -0,0 +1,48 @@
+package kr.co.pennyway.domain.domains.notification.repository;
+
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import kr.co.pennyway.domain.domains.notification.domain.QNotification;
+import kr.co.pennyway.domain.domains.notification.type.Announcement;
+import kr.co.pennyway.domain.domains.notification.type.NoticeType;
+import kr.co.pennyway.domain.domains.user.domain.QUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class NotificationCustomRepositoryImpl implements NotificationCustomRepository {
+    private final JPAQueryFactory queryFactory;
+
+    private final QUser user = QUser.user;
+    private final QNotification notification = QNotification.notification;
+
+    @Override
+    public void saveDailySpendingAnnounceInBulk(List<Long> userIds, LocalDateTime publishedAt, Announcement announcement) {
+        queryFactory.insert(notification)
+                .columns(
+                        notification.type,
+                        notification.announcement,
+                        notification.receiver.id
+                )
+                .select(
+                        JPAExpressions
+                                .select(
+                                        Expressions.constant(NoticeType.ANNOUNCEMENT),
+                                        Expressions.constant(announcement),
+                                        user.id
+                                )
+                                .from(user)
+                                .where(user.id.in(userIds)
+                                        .and(notification.createdAt.dayOfYear().eq(publishedAt.getDayOfYear()))
+                                        .and(notification.createdAt.year().eq(publishedAt.getYear()))
+                                        .and(notification.type.eq(NoticeType.ANNOUNCEMENT))
+                                        .and(notification.announcement.ne(announcement)))
+                )
+                .execute();
+    }
+}
