68f551e498cf93e6c86940d05497534821c433bb
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-09 00:09:06 +0900

    chore: @enable_async config 파일 분리

--- pennyway-infra/src/main/java/kr/co/pennyway/infra/config/FcmConfig.java
+++ pennyway-infra/src/main/java/kr/co/pennyway/infra/config/FcmConfig.java
@@ -9,16 +9,11 @@ import kr.co.pennyway.infra.common.importer.PennywayInfraConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.io.IOException;
 
 @Slf4j
-@Profile({"local", "dev", "prod"})
-@EnableAsync
-// TODO: 2024.05.17 우선 테스트 통과를 위해 임시로 처리함. Push Notification 기능 테스트 시 문제가 발생하면 수정이 필요함.
 public class FcmConfig implements PennywayInfraConfig {
     private final ClassPathResource firebaseResource;
     private final String projectId;
