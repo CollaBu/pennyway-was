52a1b45a9a0ee3ee7a55414247a8ebf45860865c
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-09 18:40:42 +0900

    fix: device_token_owner device_tokens -> device_token

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/device/dto/DeviceTokenOwner.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/device/dto/DeviceTokenOwner.java
@@ -1,13 +1,11 @@
 package kr.co.pennyway.domain.domains.device.dto;
 
-import java.util.List;
-
 /**
  * 디바이스 토큰과 유저 아이디를 담은 DTO
  */
 public record DeviceTokenOwner(
         Long userId,
         String name,
-        List<String> deviceTokens
+        String deviceToken
 ) {
 }
