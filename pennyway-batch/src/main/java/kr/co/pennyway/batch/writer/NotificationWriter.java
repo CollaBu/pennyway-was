1e5f206a1d6d97e46d51f8d32fa1247259465e58
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-09 19:13:01 +0900

    fix: 배치 writer 내부 로직 수정 -> daily_spending_notification 매핑 로직 추가

--- pennyway-batch/src/main/java/kr/co/pennyway/batch/writer/NotificationWriter.java
+++ pennyway-batch/src/main/java/kr/co/pennyway/batch/writer/NotificationWriter.java
@@ -1,6 +1,7 @@
 package kr.co.pennyway.batch.writer;
 
 import kr.co.pennyway.batch.dto.DailySpendingNotification;
+import kr.co.pennyway.domain.domains.device.dto.DeviceTokenOwner;
 import kr.co.pennyway.domain.domains.notification.repository.NotificationRepository;
 import kr.co.pennyway.domain.domains.notification.type.Announcement;
 import kr.co.pennyway.infra.common.event.NotificationEvent;
@@ -14,27 +15,32 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
-public class NotificationWriter implements ItemWriter<DailySpendingNotification> {
+public class NotificationWriter implements ItemWriter<DeviceTokenOwner> {
     private final NotificationRepository notificationRepository;
     private final ApplicationEventPublisher publisher;
 
     @Override
     @Transactional
-    public void write(@NonNull Chunk<? extends DailySpendingNotification> notifications) throws Exception {
+    public void write(@NonNull Chunk<? extends DeviceTokenOwner> owners) throws Exception {
         LocalDateTime publishedAt = LocalDateTime.now();
 
         List<Long> userIds = new ArrayList<>();
-        for (DailySpendingNotification notification : notifications) {
-            userIds.add(notification.userId());
+        Map<Long, DailySpendingNotification> notificationMap = new HashMap<>();
+
+        for (DeviceTokenOwner owner : owners) {
+            userIds.add(owner.userId());
+            notificationMap.computeIfAbsent(owner.userId(), k -> DailySpendingNotification.from(owner)).addDeviceToken(owner.deviceToken());
         }
 
         notificationRepository.saveDailySpendingAnnounceInBulk(userIds, publishedAt, Announcement.DAILY_SPENDING);
 
-        for (DailySpendingNotification notification : notifications) {
+        for (DailySpendingNotification notification : notificationMap.values()) {
             publisher.publishEvent(NotificationEvent.of(notification.title(), notification.content(), notification.deviceTokens(), ""));
         }
     }
