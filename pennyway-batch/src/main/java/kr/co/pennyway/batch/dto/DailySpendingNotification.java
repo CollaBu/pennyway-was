7be2de9a667fbecd5a3ab1e2c1c1186052d8b152
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-09 19:11:52 +0900

    fix: daily_spending_notification dto 정적 팩토리 메서드 수정 & 디바이스 토큰 추가 메서드

--- pennyway-batch/src/main/java/kr/co/pennyway/batch/dto/DailySpendingNotification.java
+++ pennyway-batch/src/main/java/kr/co/pennyway/batch/dto/DailySpendingNotification.java
@@ -4,6 +4,8 @@ import kr.co.pennyway.domain.domains.device.dto.DeviceTokenOwner;
 import kr.co.pennyway.domain.domains.notification.type.Announcement;
 import lombok.Builder;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Builder
@@ -11,23 +13,34 @@ public record DailySpendingNotification(
         Long userId,
         String title,
         String content,
-        String deviceToken
+        List<String> deviceTokens
 ) {
     public DailySpendingNotification {
         Objects.requireNonNull(userId, "userId must not be null");
         Objects.requireNonNull(title, "title must not be null");
         Objects.requireNonNull(content, "content must not be null");
-        Objects.requireNonNull(deviceToken, "deviceTokens must not be null");
+        Objects.requireNonNull(deviceTokens, "deviceTokens must not be null");
     }
 
+    /**
+     * {@link DeviceTokenOwner}를 DailySpendingNotification DTO로 변환하는 정적 팩토리 메서드
+     * <p>
+     * DeviceToken은 List로 변환되어 멤버 변수로 관리하게 된다.
+     */
     public static DailySpendingNotification from(DeviceTokenOwner owner) {
         Announcement announcement = Announcement.DAILY_SPENDING;
+        List<String> deviceTokens = new ArrayList<>();
+        deviceTokens.add(owner.deviceToken());
 
         return DailySpendingNotification.builder()
                 .userId(owner.userId())
                 .title(announcement.createFormattedTitle(owner.name()))
                 .content(announcement.getContent())
-                .deviceToken(owner.deviceToken())
+                .deviceTokens(deviceTokens)
                 .build();
+    }
+
+    public void addDeviceToken(String deviceToken) {
+        deviceTokens.add(deviceToken);
     }
 }
