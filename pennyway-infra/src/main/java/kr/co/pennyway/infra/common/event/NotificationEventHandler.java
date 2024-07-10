bc615845d354a688ac51783b527e3cb5870e6bf5
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-10 14:09:57 +0900

    rename: notification_event_handler 명세 수정

--- pennyway-infra/src/main/java/kr/co/pennyway/infra/common/event/NotificationEventHandler.java
+++ pennyway-infra/src/main/java/kr/co/pennyway/infra/common/event/NotificationEventHandler.java
@@ -1,9 +1,9 @@
 package kr.co.pennyway.infra.common.event;
 
 /**
- * FCM 푸시 알림을 처리하는 핸들러 인터페이스
+ * 푸시 알림을 처리하는 핸들러 인터페이스
  * <p>
- * 테스트 환경에서 Fcm Config 빈 생성을 막기 위해 사용하며, 테스트 시 해당 인터페이스를 구현한 Mock 객체를 사용한다.
+ * 푸시 알림을 포함한 기능을 테스트할 때는 해당 인터페이스를 구현한 Mock 객체를 사용한다.
  *
  * @author YANG JAESEO
  * @since 2024-07-09
