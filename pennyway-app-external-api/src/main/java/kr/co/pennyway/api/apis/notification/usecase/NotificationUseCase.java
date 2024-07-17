66a96548cabcc469ca2922e1890f129f3ecd5ea5
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-17 16:53:38 +0900

    feat: notification use case 내, service 및 mapper 호출 로직 처리

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/notification/usecase/NotificationUseCase.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/notification/usecase/NotificationUseCase.java
@@ -1,17 +1,23 @@
 package kr.co.pennyway.api.apis.notification.usecase;
 
 import kr.co.pennyway.api.apis.notification.dto.NotificationDto;
+import kr.co.pennyway.api.apis.notification.service.NotificationSearchService;
 import kr.co.pennyway.common.annotation.UseCase;
+import kr.co.pennyway.domain.domains.notification.domain.Notification;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 @Slf4j
 @UseCase
 @RequiredArgsConstructor
 public class NotificationUseCase {
+    private final NotificationSearchService notificationSearchService;
 
     public NotificationDto.SliceRes getNotifications(Long userId, Pageable pageable) {
-        return null;
+        Slice<Notification> notifications = notificationSearchService.getNotifications(userId, pageable);
+
+        return NotificationMapper.toSliceRes(notifications, pageable);
     }
 }
