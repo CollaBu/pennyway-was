7fb4b90b55f0b4f5d1340e15bf61e0393a301776
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-17 16:52:33 +0900

    feat: notificaiton search service impl

---
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/notification/service/NotificationSearchService.java
@@ -0,0 +1,22 @@
+package kr.co.pennyway.api.apis.notification.service;
+
+import kr.co.pennyway.domain.domains.notification.domain.Notification;
+import kr.co.pennyway.domain.domains.notification.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class NotificationSearchService {
+    private final NotificationService notificationService;
+
+    @Transactional(readOnly = true)
+    public Slice<Notification> getNotifications(Long userId, Pageable pageable) {
+        return notificationService.readNotificationsSlice(userId, pageable);
+    }
+}
