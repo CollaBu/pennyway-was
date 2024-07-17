5e4591083871196ea81ce6701dec5acd567bc465
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-17 13:50:45 +0900

    feat: notification use case 클래스 작성

---
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/notification/usecase/NotificationUseCase.java
@@ -0,0 +1,11 @@
+package kr.co.pennyway.api.apis.notification.usecase;
+
+import kr.co.pennyway.common.annotation.UseCase;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@UseCase
+@RequiredArgsConstructor
+public class NotificationUseCase {
+}
