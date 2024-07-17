af783d144e1f853ace23f5ebaf3447db28028b14
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-17 14:49:32 +0900

    feat: notification info & slice dto 정의

---
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/notification/dto/NotificationDto.java
@@ -0,0 +1,63 @@
+package kr.co.pennyway.api.apis.notification.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class NotificationDto {
+    @Schema(title = "푸시 알림 슬라이스 응답")
+    public record SliceRes(
+            @Schema(description = "푸시 알림 리스트")
+            List<Info> content,
+            @Schema(description = "현재 페이지 번호")
+            int currentPageNumber,
+            @Schema(description = "페이지 크기")
+            int pageSize,
+            @Schema(description = "전체 요소 개수")
+            int numberOfElements,
+            @Schema(description = "다음 페이지 존재 여부")
+            boolean hasNext
+    ) {
+        public static SliceRes from(List<Info> notifications, Pageable pageable, int numberOfElements, boolean hasNext) {
+            return new SliceRes(notifications, pageable.getPageNumber(), pageable.getPageSize(), numberOfElements, hasNext);
+        }
+    }
+
+    @Schema(title = "푸시 알림 상세 정보", description = "푸시 알림 pk, 읽음 여부, 제목, 내용, 타입 그리고 딥 링크 정보를 담고 있다.")
+    public record Info(
+            @Schema(description = "푸시 알림 pk", example = "1")
+            Long id,
+            @Schema(description = "푸시 알림 읽음 여부", example = "true")
+            boolean isRead,
+            @Schema(description = "푸시 알림 제목", example = "페니웨이 공지")
+            String title,
+            @Schema(description = "푸시 알림 내용", example = "안녕하세요. 페니웨이입니다.")
+            String content,
+            @Schema(description = "푸시 알림 타입. ex) ANNOUNCEMENT", example = "FEED_LIKE_FROM_TO")
+            String type,
+            @Schema(description = "푸시 알림 행위자. ex) 다른 사용자 <type이 ANNOUNCEMENT면 존재하지 않음>", example = "pennyway")
+            @JsonInclude(JsonInclude.Include.NON_NULL)
+            String from,
+            @Schema(description = "푸시 알림 행위자 pk <type이 ANNOUNCEMENT면 존재하지 않음>", example = "1")
+            @JsonInclude(JsonInclude.Include.NON_NULL)
+            Long fromId,
+            @Schema(description = "푸시 알림 행위자가 액션을 취한 대상 ex) 피드, 채팅 <type이 ANNOUNCEMENT면 존재하지 않음>", example = "feed")
+            @JsonInclude(JsonInclude.Include.NON_NULL)
+            String to,
+            @Schema(description = "푸시 알림 행위자가 액션을 취한 대상 pk <type이 ANNOUNCEMENT면 존재하지 않음>", example = "3")
+            @JsonInclude(JsonInclude.Include.NON_NULL)
+            Long toId,
+            @Schema(description = "푸시 알림 생성 시간", example = "yyyy-MM-dd HH:mm:ss")
+            @JsonSerialize(using = LocalDateTimeSerializer.class)
+            @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+            LocalDateTime createdAt
+    ) {
+
+    }
+}
