09fdfa8f4c5c76151422d3238cad426e7ead3f7d
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-17 14:50:56 +0900

    fix: notification use case 껍데기 구현

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/notification/usecase/NotificationUseCase.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/notification/usecase/NotificationUseCase.java
@@ -1,11 +1,17 @@
 package kr.co.pennyway.api.apis.notification.usecase;
 
+import kr.co.pennyway.api.apis.notification.dto.NotificationDto;
 import kr.co.pennyway.common.annotation.UseCase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 
 @Slf4j
 @UseCase
 @RequiredArgsConstructor
 public class NotificationUseCase {
+
+    public NotificationDto.SliceRes getNotifications(Long userId, Pageable pageable) {
+        return null;
+    }
 }
