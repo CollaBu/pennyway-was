3f9c941d3262813a1473ef4376a68254d3679f16
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-18 11:52:53 +0900

    test: notification update read_at 메서드 unit 테스트

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/notification/repository/NotificationRepository.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/notification/repository/NotificationRepository.java
@@ -2,6 +2,15 @@ package kr.co.pennyway.domain.domains.notification.repository;
 
 import kr.co.pennyway.domain.common.repository.ExtendedRepository;
 import kr.co.pennyway.domain.domains.notification.domain.Notification;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface NotificationRepository extends ExtendedRepository<Notification, Long>, NotificationCustomRepository {
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query("update Notification n set n.readAt = current_timestamp where n.id in ?1")
+    void updateReadAtByIds(List<Long> notificationIds);
 }
