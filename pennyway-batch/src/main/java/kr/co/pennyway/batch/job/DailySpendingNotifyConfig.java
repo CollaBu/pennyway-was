f0b3807d2a727442db87a2bea0896a640a79be88
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-24 20:23:48 +0900

    style: step config 파일 삭제 -> job config에 통합

--- pennyway-batch/src/main/java/kr/co/pennyway/batch/job/DailySpendingNotifyConfig.java
+++ pennyway-batch/src/main/java/kr/co/pennyway/batch/job/DailySpendingNotifyConfig.java
@@ -1,11 +1,12 @@
-package kr.co.pennyway.batch.step;
+package kr.co.pennyway.batch.job;
 
-import kr.co.pennyway.batch.processor.NotificationProcessor;
 import kr.co.pennyway.batch.reader.ActiveDeviceTokenReader;
 import kr.co.pennyway.batch.writer.NotificationWriter;
 import kr.co.pennyway.domain.domains.device.dto.DeviceTokenOwner;
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.context.annotation.Bean;
@@ -14,18 +15,28 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @RequiredArgsConstructor
-public class SendSpendingNotifyStepConfig {
+public class DailySpendingNotifyConfig {
     private final JobRepository jobRepository;
     private final ActiveDeviceTokenReader reader;
-    private final NotificationProcessor processor;
     private final NotificationWriter writer;
 
     @Bean
-    public Step sendSpendingNotifyStep(PlatformTransactionManager transactionManager) {
+    public Job dailyNotificationJob(PlatformTransactionManager transactionManager) {
+        return new JobBuilder("dailyNotificationJob", jobRepository)
+                .start(dailyNotificationStep(transactionManager))
+                .on("FAILED")
+                .stopAndRestart(dailyNotificationStep(transactionManager))
+                .on("*")
+                .end()
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step dailyNotificationStep(PlatformTransactionManager transactionManager) {
         return new StepBuilder("sendSpendingNotifyStep", jobRepository)
-                .<DeviceTokenOwner, DeviceTokenOwner>chunk(100, transactionManager)
+                .<DeviceTokenOwner, DeviceTokenOwner>chunk(1000, transactionManager)
                 .reader(reader.execute())
-                .processor(processor)
                 .writer(writer)
                 .build();
     }
