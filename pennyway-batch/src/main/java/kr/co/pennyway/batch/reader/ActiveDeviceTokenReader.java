6d7d27e5aa70c2e2927db9fa0b607731689285db
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-24 20:59:03 +0900

    fix: repository_item_reader -> querydsl_no_offset_paging_item_reader 변경

--- pennyway-batch/src/main/java/kr/co/pennyway/batch/reader/ActiveDeviceTokenReader.java
+++ pennyway-batch/src/main/java/kr/co/pennyway/batch/reader/ActiveDeviceTokenReader.java
@@ -1,33 +1,46 @@
 package kr.co.pennyway.batch.reader;
 
+import com.querydsl.core.types.Projections;
+import jakarta.persistence.EntityManagerFactory;
+import kr.co.pennyway.batch.common.reader.QuerydslNoOffsetPagingItemReader;
+import kr.co.pennyway.batch.common.reader.expression.Expression;
+import kr.co.pennyway.batch.common.reader.options.QuerydslNoOffsetNumberOptions;
+import kr.co.pennyway.batch.common.reader.options.QuerydslNoOffsetOptions;
+import kr.co.pennyway.domain.domains.device.domain.QDeviceToken;
 import kr.co.pennyway.domain.domains.device.dto.DeviceTokenOwner;
-import kr.co.pennyway.domain.domains.device.repository.DeviceTokenRepository;
+import kr.co.pennyway.domain.domains.user.domain.QUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.item.data.RepositoryItemReader;
-import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
-
-import java.util.HashMap;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class ActiveDeviceTokenReader {
-    private final DeviceTokenRepository deviceTokenRepository;
+    private final EntityManagerFactory emf;
+
+    private final QUser user = QUser.user;
+    private final QDeviceToken deviceToken = QDeviceToken.deviceToken;
 
     @Bean
-    public RepositoryItemReader<DeviceTokenOwner> execute() {
-        return new RepositoryItemReaderBuilder<DeviceTokenOwner>()
-                .name("execute")
-                .repository(deviceTokenRepository)
-                .methodName("findActivatedDeviceTokenOwners")
-                .pageSize(100)
-                .sorts(new HashMap<>() {{
-                    put("id", Sort.Direction.ASC);
-                }})
-                .build();
+    @StepScope
+    public QuerydslNoOffsetPagingItemReader<DeviceTokenOwner> querydslNoOffsetPagingItemReader() {
+        QuerydslNoOffsetOptions<DeviceTokenOwner> options = QuerydslNoOffsetNumberOptions.of(user.id, Expression.ASC, "userId");
+
+        return new QuerydslNoOffsetPagingItemReader<>(emf, 1000, options, queryFactory -> queryFactory
+                .select(
+                        Projections.constructor(
+                                DeviceTokenOwner.class,
+                                user.id,
+                                user.name,
+                                deviceToken.token
+                        )
+                )
+                .from(deviceToken)
+                .innerJoin(user).on(deviceToken.user.id.eq(user.id))
+                .where(deviceToken.activated.isTrue().and(user.notifySetting.accountBookNotify.isTrue()))
+        );
     }
 }
