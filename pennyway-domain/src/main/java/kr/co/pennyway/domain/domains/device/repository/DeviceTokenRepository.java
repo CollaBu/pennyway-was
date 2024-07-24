93f122df07268155b9f87dc840d9aa6ebd373ff1
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-24 21:04:24 +0900

    fix: device_token_custom_repository 제거

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/device/repository/DeviceTokenRepository.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/device/repository/DeviceTokenRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface DeviceTokenRepository extends JpaRepository<DeviceToken, Long>, DeviceTokenCustomRepository {
+public interface DeviceTokenRepository extends JpaRepository<DeviceToken, Long> {
     Optional<DeviceToken> findByUser_IdAndToken(Long userId, String token);
 
     List<DeviceToken> findAllByUser_Id(Long userId);
