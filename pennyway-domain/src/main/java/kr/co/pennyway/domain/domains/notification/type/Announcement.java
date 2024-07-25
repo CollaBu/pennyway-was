88083eb901dd03e6176f524bbf94e91da6b9080c
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-26 01:08:31 +0900

    fix: monthly_target_amount title %s누락 수정

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/notification/type/Announcement.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/notification/type/Announcement.java
@@ -10,7 +10,7 @@ public enum Announcement implements LegacyCommonType {
 
     // 정기 지출 알림
     DAILY_SPENDING("1", "%s님, 3분 카레보다 빨리 끝나요!", "많은 친구들이 소비 기록에 참여하고 있어요👀"),
-    MONTHLY_TARGET_AMOUNT("2", "%월의 첫 시작! 두구두구..🥁", "%s님의 이번 달 목표 소비 금액은?");
+    MONTHLY_TARGET_AMOUNT("2", "%s월의 첫 시작! 두구두구..🥁", "%s님의 이번 달 목표 소비 금액은?");
 
     private final String code;
     private final String title;
