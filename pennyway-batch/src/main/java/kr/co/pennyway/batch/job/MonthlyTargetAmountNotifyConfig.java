917764468f98e0903837ed549894bbb0c5d360b9
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-26 00:37:58 +0900

    feat: monthly_target_amount_notify_config job $ step impl

--- pennyway-batch/src/main/java/kr/co/pennyway/batch/job/MonthlyTargetAmountNotifyConfig.java
+++ pennyway-batch/src/main/java/kr/co/pennyway/batch/job/MonthlyTargetAmountNotifyConfig.java
@@ -2,7 +2,7 @@ package kr.co.pennyway.batch.job;
 
 import kr.co.pennyway.batch.common.dto.DeviceTokenOwner;
 import kr.co.pennyway.batch.reader.ActiveDeviceTokenReader;
-import kr.co.pennyway.batch.writer.DailySpendingNotifyWriter;
+import kr.co.pennyway.batch.writer.MonthlyTotalAmountNotifyWriter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -16,17 +16,17 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @RequiredArgsConstructor
-public class DailySpendingNotifyConfig {
+public class MonthlyTargetAmountNotifyConfig {
     private final JobRepository jobRepository;
     private final ActiveDeviceTokenReader reader;
-    private final DailySpendingNotifyWriter writer;
+    private final MonthlyTotalAmountNotifyWriter writer;
 
     @Bean
-    public Job dailyNotificationJob(PlatformTransactionManager transactionManager) {
-        return new JobBuilder("dailyNotificationJob", jobRepository)
-                .start(dailyNotificationStep(transactionManager))
+    public Job monthlyNotificationJob(PlatformTransactionManager transactionManager) {
+        return new JobBuilder("monthlyNotificationJob", jobRepository)
+                .start(monthlyNotificationStep(transactionManager))
                 .on("FAILED")
-                .stopAndRestart(dailyNotificationStep(transactionManager))
+                .stopAndRestart(monthlyNotificationStep(transactionManager))
                 .on("*")
                 .end()
                 .end()
@@ -35,8 +35,8 @@ public class DailySpendingNotifyConfig {
 
     @Bean
     @JobScope
-    public Step dailyNotificationStep(PlatformTransactionManager transactionManager) {
-        return new StepBuilder("sendSpendingNotifyStep", jobRepository)
+    public Step monthlyNotificationStep(PlatformTransactionManager transactionManager) {
+        return new StepBuilder("sendMonthlyNotifyStep", jobRepository)
                 .<DeviceTokenOwner, DeviceTokenOwner>chunk(1000, transactionManager)
                 .reader(reader.querydslNoOffsetPagingItemReader())
                 .writer(writer)
