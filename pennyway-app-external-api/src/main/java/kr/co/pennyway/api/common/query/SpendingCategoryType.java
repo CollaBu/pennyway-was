db18259e86eaca1ecd477e6baa569c6bdf4e6d1f
asn6878 <asn6878@gmail.com>
2024-07-29 01:53:58 +0900

    fix: 불필요한 schema 제거

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/common/query/SpendingCategoryType.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/common/query/SpendingCategoryType.java
@@ -1,12 +1,9 @@
 package kr.co.pennyway.api.common.query;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-
 public enum SpendingCategoryType {
     DEFAULT("default"),
     CUSTOM("custom");
-
-    @Schema(description = "카테고리 종류<br>default / custom", example = "default")
+    
     private final String type;
 
     SpendingCategoryType(String type) {
