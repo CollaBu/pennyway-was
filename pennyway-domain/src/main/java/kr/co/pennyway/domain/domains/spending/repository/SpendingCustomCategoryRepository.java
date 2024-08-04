da15912737998547dca7d30f6bf96f73628ecc39
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-08-04 11:43:11 +0900

    feat: 사용자 아이디 기반 spending_custom_category 삭제 bulk 메서드 추가

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/spending/repository/SpendingCustomCategoryRepository.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/spending/repository/SpendingCustomCategoryRepository.java
@@ -2,6 +2,8 @@ package kr.co.pennyway.domain.domains.spending.repository;
 
 import kr.co.pennyway.domain.domains.spending.domain.SpendingCustomCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -12,4 +14,9 @@ public interface SpendingCustomCategoryRepository extends JpaRepository<Spending
 
     @Transactional(readOnly = true)
     List<SpendingCustomCategory> findAllByUser_Id(Long userId);
+
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query("UPDATE SpendingCustomCategory s SET s.deletedAt = NOW() WHERE s.user.id = :userId AND s.deletedAt IS NULL")
+    void deleteAllByUserIdInQuery(Long userId);
 }
