66f6b461867e09e0b757d22974e4b66a0e655820
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-08-04 16:29:26 +0900

    chore: batch application timezone 설정

--- pennyway-batch/src/main/java/kr/co/pennyway/PennywayBatchApplication.java
+++ pennyway-batch/src/main/java/kr/co/pennyway/PennywayBatchApplication.java
@@ -1,13 +1,21 @@
 package kr.co.pennyway;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
 public class PennywayBatchApplication {
     public static void main(String[] args) {
         SpringApplication.run(PennywayBatchApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 }
