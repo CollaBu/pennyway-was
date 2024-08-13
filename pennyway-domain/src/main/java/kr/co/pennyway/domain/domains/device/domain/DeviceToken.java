5db9cddce07fc5ea340eb0bb8789ca4c9fb197b7
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-08-13 18:28:25 +0900

    fix: device token entity updated_at auditing 제거 -> 조회할 때마다 last_sigend_in 필드 업데이트

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/device/domain/DeviceToken.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/device/domain/DeviceToken.java
@@ -1,27 +1,36 @@
 package kr.co.pennyway.domain.domains.device.domain;
 
 import jakarta.persistence.*;
-import kr.co.pennyway.domain.common.model.DateAuditable;
 import kr.co.pennyway.domain.domains.user.domain.User;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
 @Getter
 @Table(name = "device_token")
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class DeviceToken extends DateAuditable {
+public class DeviceToken {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String token;
+
     @ColumnDefault("true")
     private Boolean activated;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    private LocalDateTime lastSignedInAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
@@ -31,6 +40,7 @@ public class DeviceToken extends DateAuditable {
         this.token = Objects.requireNonNull(token, "token은 null이 될 수 없습니다.");
         this.activated = Objects.requireNonNull(activated, "activated는 null이 될 수 없습니다.");
         this.user = Objects.requireNonNull(user, "user는 null이 될 수 없습니다.");
+        this.lastSignedInAt = LocalDateTime.now();
     }
 
     public static DeviceToken of(String token, User user) {
@@ -49,12 +59,19 @@ public class DeviceToken extends DateAuditable {
         this.activated = Boolean.FALSE;
     }
 
+    public void updateLastSignedInAt() {
+        this.lastSignedInAt = LocalDateTime.now();
+    }
+
     /**
-     * 디바이스 토큰을 갱신하고 활성화 상태로 변경한다.
+     * 디바이스 토큰이 만료되었는지 확인한다.
+     *
+     * @return 토큰이 갱신된지 7일이 지났으면 true, 그렇지 않으면 false
      */
-    public void updateToken(String token) {
-        this.activated = Boolean.TRUE;
-        this.token = token;
+    public boolean isExpired() {
+        LocalDateTime now = LocalDateTime.now();
+
+        return lastSignedInAt.plusDays(7).isBefore(now);
     }
 
     @Override
