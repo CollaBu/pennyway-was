e593b82bd6949f22ac0420666e25f00cd7326d8d
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-08-13 18:29:05 +0900

    fix: register service에서 이미 존재하는 토큰 조회 시, signed in at 갱신

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/users/service/DeviceTokenRegisterService.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/users/service/DeviceTokenRegisterService.java
@@ -1,8 +1,6 @@
 package kr.co.pennyway.api.apis.users.service;
 
 import kr.co.pennyway.domain.domains.device.domain.DeviceToken;
-import kr.co.pennyway.domain.domains.device.exception.DeviceTokenErrorCode;
-import kr.co.pennyway.domain.domains.device.exception.DeviceTokenErrorException;
 import kr.co.pennyway.domain.domains.device.service.DeviceTokenService;
 import kr.co.pennyway.domain.domains.user.domain.User;
 import kr.co.pennyway.domain.domains.user.exception.UserErrorCode;
@@ -12,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -23,17 +23,25 @@ public class DeviceTokenRegisterService {
     @Transactional
     public DeviceToken execute(Long userId, String token) {
         User user = userService.readUser(userId).orElseThrow(() -> new UserErrorException(UserErrorCode.NOT_FOUND));
-        DeviceToken deviceToken = getOrCreateDevice(user, token);
 
-        if (!deviceToken.isActivated()) {
-            throw new DeviceTokenErrorException(DeviceTokenErrorCode.NOT_ACTIVATED_DEVICE);
-        }
-
-        return deviceToken;
+        return getOrCreateDevice(user, token);
     }
 
+    /**
+     * 사용자의 디바이스 토큰을 가져오거나 생성한다.
+     * <p>
+     * 이미 등록된 디바이스 토큰인 경우 마지막 로그인 시간을 갱신한다.
+     */
     private DeviceToken getOrCreateDevice(User user, String token) {
-        return deviceTokenService.readDeviceByUserIdAndToken(user.getId(), token)
-                .orElseGet(() -> deviceTokenService.createDevice(DeviceToken.of(token, user)));
+        Optional<DeviceToken> deviceToken = deviceTokenService.readDeviceByUserIdAndToken(user.getId(), token);
+
+        if (deviceToken.isPresent()) {
+            DeviceToken device = deviceToken.get();
+            device.activate();
+            device.updateLastSignedInAt();
+            return device;
+        } else {
+            return deviceTokenService.createDevice(DeviceToken.of(token, user));
+        }
     }
 }
