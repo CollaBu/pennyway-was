49f30308279251cf18722b1eb05f49b0c64ba175
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-08-13 13:31:28 +0900

    feat: 복수개의 error response를 담기 위한 중간 어노테이션

---
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/common/annotation/ApiResponseExplainations.java
@@ -0,0 +1,12 @@
+package kr.co.pennyway.api.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ApiResponseExplainations {
+    ApiExceptionExplain[] errors() default {};
+}
