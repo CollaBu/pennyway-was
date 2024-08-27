25cb14d26027c2e48920e94050991435434d14b6
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-08-27 19:16:40 +0900

    fix: web_config jwt provider 의존성 제거

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/config/WebConfig.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/config/WebConfig.java
@@ -4,7 +4,6 @@ import kr.co.pennyway.api.common.converter.NotifyTypeConverter;
 import kr.co.pennyway.api.common.converter.ProviderConverter;
 import kr.co.pennyway.api.common.converter.SpendingCategoryTypeConverter;
 import kr.co.pennyway.api.common.converter.VerificationTypeConverter;
-import kr.co.pennyway.infra.common.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
@@ -13,8 +12,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-    private final JwtProvider accessTokenProvider;
-
     @Override
     public void addFormatters(FormatterRegistry registrar) {
         registrar.addConverter(new ProviderConverter());
