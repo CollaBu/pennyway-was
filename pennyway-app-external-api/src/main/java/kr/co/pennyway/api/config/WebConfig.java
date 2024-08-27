2851d1279f932ac5d9ec227e6fdb1b46354dd759
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-08-27 19:14:09 +0900

    fix: sigin event interceptor 제거

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/config/WebConfig.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/config/WebConfig.java
@@ -4,19 +4,15 @@ import kr.co.pennyway.api.common.converter.NotifyTypeConverter;
 import kr.co.pennyway.api.common.converter.ProviderConverter;
 import kr.co.pennyway.api.common.converter.SpendingCategoryTypeConverter;
 import kr.co.pennyway.api.common.converter.VerificationTypeConverter;
-import kr.co.pennyway.api.common.interceptor.SignEventLogInterceptor;
-import kr.co.pennyway.domain.common.redis.sign.SignEventLogService;
 import kr.co.pennyway.infra.common.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-    private final SignEventLogService signEventLogService;
     private final JwtProvider accessTokenProvider;
 
     @Override
@@ -25,11 +21,5 @@ public class WebConfig implements WebMvcConfigurer {
         registrar.addConverter(new VerificationTypeConverter());
         registrar.addConverter(new NotifyTypeConverter());
         registrar.addConverter(new SpendingCategoryTypeConverter());
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new SignEventLogInterceptor(signEventLogService, accessTokenProvider))
-                .addPathPatterns("/v1/auth/sign-in", "/v1/auth/oauth/sign-up", "/v1/auth/refresh");
     }
 }
