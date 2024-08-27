022de929fb5ca8bc0e614dea75f6ccdf8d168c98
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-08-27 19:12:59 +0900

    fix: sigin in event redis 코드 제거

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/common/redis/sign/SignEventLogRepository.java
+++
@@ -1,6 +0,0 @@
-package kr.co.pennyway.domain.common.redis.sign;
-
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface SignEventLogRepository extends ListCrudRepository<SignEventLog, Long> {
-}
