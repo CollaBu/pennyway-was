e0f5fe38b7b9b481765fe2f041e569f433a163f5
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-10-08 20:01:25 +0900

    chore: rabbitmq yml 설정

---
+++ pennyway-infra/src/main/java/kr/co/pennyway/infra/common/properties/RabbitMQProperties.java
@@ -0,0 +1,14 @@
+package kr.co.pennyway.infra.common.properties;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@RequiredArgsConstructor
+@ConfigurationProperties(prefix = "pennyway.rabbitmq.chat")
+public class RabbitMQProperties {
+    private final String exchange;
+    private final String queue;
+    private final String routing;
+}
