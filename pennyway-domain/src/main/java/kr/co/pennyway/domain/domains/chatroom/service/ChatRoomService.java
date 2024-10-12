6dfa61d7c32688997e9ca77204e67ea147ff8956
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-10-12 16:48:47 +0900

    feat: 채팅 멤버 생성 service 정의

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/chatroom/service/ChatRoomService.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/chatroom/service/ChatRoomService.java
@@ -1,14 +1,14 @@
 package kr.co.pennyway.domain.domains.chatroom.service;
 
+import kr.co.pennyway.common.annotation.DomainService;
 import kr.co.pennyway.domain.domains.chatroom.domain.ChatRoom;
 import kr.co.pennyway.domain.domains.chatroom.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
-@Service
+@DomainService
 @RequiredArgsConstructor
 public class ChatRoomService {
     private final ChatRoomRepository chatRoomRepository;
--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/member/service/ChatMemberService.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/member/service/ChatMemberService.java
@@ -1,6 +1,7 @@
 package kr.co.pennyway.domain.domains.member.service;
 
 import kr.co.pennyway.common.annotation.DomainService;
+import kr.co.pennyway.domain.domains.member.domain.ChatMember;
 import kr.co.pennyway.domain.domains.member.repository.ChatMemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,11 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ChatMemberService {
     private final ChatMemberRepository chatMemberRepository;
+
+    @Transactional
+    public void create(ChatMember chatMember) {
+        chatMemberRepository.save(chatMember);
+    }
 
     @Transactional(readOnly = true)
     public boolean isExists(Long chatRoomId, Long userId) {
