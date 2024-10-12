1fb48819ea352888fe45a3f2f950b3cb6ffe20b6
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-10-12 20:38:05 +0900

    fix: 채팅방 생성 요청 background_image_url pattern 수정

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/chat/dto/ChatRoomReq.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/chat/dto/ChatRoomReq.java
@@ -2,6 +2,7 @@ package kr.co.pennyway.api.apis.chat.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import kr.co.pennyway.domain.domains.chatroom.domain.ChatRoom;
 
@@ -15,7 +16,8 @@ public final class ChatRoomReq {
             @Size(min = 1, max = 100)
             @Schema(description = "채팅방 설명. NULL을 허용하며, 문자가 존재할 시 공백 허용 없이 1~100자 이내의 문자열이어야 한다.", example = "페니웨이 채팅방입니다.")
             String description,
-            @Schema(description = "채팅방 배경 이미지 URL. NULL을 허용한다.", example = "https://pennyway.co.kr/background.jpg")
+            @Schema(description = "채팅방 배경 이미지 URL. NULL을 허용한다.", example = "delete/chatroom/{chatroom_id}/{uuid}_{timestamp}.{ext}")
+            @Pattern(regexp = "^delete/.*", message = "채팅방 배경 이미지 URL은 delete/로 시작해야 합니다.")
             String backgroundImageUrl,
             @Schema(description = "채팅방 비밀번호. NULL을 허용한다. 비밀번호는 6자리 정수만 허용", example = "123456")
             @Size(min = 6, max = 6)
