f4299f282feb983e0c7d9b419b0bbd04629c4e5b
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-10-12 20:05:59 +0900

    feat: chat_room_mapper 정의

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/chat/usecase/ChatRoomUseCase.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/chat/usecase/ChatRoomUseCase.java
@@ -2,13 +2,20 @@ package kr.co.pennyway.api.apis.chat.usecase;
 
 import kr.co.pennyway.api.apis.chat.dto.ChatRoomReq;
 import kr.co.pennyway.api.apis.chat.dto.ChatRoomRes;
+import kr.co.pennyway.api.apis.chat.mapper.ChatRoomMapper;
+import kr.co.pennyway.api.apis.chat.service.ChatRoomSaveService;
 import kr.co.pennyway.common.annotation.UseCase;
+import kr.co.pennyway.domain.domains.chatroom.domain.ChatRoom;
 import lombok.RequiredArgsConstructor;
 
 @UseCase
 @RequiredArgsConstructor
 public class ChatRoomUseCase {
+    private final ChatRoomSaveService chatRoomSaveService;
+
     public ChatRoomRes.Detail createChatRoom(ChatRoomReq.Create request, Long userId) {
-        return null;
+        ChatRoom chatRoom = chatRoomSaveService.createChatRoom(request, userId);
+
+        return ChatRoomMapper.toChatRoomResDetail(chatRoom, 1);
     }
 }
