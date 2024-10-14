a738d10e050af5fb36f6ae76cec02fa252466cc1
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-10-14 13:42:12 +0900

    feat: ✨가입한 채팅방 목록 조회 API (#174)
    * feat: 각 모듈별 docker파일 분리
    
    * chore: common, infra, domain dockerfile 제거
    
    * feat: db - domain 채팅방 데이터 조회 결과를 담을 dto 생성
    
    * feat: 참여한 채팅방 리스트 조회 domain service 로직 구현
    
    * feat: 내가 가입한 채팅방 조회 usecase, application-service, mapper 구현
    
    * fix: 채팅방 응답 dto 생성 유효성 로직 정적 팩토리 메서드에서 생성자로 이전
    
    * feat: 내가 가입한 채팅방 목록 조회 api 개방
    
    * docs: swagger 문서 작성

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/chat/usecase/ChatRoomUseCase.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/chat/usecase/ChatRoomUseCase.java
@@ -4,14 +4,19 @@ import kr.co.pennyway.api.apis.chat.dto.ChatRoomReq;
 import kr.co.pennyway.api.apis.chat.dto.ChatRoomRes;
 import kr.co.pennyway.api.apis.chat.mapper.ChatRoomMapper;
 import kr.co.pennyway.api.apis.chat.service.ChatRoomSaveService;
+import kr.co.pennyway.api.apis.chat.service.ChatRoomSearchService;
 import kr.co.pennyway.common.annotation.UseCase;
 import kr.co.pennyway.domain.domains.chatroom.domain.ChatRoom;
+import kr.co.pennyway.domain.domains.chatroom.dto.ChatRoomDetail;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @UseCase
 @RequiredArgsConstructor
 public class ChatRoomUseCase {
     private final ChatRoomSaveService chatRoomSaveService;
+    private final ChatRoomSearchService chatRoomSearchService;
 
     public Long pendChatRoom(ChatRoomReq.Pend request, Long userId) {
         return chatRoomSaveService.pendChatRoom(request, userId);
@@ -21,5 +26,11 @@ public class ChatRoomUseCase {
         ChatRoom chatRoom = chatRoomSaveService.createChatRoom(request, userId);
 
         return ChatRoomMapper.toChatRoomResDetail(chatRoom, 1);
+    }
+
+    public List<ChatRoomRes.Detail> getChatRooms(Long userId) {
+        List<ChatRoomDetail> chatRooms = chatRoomSearchService.readChatRooms(userId);
+
+        return ChatRoomMapper.toChatRoomResDetailList(chatRooms);
     }
 }
--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/chatroom/service/ChatRoomService.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/chatroom/service/ChatRoomService.java
@@ -2,10 +2,13 @@ package kr.co.pennyway.domain.domains.chatroom.service;
 
 import kr.co.pennyway.common.annotation.DomainService;
 import kr.co.pennyway.domain.domains.chatroom.domain.ChatRoom;
+import kr.co.pennyway.domain.domains.chatroom.dto.ChatRoomDetail;
 import kr.co.pennyway.domain.domains.chatroom.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @DomainService
@@ -16,5 +19,10 @@ public class ChatRoomService {
     @Transactional
     public ChatRoom create(ChatRoom chatRoom) {
         return chatRoomRepository.save(chatRoom);
+    }
+
+    @Transactional
+    public List<ChatRoomDetail> readChatRoomsByUserId(Long userId) {
+        return chatRoomRepository.findChatRoomsByUserId(userId);
     }
 }
