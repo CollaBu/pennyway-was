f1e2616f5ab6698d42dfa603d063bfab99bd2cc7
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-10-17 12:17:58 +0900

    feat: user session 존재 여부 확인 메서드 추가

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/common/redis/session/UserSessionService.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/common/redis/session/UserSessionService.java
@@ -1,6 +1,5 @@
 package kr.co.pennyway.domain.common.redis.session;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import kr.co.pennyway.common.annotation.DomainService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,16 +13,22 @@ import java.util.Optional;
 public class UserSessionService {
     private final UserSessionRepository userSessionRepository;
 
-    public void create(Long userId, String hashKey, UserSession value) {
-        userSessionRepository.save(userId, hashKey, value);
+    public void create(Long userId, String domainId, UserSession value) {
+        userSessionRepository.save(userId, domainId, value);
     }
 
-    public Optional<UserSession> read(Long userId, String hashKey) throws JsonProcessingException {
-        return userSessionRepository.findUserSession(userId, hashKey);
+    public Optional<UserSession> read(Long userId, String domainId) {
+        return userSessionRepository.findUserSession(userId, domainId);
     }
 
-    public Map<String, UserSession> readAll(Long userId) throws JsonProcessingException {
+    public Map<String, UserSession> readAll(Long userId) {
         return userSessionRepository.findAllUserSessions(userId);
+    }
+
+    public boolean isExists(Long userId, String domainId) {
+        Optional<UserSession> userSession = userSessionRepository.findUserSession(userId, domainId);
+
+        return userSession.isPresent();
     }
 
     public Long getSessionTtl(Long userId, String hashKey) {
