0e1c1b68fc443e196129f35183b289e187c67490
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-10-19 01:00:54 +0900

    docs: swagger 문서 업그레이드

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/storage/dto/PresignedUrlDto.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/storage/dto/PresignedUrlDto.java
@@ -9,20 +9,14 @@ import kr.co.pennyway.infra.client.aws.s3.ObjectKeyType;
 import java.net.URI;
 
 public class PresignedUrlDto {
-    @Schema(title = "S3 이미지 저장을 위한 Presigned URL 발급 요청 DTO", description = "S3에 이미지를 저장하기 위한 Presigned URL을 발급 요청을 위한 DTO")
     public record Req(
-            @Schema(description = "이미지 종류", example = "PROFILE/FEED/CHATROOM_PROFILE/CHAT/CHAT_PROFILE")
             @NotNull(message = "이미지 종류는 필수입니다.")
             ObjectKeyType type,
-            @Schema(description = "파일 확장자", example = "jpg/png/jpeg")
             @NotBlank(message = "파일 확장자는 필수입니다.")
             @Pattern(regexp = "^(jpg|png|jpeg)$", message = "파일 확장자는 jpg, png, jpeg 중 하나여야 합니다.")
             String ext,
-            @Schema(description = "채팅방 ID", example = "123456789")
             Long chatroomId,
-            @Schema(description = "채팅 ID", example = "123456789")
             Long chatId,
-            @Schema(description = "피드 ID", example = "123456789")
             Long feedId
     ) {
     }
