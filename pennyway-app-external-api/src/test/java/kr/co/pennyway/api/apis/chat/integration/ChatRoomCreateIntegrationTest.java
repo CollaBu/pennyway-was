190918bc7aba845763f28ded3b7c42a124a199db
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-10-19 20:38:18 +0900

    test: 채팅방 생성 통합 테스트 기대값 수정

--- pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/chat/integration/ChatRoomCreateIntegrationTest.java
+++ pennyway-app-external-api/src/test/java/kr/co/pennyway/api/apis/chat/integration/ChatRoomCreateIntegrationTest.java
@@ -9,7 +9,6 @@ import kr.co.pennyway.api.config.ExternalApiDBTestConfig;
 import kr.co.pennyway.api.config.ExternalApiIntegrationTest;
 import kr.co.pennyway.api.config.fixture.ChatRoomFixture;
 import kr.co.pennyway.api.config.fixture.UserFixture;
-import kr.co.pennyway.domain.domains.chatroom.domain.ChatRoom;
 import kr.co.pennyway.domain.domains.user.domain.User;
 import kr.co.pennyway.domain.domains.user.service.UserService;
 import kr.co.pennyway.infra.client.aws.s3.ActualIdProvider;
@@ -27,12 +26,14 @@ import org.springframework.http.*;
 
 import java.util.Map;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 
 
 @Slf4j
 @ExternalApiIntegrationTest
-public class ChatRoomPendIntegrationTest extends ExternalApiDBTestConfig {
+public class ChatRoomCreateIntegrationTest extends ExternalApiDBTestConfig {
     @Autowired
     private TestRestTemplate restTemplate;
 
@@ -53,31 +54,19 @@ public class ChatRoomPendIntegrationTest extends ExternalApiDBTestConfig {
     void success() {
         // given
         User user = userService.createUser(UserFixture.GENERAL_USER.toUser());
-        ChatRoom room = ChatRoomFixture.PRIVATE_CHAT_ROOM.toEntity();
-        ChatRoomReq.Pend request = new ChatRoomReq.Pend(room.getTitle(), room.getDescription(), room.getPassword());
-        ChatRoomReq.Create request2 = new ChatRoomReq.Create(room.getBackgroundImageUrl());
-        given(awsS3Adapter.saveImage(room.getBackgroundImageUrl(), ActualIdProvider.createInstanceOfChatProfile())).willReturn("chatroom/1");
+        ChatRoomReq.Create request = ChatRoomFixture.PRIVATE_CHAT_ROOM.toCreateRequest();
+        given(awsS3Adapter.saveImage(eq(request.backgroundImageUrl()), any(ActualIdProvider.class))).willReturn("chatroom/1");
 
         // when
-        ResponseEntity<SuccessResponse<Map<String, Long>>> response = postPending(user, request);
-        ResponseEntity<SuccessResponse<Map<String, ChatRoomRes.Detail>>> response2 = postCreating(user, request2);
-        ChatRoomRes.Detail detail = response2.getBody().getData().get("chatRoom");
+        ResponseEntity<SuccessResponse<Map<String, ChatRoomRes.Detail>>> response = postCreating(user, request);
+        ChatRoomRes.Detail detail = response.getBody().getData().get("chatRoom");
 
         // then
-        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode(), "201 Created 응답을 받아야 합니다.");
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode(), "200 OK 응답을 받아야 합니다.");
         Assertions.assertEquals(request.title(), detail.title(), "생성된 채팅방의 제목이 일치해야 합니다.");
         Assertions.assertEquals(request.description(), detail.description(), "생성된 채팅방의 설명이 일치해야 합니다.");
+        Assertions.assertEquals("chatroom/1", detail.backgroundImageUrl(), "생성된 채팅방의 배경 이미지 URL이 일치해야 합니다.");
         Assertions.assertTrue(detail.isPrivate(), "생성된 채팅방은 비공개여야 합니다.");
-    }
-
-    private ResponseEntity<SuccessResponse<Map<String, Long>>> postPending(User user, ChatRoomReq.Pend request) {
-        return restTemplate.exchange(
-                "http://localhost:" + port + "/v2/chat-rooms/pend",
-                HttpMethod.POST,
-                createHttpEntity(user, request),
-                new ParameterizedTypeReference<>() {
-                }
-        );
     }
 
     private ResponseEntity<SuccessResponse<Map<String, ChatRoomRes.Detail>>> postCreating(User user, ChatRoomReq.Create request) {
@@ -88,14 +77,6 @@ public class ChatRoomPendIntegrationTest extends ExternalApiDBTestConfig {
                 new ParameterizedTypeReference<>() {
                 }
         );
-    }
-
-    private HttpEntity<?> createHttpEntity(User user, ChatRoomReq.Pend request) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + accessTokenProvider.generateToken(AccessTokenClaim.of(user.getId(), user.getRole().name())));
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        return new HttpEntity<>(request, headers);
     }
 
     private HttpEntity<?> createHttpEntity(User user, ChatRoomReq.Create request) {
