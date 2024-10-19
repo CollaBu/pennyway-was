0bfa89e50984da74b2b765a5a0a791000cdd486a
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-10-19 19:08:01 +0900

    fix: object_key_type에서 필수로 변경되어야 할 아이디 리스트 제공 메서드 추가

--- pennyway-infra/src/main/java/kr/co/pennyway/infra/client/aws/s3/ObjectKeyType.java
+++ pennyway-infra/src/main/java/kr/co/pennyway/infra/client/aws/s3/ObjectKeyType.java
@@ -1,15 +1,15 @@
 package kr.co.pennyway.infra.client.aws.s3;
 
-import lombok.RequiredArgsConstructor;
+import java.util.Arrays;
+import java.util.List;
 
-@RequiredArgsConstructor
 public enum ObjectKeyType {
     PROFILE("1", "PROFILE", "delete/profile/{user_id}/{uuid}_{timestamp}.{ext}", "profile/{user_id}/origin/{uuid}_{timestamp}.{ext}"),
-    FEED("2", "FEED", "delete/feed/{feed_id}/{uuid}_{timestamp}.{ext}", "feed/{feed_id}/origin/{uuid}_{timestamp}.{ext}"),
+    FEED("2", "FEED", "delete/feed/{feed_id}/{uuid}_{timestamp}.{ext}", "feed/{feed_id}/origin/{uuid}_{timestamp}.{ext}", "feed_id"),
     CHATROOM_PROFILE("3", "CHATROOM_PROFILE", "delete/chatroom/{chatroom_id}/{uuid}_{timestamp}.{ext}",
-            "chatroom/{chatroom_id}/origin/{uuid}_{timestamp}.{ext}"),
+            "chatroom/{chatroom_id}/origin/{uuid}_{timestamp}.{ext}", "chatroom_id"),
     CHAT("4", "CHAT", "delete/chatroom/{chatroom_id}/chat/{chat_id}/{uuid}_{timestamp}.{ext}",
-            "chatroom/{chatroom_id}/chat/{chat_id}/origin/{uuid}_{timestamp}.{ext}"),
+            "chatroom/{chatroom_id}/chat/{chat_id}/origin/{uuid}_{timestamp}.{ext}", "chat_id"),
     CHAT_PROFILE("5", "CHAT_PROFILE", "delete/chatroom/{chatroom_id}/chat_profile/{user_id}/{uuid}_{timestamp}.{ext}",
             "chatroom/{chatroom_id}/chat_profile/{user_id}/origin/{uuid}_{timestamp}.{ext}");
 
@@ -17,6 +17,15 @@ public enum ObjectKeyType {
     private final String type;
     private final String deleteTemplate;
     private final String originTemplate;
+    private final String[] requiredExchangeParams;
+
+    ObjectKeyType(String code, String type, String deleteTemplate, String originTemplate, String... requiredExchangeParams) {
+        this.code = code;
+        this.type = type;
+        this.deleteTemplate = deleteTemplate;
+        this.originTemplate = originTemplate;
+        this.requiredExchangeParams = requiredExchangeParams;
+    }
 
     public String getCode() {
         return code;
@@ -24,6 +33,10 @@ public enum ObjectKeyType {
 
     public String getType() {
         return type;
+    }
+
+    public List<String> getRequiredParams() {
+        return Arrays.asList(requiredExchangeParams);
     }
 
     public String getDeleteTemplate() {
