d63e0a8938db1d826dbefb21133a1723abdfa93d
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-10-21 15:11:07 +0900

    chore: mysql full-text-index query setting

---
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/config/MySqlFunctionContributor.java
@@ -0,0 +1,20 @@
+package kr.co.pennyway.domain.config;
+
+import org.hibernate.boot.model.FunctionContributions;
+import org.hibernate.boot.model.FunctionContributor;
+import org.hibernate.query.sqm.function.SqmFunctionRegistry;
+import org.hibernate.type.StandardBasicTypes;
+import org.hibernate.type.spi.TypeConfiguration;
+
+public class MySqlFunctionContributor implements FunctionContributor {
+    private static final String TWO_COLUMN_NATURAL_FUNCTION_NAME = "two_column_natural";
+    private static final String TWO_COLUMN_NATURAL_PATTERN = "match(?1, ?2) against(?3 in natural language mode)";
+
+    @Override
+    public void contributeFunctions(final FunctionContributions functionContributions) {
+        SqmFunctionRegistry registry = functionContributions.getFunctionRegistry();
+        TypeConfiguration typeConfiguration = functionContributions.getTypeConfiguration();
+
+        registry.registerPattern(TWO_COLUMN_NATURAL_FUNCTION_NAME, TWO_COLUMN_NATURAL_PATTERN, typeConfiguration.getBasicTypeRegistry().resolve(StandardBasicTypes.DOUBLE));
+    }
+}
