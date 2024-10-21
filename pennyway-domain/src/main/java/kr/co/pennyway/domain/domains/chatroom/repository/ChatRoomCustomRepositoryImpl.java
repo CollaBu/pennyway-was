7298db22a0228cf912ea7401c05a5ef3c497198f
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-10-21 15:44:22 +0900

    feat: implement search chatrooms business logic

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/chatroom/repository/ChatRoomCustomRepositoryImpl.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/chatroom/repository/ChatRoomCustomRepositoryImpl.java
@@ -1,11 +1,16 @@
 package kr.co.pennyway.domain.domains.chatroom.repository;
 
+import com.querydsl.core.types.ConstructorExpression;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import kr.co.pennyway.domain.common.util.QueryDslUtil;
+import kr.co.pennyway.domain.common.util.SliceUtil;
 import kr.co.pennyway.domain.domains.chatroom.domain.QChatRoom;
 import kr.co.pennyway.domain.domains.chatroom.dto.ChatRoomDetail;
 import kr.co.pennyway.domain.domains.member.domain.QChatMember;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -21,20 +26,7 @@ public class ChatRoomCustomRepositoryImpl implements ChatRoomCustomRepository {
     @Override
     public List<ChatRoomDetail> findChatRoomsByUserId(Long userId) {
         return queryFactory
-                .select(
-                        Projections.constructor(
-                                ChatRoomDetail.class,
-                                chatRoom.id,
-                                chatRoom.title,
-                                chatRoom.description,
-                                chatRoom.backgroundImageUrl,
-                                chatRoom.password,
-                                chatRoom.createdAt,
-                                queryFactory.select(chatMember.count().intValue())
-                                        .from(chatMember)
-                                        .where(chatMember.chatRoom.id.eq(chatRoom.id))
-                        )
-                )
+                .select(createChatRoomDetailConstructorExpression())
                 .from(chatRoom)
                 .where(
                         chatRoom.id.in(
@@ -44,5 +36,33 @@ public class ChatRoomCustomRepositoryImpl implements ChatRoomCustomRepository {
                         )
                 )
                 .fetch();
+    }
+
+    @Override
+    public Slice<ChatRoomDetail> findChatRooms(String target, Pageable pageable) {
+        List<ChatRoomDetail> results = queryFactory
+                .select(createChatRoomDetailConstructorExpression())
+                .from(chatRoom)
+                .where(QueryDslUtil.matchAgainstTwoElemNaturalMode(chatRoom.title, chatRoom.description, target))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize() + 1)
+                .fetch();
+
+        return SliceUtil.toSlice(results, pageable);
+    }
+
+    private ConstructorExpression<ChatRoomDetail> createChatRoomDetailConstructorExpression() {
+        return Projections.constructor(
+                ChatRoomDetail.class,
+                chatRoom.id,
+                chatRoom.title,
+                chatRoom.description,
+                chatRoom.backgroundImageUrl,
+                chatRoom.password,
+                chatRoom.createdAt,
+                queryFactory.select(chatMember.count().intValue())
+                        .from(chatMember)
+                        .where(chatMember.chatRoom.id.eq(chatRoom.id))
+        );
     }
 }
