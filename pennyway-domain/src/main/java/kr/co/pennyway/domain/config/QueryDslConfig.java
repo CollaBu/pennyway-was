1c65109f6129c94fecab2490692ad695f63e7392
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-10-23 18:32:48 +0900

    chore: query dsl sub-query를 위한 jpa_sql_query 설정 import

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/config/QueryDslConfig.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/config/QueryDslConfig.java
@@ -1,6 +1,8 @@
 package kr.co.pennyway.domain.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.querydsl.sql.MySQLTemplates;
+import com.querydsl.sql.SQLTemplates;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
@@ -16,5 +18,10 @@ public class QueryDslConfig {
     @Primary
     public JPAQueryFactory jpaQueryFactory() {
         return new JPAQueryFactory(entityManager);
+    }
+
+    @Bean
+    public SQLTemplates sqlTemplates() {
+        return new MySQLTemplates();
     }
 }
