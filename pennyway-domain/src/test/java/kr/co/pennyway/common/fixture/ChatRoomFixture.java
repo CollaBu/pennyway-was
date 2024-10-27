7ef6be7cf9636314230c7e7f2c05c6567bf755f0
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-10-27 21:16:56 +0900

    test: add user_fixture & chat_room_fixture within the domain test package

---
+++ pennyway-domain/src/test/java/kr/co/pennyway/common/fixture/ChatRoomFixture.java
@@ -0,0 +1,30 @@
+package kr.co.pennyway.common.fixture;
+
+import kr.co.pennyway.domain.domains.chatroom.domain.ChatRoom;
+
+public enum ChatRoomFixture {
+    PRIVATE_CHAT_ROOM("페니웨이", "페니웨이 채팅방입니다.", "delete/chatroom/1/fsdflasdfa_12121210.jpg", "123456"),
+    PUBLIC_CHAT_ROOM("페니웨이", "페니웨이 채팅방입니다.", "delete/chatroom/1/fsdflasdfa_12121210.jpg", null);
+
+    private final String title;
+    private final String description;
+    private final String backgroundImageUrl;
+    private final String password;
+
+    ChatRoomFixture(String title, String description, String backgroundImageUrl, String password) {
+        this.title = title;
+        this.description = description;
+        this.backgroundImageUrl = backgroundImageUrl;
+        this.password = password;
+    }
+
+    public ChatRoom toEntity() {
+        return ChatRoom.builder()
+                .id(1L)
+                .title(title)
+                .description(description)
+                .backgroundImageUrl(backgroundImageUrl)
+                .password(password != null ? Integer.valueOf(password) : null)
+                .build();
+    }
+}
