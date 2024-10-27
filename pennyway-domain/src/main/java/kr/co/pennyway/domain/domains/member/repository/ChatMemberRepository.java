57fdc467bd1f4199a276bb5080f93c5843d7a891
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-10-27 21:32:10 +0900

    feat: impl chat_member_repository

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/member/repository/ChatMemberRepository.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/domains/member/repository/ChatMemberRepository.java
@@ -3,5 +3,8 @@ package kr.co.pennyway.domain.domains.member.repository;
 import kr.co.pennyway.domain.common.repository.ExtendedRepository;
 import kr.co.pennyway.domain.domains.member.domain.ChatMember;
 
+import java.util.Set;
+
 public interface ChatMemberRepository extends ExtendedRepository<ChatMember, Long>, CustomChatMemberRepository {
+    Set<ChatMember> findByChat_Room_IdAndUser_Id(Long chatRoomId, Long userId);
 }
