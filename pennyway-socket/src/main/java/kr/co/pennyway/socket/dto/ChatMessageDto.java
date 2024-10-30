46660c8ccf0483db9304495cc178dde1f6293e52
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-10-30 22:42:03 +0900

    feat: add dto validation

--- pennyway-socket/src/main/java/kr/co/pennyway/socket/dto/ChatMessageDto.java
+++ pennyway-socket/src/main/java/kr/co/pennyway/socket/dto/ChatMessageDto.java
@@ -1,9 +1,14 @@
 package kr.co.pennyway.socket.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import kr.co.pennyway.domain.common.redis.message.type.MessageContentType;
 
 public record ChatMessageDto(
+        @NotNull(message = "메시지 내용은 null을 허용하지 않습니다.")
+        @Size(min = 1, max = 1000, message = "메시지 내용은 1자 이상 1000자 이하로 입력해주세요.")
         String content,
+        @NotNull(message = "메시지 타입은 null을 허용하지 않습니다.")
         MessageContentType contentType
 ) {
 }
