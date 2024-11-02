b281e6094ff34489adda10e677aed0df29de7f31
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-11-02 15:38:27 +0900

    docs: add information about recent_messages field's sorted in the chat_room_res

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/chat/dto/ChatRoomRes.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/chat/dto/ChatRoomRes.java
@@ -75,7 +75,7 @@ public final class ChatRoomRes {
             List<ChatMemberRes.Detail> recentParticipants,
             @Schema(description = "채팅방에서 내 정보와 최근 활동자를 제외한 참여자 ID 목록")
             List<Long> otherParticipantIds,
-            @Schema(description = "최근 채팅 이력")
+            @Schema(description = "최근 채팅 이력. 메시지는 최신순으로 정렬되어 반환.")
             List<ChatRes.Detail> recentMessages
     ) {
 
