d9ca63e9ae0b1b000ca6d23f484e9b1cfaaeab2d
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-11-02 14:12:26 +0900

    fix: insert joined_at field in the chat_member_res.detail dto

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/chat/dto/ChatMemberRes.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/chat/dto/ChatMemberRes.java
@@ -1,9 +1,14 @@
 package kr.co.pennyway.api.apis.chat.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import io.swagger.v3.oas.annotations.media.Schema;
 import kr.co.pennyway.domain.domains.member.domain.ChatMember;
 import kr.co.pennyway.domain.domains.member.type.ChatMemberRole;
+
+import java.time.LocalDateTime;
 
 public final class ChatMemberRes {
     @Schema(description = "채팅방 참여자 상세 정보")
@@ -16,14 +21,19 @@ public final class ChatMemberRes {
             ChatMemberRole role,
             @Schema(description = "채팅방 참여자 알림 설정 여부. 내 정보를 조회할 때만 포함됩니다.")
             @JsonInclude(JsonInclude.Include.NON_NULL)
-            Boolean notifyEnabled
+            Boolean notifyEnabled,
+            @Schema(description = "채팅방 가입일")
+            @JsonSerialize(using = LocalDateTimeSerializer.class)
+            @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+            LocalDateTime createdAt
     ) {
         public static Detail from(ChatMember chatMember, boolean isContainNotifyEnabled) {
             return new Detail(
                     chatMember.getId(),
                     chatMember.getName(),
                     chatMember.getRole(),
-                    isContainNotifyEnabled ? chatMember.isNotifyEnabled() : null
+                    isContainNotifyEnabled ? chatMember.isNotifyEnabled() : null,
+                    chatMember.getCreatedAt()
             );
         }
     }
