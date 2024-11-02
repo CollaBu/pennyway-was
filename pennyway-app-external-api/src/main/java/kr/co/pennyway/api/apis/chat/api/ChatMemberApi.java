d2d822ca5b6405648c2f0d3a5cd3dc8a001c7880
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-11-02 19:28:58 +0900

    docs: 채팅방 멤버 조회 시, 경고 사항 스웨거에 추가

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/chat/api/ChatMemberApi.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/chat/api/ChatMemberApi.java
@@ -53,7 +53,9 @@ public interface ChatMemberApi {
     @Operation(summary = "채팅방 멤버 조회", method = "GET", description = "채팅방 멤버 목록을 조회한다. 오로지 요청자의 채팅방 접근 권한만을 검사하며, 요청 아이디의 채팅방 포함 여부에 대한 검사 및 응답은 포함하지 않는다.")
     @Parameters({
             @Parameter(name = "chatRoomId", description = "채팅방 ID", required = true, in = ParameterIn.PATH),
-            @Parameter(name = "ids", description = "멤버 ID 목록. 중복을 허용하며, 순서가 일관되지 않아도 된다. 단, 최대 50개까지 조회 가능하며, null을 허용하지 않는다. ", required = true, in = ParameterIn.QUERY, array = @ArraySchema(schema = @Schema(type = "integer")))
+            @Parameter(name = "ids", description = """
+                    멤버 ID 목록. 중복을 허용하며, 순서가 일관되지 않아도 된다. 단, 최대 50개까지 조회 가능하며, null을 허용하지 않는다. 값은 `[채팅방 API] 채팅방 조회`의 응답으로 얻은 `otherParticipantIds`의 값을 사용하면 된다. (주의, userId가 아님!)"""
+                    , required = true, in = ParameterIn.QUERY, array = @ArraySchema(schema = @Schema(type = "integer")))
     })
     @ApiResponseExplanations(errors = {
             @ApiExceptionExplanation(value = ApiErrorCode.class, constant = "OVERFLOW_QUERY_PARAMETER", summary = "쿼리 파라미터 오버플로우", description = "쿼리 파라미터가 최대 개수를 초과하여 채팅방 멤버 조회에 실패했습니다.")
