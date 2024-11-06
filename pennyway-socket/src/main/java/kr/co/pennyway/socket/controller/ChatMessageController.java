cfa8299f8ca3a89391e0276dc38db059601257a5
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-11-06 19:17:31 +0900

    feat: add message path about read_message

--- pennyway-socket/src/main/java/kr/co/pennyway/socket/controller/ChatMessageController.java
+++ pennyway-socket/src/main/java/kr/co/pennyway/socket/controller/ChatMessageController.java
@@ -1,10 +1,12 @@
 package kr.co.pennyway.socket.controller;
 
+import jakarta.validation.constraints.NotNull;
 import kr.co.pennyway.socket.command.SendMessageCommand;
 import kr.co.pennyway.socket.common.annotation.PreAuthorize;
 import kr.co.pennyway.socket.common.security.authenticate.UserPrincipal;
 import kr.co.pennyway.socket.dto.ChatMessageDto;
 import kr.co.pennyway.socket.service.ChatMessageSendService;
+import kr.co.pennyway.socket.service.LastMessageIdSaveService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -17,10 +19,20 @@ import org.springframework.validation.annotation.Validated;
 @RequiredArgsConstructor
 public class ChatMessageController {
     private final ChatMessageSendService chatMessageSendService;
+    private final LastMessageIdSaveService lastMessageIdSaveService;
 
     @MessageMapping("chat.message.{chatRoomId}")
     @PreAuthorize("#isAuthenticated(#principal) and @chatRoomAccessChecker.hasPermission(#chatRoomId, #principal)")
     public void sendMessage(@DestinationVariable Long chatRoomId, @Validated ChatMessageDto.Request payload, UserPrincipal principal) {
         chatMessageSendService.execute(SendMessageCommand.createUserMessage(chatRoomId, payload.content(), payload.contentType(), principal.getUserId()));
+    }
+
+    @MessageMapping("chat.message.{chatRoomId}.read.{chatRoomId}")
+    @PreAuthorize("#isAuthenticated(#principal) and @chatRoomAccessChecker.hasPermission(#chatRoomId, #principal)")
+    public void readMessage(@DestinationVariable(value = "chatRoomId") @Validated @NotNull Long chatRoomId,
+                            @DestinationVariable(value = "lastReadMessageId") @Validated @NotNull Long lastReadMessageId,
+                            UserPrincipal principal
+    ) {
+        lastMessageIdSaveService.execute(principal.getUserId(), chatRoomId, lastReadMessageId);
     }
 }
