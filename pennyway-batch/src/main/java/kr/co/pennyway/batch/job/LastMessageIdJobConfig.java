d7b3362b890b8b6c06507e79f5e3a8fa8a2302e5
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-11-07 21:15:13 +0900

    chore: convert reader component to bean in job config

--- pennyway-batch/src/main/java/kr/co/pennyway/batch/job/LastMessageIdJobConfig.java
+++ pennyway-batch/src/main/java/kr/co/pennyway/batch/job/LastMessageIdJobConfig.java
@@ -9,22 +9,26 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.Cursor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @RequiredArgsConstructor
 public class LastMessageIdJobConfig {
     private static final int CHUNK_SIZE = 1000;
-
+    private static final String PREFIX_PATTERN = "chat:last_read:*";
     private final JobRepository jobRepository;
-    private final LastMessageIdReader reader;
     private final LastMessageIdProcessor processor;
     private final LastMessageIdWriter writer;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Bean
     public Job lastMessageIdJob(PlatformTransactionManager transactionManager) {
@@ -43,9 +47,17 @@ public class LastMessageIdJobConfig {
     public Step lastMessageIdStep(PlatformTransactionManager transactionManager) {
         return new StepBuilder("lastMessageIdStep", jobRepository)
                 .<KeyValue, ChatMessageStatus>chunk(CHUNK_SIZE, transactionManager)
-                .reader(reader)
+                .reader(lastMessageIdReader())
                 .processor(processor)
                 .writer(writer)
                 .build();
+    }
+
+    @Bean
+    @StepScope
+    public LastMessageIdReader lastMessageIdReader() {
+        ScanOptions options = ScanOptions.scanOptions().match(PREFIX_PATTERN).count(CHUNK_SIZE).build();
+        Cursor<String> cursor = redisTemplate.scan(options);
+        return new LastMessageIdReader(redisTemplate, cursor);
     }
 }
