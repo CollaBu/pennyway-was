fe006f312a7b9ad22a0c1e35d23fe84f202ff3b1
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-11-09 18:18:26 +0900

    feat: impl chat_controller

---
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/chat/controller/ChatController.java
@@ -0,0 +1,29 @@
+package kr.co.pennyway.api.apis.chat.controller;
+
+import kr.co.pennyway.api.apis.chat.usecase.ChatUseCase;
+import kr.co.pennyway.api.common.response.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v2/chat-rooms/{chatRoomId}/chats")
+public class ChatController {
+    private static final String CHATS = "chats";
+
+    private final ChatUseCase chatUseCase;
+
+    @GetMapping("")
+    @PreAuthorize("isAuthenticated() and @chatRoomManager.hasPermission(principal.userId, #chatRoomId)")
+    public ResponseEntity<?> readChats(
+            @PathVariable("chatRoomId") Long chatRoomId,
+            @RequestParam(value = "lastMessageId") Long lastMessageId,
+            @RequestParam(value = "size", defaultValue = "30") int size
+    ) {
+        return ResponseEntity.ok(SuccessResponse.from(CHATS, chatUseCase.readChats(chatRoomId, lastMessageId, size)));
+    }
+}
