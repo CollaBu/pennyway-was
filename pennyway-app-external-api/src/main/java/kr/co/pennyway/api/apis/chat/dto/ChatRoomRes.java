1f28b3d5a2b446e49c66db327e079103f4cee95d
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-11-11 14:25:12 +0900

    feat: add chat_room_res info dto for service return data mapping

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/chat/dto/ChatRoomRes.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/chat/dto/ChatRoomRes.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import io.swagger.v3.oas.annotations.media.Schema;
 import kr.co.pennyway.domain.domains.chatroom.domain.ChatRoom;
+import kr.co.pennyway.domain.domains.chatroom.dto.ChatRoomDetail;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
@@ -13,6 +14,23 @@ import java.util.Objects;
 import java.util.Set;
 
 public final class ChatRoomRes {
+    /**
+     * 채팅방 정보를 담기 위한 DTO
+     *
+     * @param chatRoom           {@link ChatRoomDetail} : 채팅방 정보
+     * @param unreadMessageCount long : 읽지 않은 메시지 수
+     * @param lastMessage        {@link ChatRes.ChatDetail} : 가장 최근 메시지. 없을 경우 null
+     */
+    public record Info(
+            ChatRoomDetail chatRoom,
+            long unreadMessageCount,
+            ChatRes.ChatDetail lastMessage
+    ) {
+        public static Info of(ChatRoomDetail chatRoom, long unreadMessageCount, ChatRes.ChatDetail recentMessage) {
+            return new Info(chatRoom, unreadMessageCount, recentMessage);
+        }
+    }
+
     @Schema(description = "채팅방 상세 정보")
     public record Detail(
             @Schema(description = "채팅방 ID", type = "long")
@@ -33,10 +51,12 @@ public final class ChatRoomRes {
             @JsonSerialize(using = LocalDateTimeSerializer.class)
             @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
             LocalDateTime createdAt,
+            @Schema(description = "마지막 메시지 정보. 없을 경우 null이 반환된다.")
+            ChatRes.ChatDetail lastMessage,
             @Schema(description = "읽지 않은 메시지 수. 100 이상의 값을 가지면, 100으로 표시된다.")
             long unreadMessageCount
     ) {
-        public Detail(Long id, String title, String description, String backgroundImageUrl, boolean isPrivate, boolean isAdmin, int participantCount, LocalDateTime createdAt, long unreadMessageCount) {
+        public Detail(Long id, String title, String description, String backgroundImageUrl, boolean isPrivate, boolean isAdmin, int participantCount, LocalDateTime createdAt, ChatRes.ChatDetail lastMessage, long unreadMessageCount) {
             this.id = id;
             this.title = title;
             this.description = Objects.toString(description, "");
@@ -45,10 +65,11 @@ public final class ChatRoomRes {
             this.isAdmin = isAdmin;
             this.participantCount = participantCount;
             this.createdAt = createdAt;
+            this.lastMessage = lastMessage;
             this.unreadMessageCount = (unreadMessageCount > 100) ? 100 : unreadMessageCount;
         }
 
-        public static Detail of(ChatRoom chatRoom, boolean isAdmin, int participantCount, long unreadMessageCount) {
+        public static Detail of(ChatRoom chatRoom, ChatRes.ChatDetail lastMessage, boolean isAdmin, int participantCount, long unreadMessageCount) {
             return new Detail(
                     chatRoom.getId(),
                     chatRoom.getTitle(),
@@ -58,7 +79,23 @@ public final class ChatRoomRes {
                     isAdmin,
                     participantCount,
                     chatRoom.getCreatedAt(),
+                    lastMessage,
                     unreadMessageCount
+            );
+        }
+
+        public static Detail from(ChatRoomRes.Info info) {
+            return new Detail(
+                    info.chatRoom().id(),
+                    info.chatRoom().title(),
+                    info.chatRoom().description(),
+                    info.chatRoom().backgroundImageUrl(),
+                    info.chatRoom().password() != null,
+                    info.chatRoom().isAdmin(),
+                    info.chatRoom().participantCount(),
+                    info.chatRoom().createdAt(),
+                    info.lastMessage(),
+                    info.unreadMessageCount()
             );
         }
     }
