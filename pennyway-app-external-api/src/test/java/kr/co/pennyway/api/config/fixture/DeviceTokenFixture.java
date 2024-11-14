b4081fae20bf9dccc4b0c4308447216dc6239760
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-11-14 17:51:32 +0900

    test: device_token_fixture fixed

--- pennyway-app-external-api/src/test/java/kr/co/pennyway/api/config/fixture/DeviceTokenFixture.java
+++ pennyway-app-external-api/src/test/java/kr/co/pennyway/api/config/fixture/DeviceTokenFixture.java
@@ -5,20 +5,25 @@ import kr.co.pennyway.domain.domains.device.domain.DeviceToken;
 import kr.co.pennyway.domain.domains.user.domain.User;
 
 public enum DeviceTokenFixture {
-    INIT("originToken"),
-    CHANGED_TOKEN("newToken");
+    INIT("originToken", "AAA-BBBB-CC-DDDD-EE-FFF", "iPhone 15 Pro"),
+    CHANGED_TOKEN("newToken", "AAA-BBBB-CC-DDDD-EE-FFF", "iPhone 15 Pro"),
+    ;
 
     private final String token;
+    private final String deviceId;
+    private final String deviceName;
 
-    DeviceTokenFixture(String token) {
+    DeviceTokenFixture(String token, String deviceId, String deviceName) {
         this.token = token;
+        this.deviceId = deviceId;
+        this.deviceName = deviceName;
     }
 
     public DeviceToken toDevice(User user) {
-        return DeviceToken.of(token, user);
+        return DeviceToken.of(token, deviceId, deviceName, user);
     }
 
     public DeviceTokenDto.RegisterReq toRegisterReq() {
-        return new DeviceTokenDto.RegisterReq(token);
+        return new DeviceTokenDto.RegisterReq(token, deviceId, deviceName);
     }
 }
