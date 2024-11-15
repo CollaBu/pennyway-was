3bbdc574d885c481c52ea59d6649f850300ebf65
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-11-15 20:52:30 +0900

    feat: impl delete_all_by_user_id method in custom repository

--- pennyway-domain/src/main/java/kr/co/pennyway/domain/common/redis/refresh/RefreshTokenRepository.java
+++ pennyway-domain/src/main/java/kr/co/pennyway/domain/common/redis/refresh/RefreshTokenRepository.java
@@ -2,5 +2,5 @@ package kr.co.pennyway.domain.common.redis.refresh;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String>, RefreshTokenCustomRepository {
 }
