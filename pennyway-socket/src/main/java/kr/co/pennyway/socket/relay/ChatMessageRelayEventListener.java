127e23626d8552355d43e5d856332837a4c93003
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-11-18 12:58:56 +0900

    refactor: separate business logic from listener to service class

---
+++ pennyway-socket/src/main/java/kr/co/pennyway/socket/relay/ChatMessageRelayEventListener.java
@@ -0,0 +1,34 @@
+package kr.co.pennyway.socket.relay;
+
+import kr.co.pennyway.socket.dto.ChatMessageDto;
+import kr.co.pennyway.socket.service.ChatMessageRelayService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ChatMessageRelayEventListener {
+    private final ChatMessageRelayService chatMessageRelayService;
+
+    @RabbitListener(
+            containerFactory = "simpleRabbitListenerContainerFactory",
+            bindings = @QueueBinding(
+                    value = @Queue("${pennyway.rabbitmq.chat.queue}"),
+                    exchange = @Exchange(value = "${pennyway.rabbitmq.chat.exchange}", type = "topic"),
+                    key = "${pennyway.rabbitmq.chat.routing-key}"
+            ),
+            exclusive = true,
+            concurrency = "1"
+    )
+    public void handleSendEvent(ChatMessageDto.Response event) {
+        log.info("ChatMessageSendEventListener.handleSendEvent: {}", event);
+
+        chatMessageRelayService.execute(event.senderId(), event.chatRoomId(), event.content());
+    }
+}
