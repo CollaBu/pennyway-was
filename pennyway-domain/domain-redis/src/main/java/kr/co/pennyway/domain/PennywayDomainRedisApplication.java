5f95c450f65d7f49832795b83fe50674dcb2a06a
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-11-22 17:55:33 +0900

    refactor: add domain-redis module in domain module

---
+++ pennyway-domain/domain-redis/src/main/java/kr/co/pennyway/domain/PennywayDomainRedisApplication.java
@@ -0,0 +1,11 @@
+package kr.co.pennyway.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PennywayDomainRedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PennywayDomainRedisApplication.class, args);
+    }
+}
