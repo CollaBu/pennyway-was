33e6d227c5b2d8a0e1a701aff542765c329449a7
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-11-22 19:12:36 +0900

    refactor: move repositories to rds module

---
+++ pennyway-domain/domain-rds/src/main/java/kr/co/pennyway/domain/common/repository/ExtendedRepositoryFactory.java
@@ -0,0 +1,53 @@
+package kr.co.pennyway.domain.common.repository;
+
+import jakarta.persistence.EntityManager;
+import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
+import org.springframework.data.jpa.repository.support.JpaRepositoryFactoryBean;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.core.RepositoryMetadata;
+import org.springframework.data.repository.core.support.RepositoryComposition;
+import org.springframework.data.repository.core.support.RepositoryFactorySupport;
+import org.springframework.lang.NonNull;
+
+public class ExtendedRepositoryFactory<T extends Repository<E, ID>, E, ID> extends JpaRepositoryFactoryBean<T, E, ID> {
+    /**
+     * Creates a new {@link JpaRepositoryFactoryBean} for the given repository interface.
+     *
+     * @param repositoryInterface must not be {@literal null}.
+     */
+    public ExtendedRepositoryFactory(Class<? extends T> repositoryInterface) {
+        super(repositoryInterface);
+    }
+
+    @Override
+    @NonNull
+    protected RepositoryFactorySupport createRepositoryFactory(@NonNull EntityManager em) {
+        return new InnerRepositoryFactory(em);
+    }
+
+    private static class InnerRepositoryFactory extends JpaRepositoryFactory {
+        private final EntityManager em;
+
+        public InnerRepositoryFactory(EntityManager em) {
+            super(em);
+            this.em = em;
+        }
+
+        @Override
+        @NonNull
+        protected RepositoryComposition.RepositoryFragments getRepositoryFragments(@NonNull RepositoryMetadata metadata) {
+            RepositoryComposition.RepositoryFragments fragments = super.getRepositoryFragments(metadata);
+
+            if (QueryDslSearchRepository.class.isAssignableFrom(metadata.getRepositoryInterface())) {
+                var implExtendedJpa = super.instantiateClass(
+                        QueryDslSearchRepositoryImpl.class,
+                        this.getEntityInformation(metadata.getDomainType()),
+                        this.em
+                );
+                fragments = fragments.append(RepositoryComposition.RepositoryFragments.just(implExtendedJpa));
+            }
+
+            return fragments;
+        }
+    }
+}
