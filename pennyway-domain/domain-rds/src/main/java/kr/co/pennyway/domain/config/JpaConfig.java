f3df099ca86dcb9fda7683207d098df997beee03
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-11-22 19:11:00 +0900

    refactor: move rds config to rds module

---
+++ pennyway-domain/domain-rds/src/main/java/kr/co/pennyway/domain/config/JpaConfig.java
@@ -0,0 +1,16 @@
+package kr.co.pennyway.domain.config;
+
+import kr.co.pennyway.domain.DomainRdsLocation;
+import kr.co.pennyway.domain.common.repository.ExtendedRepositoryFactory;
+import kr.co.pennyway.domain.domains.JpaPackageLocation;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaAuditing
+@EntityScan(basePackageClasses = DomainRdsLocation.class)
+@EnableJpaRepositories(basePackageClasses = JpaPackageLocation.class, repositoryFactoryBeanClass = ExtendedRepositoryFactory.class)
+public class JpaConfig {
+}
