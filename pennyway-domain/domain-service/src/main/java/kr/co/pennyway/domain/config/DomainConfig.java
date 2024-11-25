4e6fd7821d02811d3172cce43044df27152c06fe
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-11-25 16:59:59 +0900

    fix: delete domain config in batch module

--- pennyway-domain/domain-service/src/main/java/kr/co/pennyway/domain/config/DomainConfig.java
+++ pennyway-domain/domain-service/src/main/java/kr/co/pennyway/domain/config/DomainConfig.java
@@ -1,14 +1,8 @@
 package kr.co.pennyway.domain.config;
 
-import kr.co.pennyway.domain.common.importer.EnablePennywayRdbDomainConfig;
 import kr.co.pennyway.domain.common.importer.EnablePennywayRedisDomainConfig;
-import kr.co.pennyway.domain.common.importer.PennywayRdbDomainConfigGroup;
 import kr.co.pennyway.domain.common.importer.PennywayRedisDomainConfigGroup;
 
-@EnablePennywayRdbDomainConfig({
-        PennywayRdbDomainConfigGroup.QUERY_DSL,
-        PennywayRdbDomainConfigGroup.JPA
-})
 @EnablePennywayRedisDomainConfig({
         PennywayRedisDomainConfigGroup.REDIS,
         PennywayRedisDomainConfigGroup.REDISSON,
