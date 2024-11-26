000af8456f3dce821cda52a7ab2b5ba4fb752676
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-11-26 13:19:43 +0900

    fix: separate constants of url_paths from security_config

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/config/security/WebSecurityUrls.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/config/security/WebSecurityUrls.java
@@ -1,9 +1,9 @@
 package kr.co.pennyway.api.config.security;
 
-public class WebSecurityUrls {
-    protected static final String[] READ_ONLY_PUBLIC_ENDPOINTS = {"/favicon.ico", "/v1/duplicate/**"};
-    protected static final String[] PUBLIC_ENDPOINTS = {"/v1/questions/**"};
-    protected static final String[] ANONYMOUS_ENDPOINTS = {"/v1/auth/**", "/v1/phone/**"};
-    protected static final String[] AUTHENTICATED_ENDPOINTS = {"/v1/auth"};
-    protected static final String[] SWAGGER_ENDPOINTS = {"/api-docs/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger",};
+public final class WebSecurityUrls {
+    public static final String[] READ_ONLY_PUBLIC_ENDPOINTS = {"/favicon.ico", "/v1/duplicate/**"};
+    public static final String[] PUBLIC_ENDPOINTS = {"/v1/questions/**"};
+    public static final String[] ANONYMOUS_ENDPOINTS = {"/v1/auth/**", "/v1/phone/**", "/v1/find/**"};
+    public static final String[] AUTHENTICATED_ENDPOINTS = {"/v1/auth"};
+    public static final String[] SWAGGER_ENDPOINTS = {"/api-docs/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger",};
 }
