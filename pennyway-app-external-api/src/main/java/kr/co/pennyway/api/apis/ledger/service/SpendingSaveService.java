da405fe32936c306fb5da67752763bbd5a8e73bc
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-11-26 14:17:52 +0900

    fix: modify package path in spending_save_service

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/service/SpendingSaveService.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/service/SpendingSaveService.java
@@ -1,16 +1,16 @@
 package kr.co.pennyway.api.apis.ledger.service;
 
 import kr.co.pennyway.api.apis.ledger.dto.SpendingReq;
+import kr.co.pennyway.domain.context.account.service.UserService;
+import kr.co.pennyway.domain.context.finance.service.SpendingCategoryService;
+import kr.co.pennyway.domain.context.finance.service.SpendingService;
 import kr.co.pennyway.domain.domains.spending.domain.Spending;
 import kr.co.pennyway.domain.domains.spending.domain.SpendingCustomCategory;
 import kr.co.pennyway.domain.domains.spending.exception.SpendingErrorCode;
 import kr.co.pennyway.domain.domains.spending.exception.SpendingErrorException;
-import kr.co.pennyway.domain.domains.spending.service.SpendingCustomCategoryService;
-import kr.co.pennyway.domain.domains.spending.service.SpendingService;
 import kr.co.pennyway.domain.domains.user.domain.User;
 import kr.co.pennyway.domain.domains.user.exception.UserErrorCode;
 import kr.co.pennyway.domain.domains.user.exception.UserErrorException;
-import kr.co.pennyway.domain.domains.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -22,7 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class SpendingSaveService {
     private final UserService userService;
     private final SpendingService spendingService;
-    private final SpendingCustomCategoryService spendingCustomCategoryService;
+    private final SpendingCategoryService spendingCategoryService;
 
     @Transactional
     public Spending createSpending(Long userId, SpendingReq request) {
@@ -32,7 +32,7 @@ public class SpendingSaveService {
         if (!request.isCustomCategory()) {
             spending = spendingService.createSpending(request.toEntity(user));
         } else {
-            SpendingCustomCategory customCategory = spendingCustomCategoryService.readSpendingCustomCategory(request.categoryId())
+            SpendingCustomCategory customCategory = spendingCategoryService.readSpendingCustomCategory(request.categoryId())
                     .orElseThrow(() -> new SpendingErrorException(SpendingErrorCode.NOT_FOUND_CUSTOM_CATEGORY));
 
             spending = spendingService.createSpending(request.toEntity(user, customCategory));
