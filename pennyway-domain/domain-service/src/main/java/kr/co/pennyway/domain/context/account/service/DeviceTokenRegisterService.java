880b1186f914ede1f71e073829a63712ffda4abf
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-12-19 21:55:36 +0900

    rename: minimalize service's javadoc

--- pennyway-domain/domain-service/src/main/java/kr/co/pennyway/domain/context/account/service/DeviceTokenRegisterService.java
+++ pennyway-domain/domain-service/src/main/java/kr/co/pennyway/domain/context/account/service/DeviceTokenRegisterService.java
@@ -23,14 +23,6 @@ public class DeviceTokenRegisterService {
     /**
      * 사용자의 디바이스 토큰을 생성하거나 갱신한다.
      *
-     * <pre>
-     * [비즈니스 규칙]
-     * - 같은 {userId, deviceId}에 대해 새로운 토큰이 발급될 수 있지만, 활성화된 토큰은 하나여야 합니다.
-     * - {deviceId, token} 조합은 시스템 전체에서 유일해야 합니다.
-     * - device token이 이미 등록된 경우, 소유자 정보를 갱신하고 마지막 로그인 시간을 갱신한다.
-     * - device token이 등록되지 않은 경우, 새로운 device token을 생성한다.
-     * </pre>
-     *
      * @param userId      사용자 식별자
      * @param deviceId    디바이스 식별자
      * @param deviceName  디바이스 이름
