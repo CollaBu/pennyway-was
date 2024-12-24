0427205e88361c09c7e62ffd1fe0d491bbf1991a
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-12-24 16:26:12 +0900

    rename: application file java to kotlin

--- pennyway-socket/src/main/java/kr/co/pennyway/PennywaySocketApplication.kt
+++ pennyway-socket/src/main/java/kr/co/pennyway/PennywaySocketApplication.kt
@@ -1,9 +1,5 @@
 package kr.co.pennyway;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import java.util.TimeZone;
 
 @SpringBootApplication
