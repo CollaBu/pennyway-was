6eaa4370d1f2a8fc76ac983117c891bedaf2c522
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-01-07 13:15:39 +0900

    fix: success message send to client after finished chat send process

--- pennyway-socket/src/main/java/kr/co/pennyway/socket/service/ChatMessageSendService.kt
+++ pennyway-socket/src/main/java/kr/co/pennyway/socket/service/ChatMessageSendService.kt
@@ -8,6 +8,7 @@ import kr.co.pennyway.infra.common.properties.ChatExchangeProperties
 import kr.co.pennyway.socket.command.SendMessageCommand
 import kr.co.pennyway.socket.common.dto.ChatMessageDto
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Component
 
 @Component
@@ -16,7 +17,8 @@ class ChatMessageSendService(
     private val chatMessageService: ChatMessageService,
     private val messageBrokerAdapter: MessageBrokerAdapter,
     private val idGenerator: IdGenerator<Long>,
-    private val chatExchangeProperties: ChatExchangeProperties
+    private val chatExchangeProperties: ChatExchangeProperties,
+    private val simpMessagingTemplate: SimpMessagingTemplate
 ) {
     /**
      * 채팅 메시지를 전송한다.
@@ -24,20 +26,33 @@ class ChatMessageSendService(
      * @param command SendMessageCommand : 채팅 메시지 전송을 위한 Command
      */
     fun execute(command: SendMessageCommand) {
-        val message = ChatMessageBuilder.builder()
-            .chatRoomId(command.chatRoomId())
-            .chatId(idGenerator.generate())
-            .content(command.content())
-            .contentType(command.contentType())
-            .categoryType(command.categoryType())
-            .sender(command.senderId())
-            .build()
+        val message = command.toChatMessage(command)
             .let { chatMessageService.create(it) }
 
-        messageBrokerAdapter.convertAndSend(
-            chatExchangeProperties.exchange,
-            "chat.room.${command.chatRoomId()}",
-            ChatMessageDto.Response.from(message)
-        )
+        with(chatExchangeProperties) {
+            messageBrokerAdapter.convertAndSend(
+                exchange,
+                "chat.room.${command.chatRoomId}",
+                ChatMessageDto.Response.from(message)
+            )
+        }
+        
+        command.senderName.takeIf { it.isNotBlank() }?.let { senderName ->
+            simpMessagingTemplate.convertAndSendToUser(
+                senderName,
+                "/queue/chat/success",
+                message,
+                command.messageIdHeader()
+            )
+        }
     }
+
+    private fun SendMessageCommand.toChatMessage(command: SendMessageCommand) = ChatMessageBuilder.builder()
+        .chatRoomId(command.chatRoomId())
+        .chatId(idGenerator.generate())
+        .content(command.content())
+        .contentType(command.contentType())
+        .categoryType(command.categoryType())
+        .sender(command.senderId())
+        .build()
 }
