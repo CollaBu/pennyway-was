da8d58d3f476edbb55d01fb3464ca6a3c6f2a890
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-01-07 19:15:56 +0900

    fix: sperate business logic from service

--- pennyway-domain/domain-service/src/main/java/kr/co/pennyway/domain/context/chat/service/ChatRoomLeaveService.java
+++ pennyway-domain/domain-service/src/main/java/kr/co/pennyway/domain/context/chat/service/ChatRoomLeaveService.java
@@ -1,7 +1,7 @@
 package kr.co.pennyway.domain.context.chat.service;
 
 import kr.co.pennyway.common.annotation.DomainService;
-import kr.co.pennyway.domain.domains.chatroom.domain.ChatRoom;
+import kr.co.pennyway.domain.context.chat.collection.ChatRoomLeaveCollection;
 import kr.co.pennyway.domain.domains.chatroom.service.ChatRoomRdbService;
 import kr.co.pennyway.domain.domains.member.domain.ChatMember;
 import kr.co.pennyway.domain.domains.member.exception.ChatMemberErrorCode;
@@ -26,22 +26,11 @@ public class ChatRoomLeaveService {
                     return new ChatMemberErrorException(ChatMemberErrorCode.NOT_FOUND);
                 });
 
-        // 방장이면 채팅방에 자신만 남아있어야 하고, 그렇지 않으면 예외를 던진다.
-        // 만약 삭제 가능하다면, 방장을 탈퇴시키고 채팅방을 삭제한다.
-        if (chatMember.isAdmin()) {
-            ChatRoom chatRoom = chatMember.getChatRoom();
+        var result = new ChatRoomLeaveCollection(chatMember).leave();
 
-            if (chatRoom.hasOnlyAdmin()) {
-                chatMember.leave();
-                chatMemberRdbService.update(chatMember);
-                chatRoomRdbService.delete(chatRoom);
-            } else {
-                log.warn("채팅방 방장은 채팅방을 탈퇴할 수 없습니다. chatRoomId: {}, chatMemberId: {}", chatRoom.getId(), chatMemberId);
-                throw new ChatMemberErrorException(ChatMemberErrorCode.ADMIN_CANNOT_LEAVE);
-            }
+        chatMemberRdbService.update(result.chatMember());
+        if (result.shouldDeleteChatRoom()) {
+            chatRoomRdbService.delete(chatMember.getChatRoom());
         }
-
-        chatMember.leave();
-        chatMemberRdbService.update(chatMember);
     }
 }
