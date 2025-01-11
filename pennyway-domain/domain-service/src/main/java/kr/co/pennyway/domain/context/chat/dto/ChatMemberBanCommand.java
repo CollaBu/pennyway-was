5163bfb3f81d9e9e2f04311c390edcd8febed513
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-01-11 19:25:47 +0900

    rename: modify admin-id to user-id cause it isn't identicated already

--- pennyway-domain/domain-service/src/main/java/kr/co/pennyway/domain/context/chat/dto/ChatMemberBanCommand.java
+++ pennyway-domain/domain-service/src/main/java/kr/co/pennyway/domain/context/chat/dto/ChatMemberBanCommand.java
@@ -1,19 +1,23 @@
 package kr.co.pennyway.domain.context.chat.dto;
 
 public record ChatMemberBanCommand(
-        Long adminId,
-        Long targetMemberId
+        Long userId,
+        Long targetMemberId,
+        Long chatRoomId
 ) {
     public ChatMemberBanCommand {
-        if (adminId == null) {
-            throw new IllegalArgumentException("adminId must not be null");
+        if (userId == null) {
+            throw new IllegalArgumentException("userId must not be null");
         }
         if (targetMemberId == null) {
             throw new IllegalArgumentException("targetMemberId must not be null");
         }
+        if (chatRoomId == null) {
+            throw new IllegalArgumentException("chatRoomId must not be null");
+        }
     }
 
-    public static ChatMemberBanCommand of(Long adminId, Long targetMemberId) {
-        return new ChatMemberBanCommand(adminId, targetMemberId);
+    public static ChatMemberBanCommand of(Long adminId, Long targetMemberId, Long chatRoomId) {
+        return new ChatMemberBanCommand(adminId, targetMemberId, chatRoomId);
     }
 }
