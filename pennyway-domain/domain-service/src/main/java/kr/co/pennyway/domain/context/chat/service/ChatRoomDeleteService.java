b2792322d12836dd069463648ff7a54b1833feb3
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-01-16 20:28:12 +0900

    style: using var key-word

--- pennyway-domain/domain-service/src/main/java/kr/co/pennyway/domain/context/chat/service/ChatRoomDeleteService.java
+++ pennyway-domain/domain-service/src/main/java/kr/co/pennyway/domain/context/chat/service/ChatRoomDeleteService.java
@@ -2,9 +2,7 @@ package kr.co.pennyway.domain.context.chat.service;
 
 import kr.co.pennyway.common.annotation.DomainService;
 import kr.co.pennyway.domain.context.chat.dto.ChatRoomDeleteCommand;
-import kr.co.pennyway.domain.domains.chatroom.domain.ChatRoom;
 import kr.co.pennyway.domain.domains.chatroom.service.ChatRoomRdbService;
-import kr.co.pennyway.domain.domains.member.domain.ChatMember;
 import kr.co.pennyway.domain.domains.member.exception.ChatMemberErrorCode;
 import kr.co.pennyway.domain.domains.member.exception.ChatMemberErrorException;
 import kr.co.pennyway.domain.domains.member.service.ChatMemberRdbService;
@@ -21,12 +19,12 @@ public class ChatRoomDeleteService {
 
     @Transactional
     public void deleteChatRoom(ChatRoomDeleteCommand command) {
-        ChatMember admin = chatMemberRdbService.readChatMember(command.userId(), command.chatRoomId())
+        var admin = chatMemberRdbService.readChatMember(command.userId(), command.chatRoomId())
                 .orElseThrow(() -> new ChatMemberErrorException(ChatMemberErrorCode.NOT_FOUND));
 
         if (!admin.isAdmin()) throw new ChatMemberErrorException(ChatMemberErrorCode.NOT_ADMIN);
 
-        ChatRoom chatRoom = admin.getChatRoom();
+        var chatRoom = admin.getChatRoom();
 
         chatMemberRdbService.deleteAllByChatRoomId(chatRoom.getId());
         chatRoomRdbService.delete(chatRoom);
