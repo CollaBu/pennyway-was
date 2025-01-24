8f6693826666a8f1e4d70f2d547c033522352329
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-01-24 16:31:50 +0900

    fix: delete chat-member-search-service

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/chat/usecase/ChatMemberUseCase.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/chat/usecase/ChatMemberUseCase.java
@@ -5,12 +5,12 @@ import kr.co.pennyway.api.apis.chat.dto.ChatRoomRes;
 import kr.co.pennyway.api.apis.chat.mapper.ChatMemberMapper;
 import kr.co.pennyway.api.apis.chat.mapper.ChatRoomMapper;
 import kr.co.pennyway.api.apis.chat.service.ChatMemberJoinService;
-import kr.co.pennyway.api.apis.chat.service.ChatMemberSearchService;
 import kr.co.pennyway.api.common.storage.AwsS3Adapter;
 import kr.co.pennyway.common.annotation.UseCase;
 import kr.co.pennyway.domain.context.chat.dto.ChatMemberBanCommand;
 import kr.co.pennyway.domain.context.chat.dto.ChatRoomAdminDelegateCommand;
 import kr.co.pennyway.domain.context.chat.service.ChatMemberBanService;
+import kr.co.pennyway.domain.context.chat.service.ChatMemberService;
 import kr.co.pennyway.domain.context.chat.service.ChatRoomAdminDelegateService;
 import kr.co.pennyway.domain.context.chat.service.ChatRoomLeaveService;
 import kr.co.pennyway.domain.domains.chatroom.domain.ChatRoom;
@@ -27,11 +27,11 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class ChatMemberUseCase {
     private final ChatMemberJoinService chatMemberJoinService;
-    private final ChatMemberSearchService chatMemberSearchService;
     private final ChatRoomLeaveService chatRoomLeaveService;
     private final ChatMemberBanService chatMemberBanService;
     private final ChatRoomAdminDelegateService chatRoomAdminDelegateService;
 
+    private final ChatMemberService chatMemberService;
     private final AwsS3Adapter awsS3Adapter;
 
     public ChatRoomRes.Detail joinChatRoom(Long userId, Long chatRoomId, Integer password) {
@@ -41,7 +41,7 @@ public class ChatMemberUseCase {
     }
 
     public List<ChatMemberRes.MemberDetail> readChatMembers(Long chatRoomId, Set<Long> chatMemberIds) {
-        List<ChatMemberResult.Detail> chatMembers = chatMemberSearchService.readChatMembers(chatRoomId, chatMemberIds);
+        List<ChatMemberResult.Detail> chatMembers = chatMemberService.readChatMembersByMemberIds(chatRoomId, chatMemberIds);
 
         return ChatMemberMapper.toChatMemberResDetail(chatMembers);
     }
