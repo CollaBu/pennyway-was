603858167216b212414ce56c6698c078c1d45f91
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-01-29 18:29:00 +0900

    fix: change return type to pair

--- pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/service/DailySpendingAggregateService.java
+++ pennyway-app-external-api/src/main/java/kr/co/pennyway/api/apis/ledger/service/DailySpendingAggregateService.java
@@ -5,11 +5,11 @@ import kr.co.pennyway.domain.domains.spending.dto.CategoryInfo;
 import kr.co.pennyway.domain.domains.spending.service.SpendingRdbService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Map;
 
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.summingLong;
@@ -21,9 +21,9 @@ public class DailySpendingAggregateService {
     private final SpendingRdbService spendingRdbService;
 
     @Transactional(readOnly = true)
-    public List<Map.Entry<CategoryInfo, Long>> execute(Long userId, int year, int month, int day) {
+    public List<Pair<CategoryInfo, Long>> execute(Long userId, int year, int month, int day) {
         var spendings = spendingRdbService.readSpendings(userId, year, month, day);
-        
+
         return spendings.stream()
                 .collect(
                         groupingBy(
@@ -32,7 +32,8 @@ public class DailySpendingAggregateService {
                         )
                 )
                 .entrySet().stream()
-                .sorted((o1, o2) -> (int) (o2.getValue() - o1.getValue()))
+                .map(entry -> Pair.of(entry.getKey(), entry.getValue()))
+                .sorted((o1, o2) -> (int) (o2.getSecond() - o1.getSecond()))
                 .toList();
     }
 }
