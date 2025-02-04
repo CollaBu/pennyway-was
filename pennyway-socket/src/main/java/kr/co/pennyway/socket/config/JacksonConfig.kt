8d24988a099aba1470ace9ea369bf7e4a3320724
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-02-04 23:56:39 +0900

    chore: add jackson config to socket module

---
+++ pennyway-socket/src/main/java/kr/co/pennyway/socket/config/JacksonConfig.kt
@@ -0,0 +1,18 @@
+package kr.co.pennyway.socket.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class JacksonConfig {
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        return ObjectMapper().apply {
+            registerModule(JavaTimeModule())
+            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        }
+    }
+}
